8952252938aad2fe69c23607e8c140b6f65af2c5
nicholaspai <npai.nyc@gmail.com>
2022-03-23 11:26:41 -0400

    feat: Add Dataworker skeleton
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

---
+++ src/Dataworker.ts
@@ -0,0 +1,54 @@
+import { BigNumber, winston, buildFillRelayProps } from "./utils";
+import { SpokePoolEventClient } from "./SpokePoolEventClient";
+import { HubPoolEventClient } from "./HubPoolEventClient";
+import { MulticallBundler } from "./MulticallBundler";
+import { Deposit } from "./interfaces/SpokePool";
+
+export interface BlockRange {
+    fromBlock: number;
+    toBlock: number;
+}
+// @notice Constructs roots to submit to HubPool on L1
+export class Dataworker {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    readonly logger: winston.Logger,
+    readonly spokePoolEventClients: { [chainId: number]: SpokePoolEventClient },
+    readonly hubPoolClient: HubPoolEventClient,
+    readonly multicallBundler: MulticallBundler | any
+  ) {}
+
+  async buildSlowRelayRoot(blockRange: BlockRange) {
+      // Grab all deposits from SpokePoolEventClient
+      // Grab all fills from SpokePoolEventClient
+      // Filter out invalid fills for deposits
+      // Filter out fills that fully fill a deposit
+      // Group remaining fills by destination chain ID
+      // Construct leaf for destination chain ID
+      // Construct root
+  }
+
+  async buildRelayerRefundRoot(blockRange: BlockRange) {
+        // Grab all deposits from SpokePoolEventClient
+        // Grab all fills from SpokePoolEventClient
+        // Filter out invalid fills for deposits
+        // Group remaining fills by destination chain ID
+        // Construct leaf for destination chain ID
+        // Construct root
+
+    }
+
+async buildPoolRebalanceRoot(blockRange: BlockRange) {
+        // Grab all deposits from SpokePoolEventClient
+        // Grab all fills from SpokePoolEventClient
+        // Filter out invalid fills for deposits
+        // Group remaining fills by destination chain ID
+        // For each destination chain ID:
+        // Group fills by L1 token
+        // For each L1 token:
+        // Sum total relay refund amount ==> netSendAmount
+        // Sum total realized LP fee % ==> bundleLpFee
+        // Construct leaf for destination chain ID with fills grouped by L1 token
+        // Construct root
+    }
+}
