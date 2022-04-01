bc666ab143db0a39689111ec166dbbef724e5aa3
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2022-04-01 15:47:56 -0400

    feat(dataworker): Add function to load and format data used in all functions (#15)
    * feat(dataworker): Add common function to load all client data
    
    * Add tests
    
    * Add comments
    
    * lint
    
    * add additional tests
    
    * Update Dataworker.loadData.ts
    
    * Update src/dataworker/Dataworker.ts
    
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>
    
    * change data => deposit, add log above top for loop
    
    * More nits
    
    * Update src/dataworker/Dataworker.ts
    
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>
    
    * move buildDeposit and buildFill to utils
    
    * rename dataworker utils methods
    
    * refactor
    
    * Rename again
    
    * Update src/dataworker/Dataworker.ts
    
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>
    
    * Update Dataworker.ts
    
    * respond to matt nits
    
    * lint-fix
    
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>

--- src/dataworker/Dataworker.ts
+++ src/dataworker/Dataworker.ts
@@ -1,9 +1,10 @@
-import { BigNumber, winston, buildFillRelayProps } from "../utils";
-import { SpokePoolClient, HubPoolClient, RateModelClient, MultiCallBundler } from "../clients";
-import { Deposit } from "../interfaces/SpokePool";
+import { winston, assign } from "../utils";
+import { SpokePoolClient, HubPoolClient, MultiCallBundler } from "../clients";
+import { UnfilledDeposits, FillsToRefund } from "../interfaces/SpokePool";
 import { BundleEvaluationBlockNumbers } from "../interfaces/HubPool";
 
-// @notice Constructs roots to submit to HubPool on L1
+// @notice Constructs roots to submit to HubPool on L1. Fetches all data synchronously from SpokePool/HubPool clients
+// so this class assumes that those upstream clients are already updated and have fetched on-chain data from RPC's.
 export class Dataworker {
   // eslint-disable-next-line no-useless-constructor
   constructor(
@@ -13,40 +14,145 @@ export class Dataworker {
     readonly multiCallBundler: MultiCallBundler | any
   ) {}
 
-  async update() {
-    // Grab all deposits from SpokePoolClient
-    // Map deposits to their destination chain IDs
-    // Grab all fills from SpokePoolClient
-    // Filter out invalid fills for deposits
-    // Group remaining fills by destination chain ID
+  // Common data re-formatting logic shared across all data worker public functions.
+  _loadData(): { unfilledDeposits: UnfilledDeposits; fillsToRefund: FillsToRefund } {
+    // For each origin chain spoke pool client:
+    //     For all destination spoke pool client's:
+    //         Store deposits that are sent from origin chain to destination chain as an UnfilledDeposit
+    //             Associate this UnfilledDeposit with the destination chain and set fillAmountRemaining
+    //         Grab all fills on destination client
+    //             Attempt to map fill to an UnfilledDeposit sent from the origin chain
+    //             If a match is found, save fill and key by repaymentChainId => refundAddress
+
+    const unfilledDeposits: UnfilledDeposits = {};
+    const fillsToRefund: FillsToRefund = {};
+
+    const allChainIds = Object.keys(this.spokePoolClients);
+    this.logger.debug({ at: "Dataworker", message: `Loading deposit and fill data`, chainIds: allChainIds });
+    for (const originChainId of allChainIds) {
+      const originClient = this.spokePoolClients[originChainId];
+      if (!originClient.isUpdated()) throw new Error(`origin spokepoolclient on chain ${originChainId} not updated`);
+
+      // Loop over all other SpokePoolClient's to find deposits whose destination chain is the selected origin chain.
+      this.logger.debug({ at: "Dataworker", message: `Looking up data for origin spoke pool`, originChainId });
+      for (const destinationChainId of Object.keys(this.spokePoolClients)) {
+        if (originChainId === destinationChainId) continue;
+
+        const destinationClient = this.spokePoolClients[destinationChainId];
+        if (!destinationClient.isUpdated())
+          throw new Error(`destination spokepoolclient with chain ID ${destinationChainId} not updated`);
+
+        // Store deposits whose destination chain is the selected chain as unfilled deposits and set the initial fill
+        // amount remaining equal to the full deposit amount minus any valid fill amounts.
+        // Remove any deposits that have no unfilled amount (i.e that have an unfilled amount of 0) and append the
+        // remaining deposits to the unfilledDeposits array.
+        const depositsForDestinationChain = originClient.getDepositsForDestinationChain(destinationChainId);
+        this.logger.debug({
+          at: "Dataworker",
+          message: `Found ${depositsForDestinationChain.length} deposits for destination chain ${destinationChainId}`,
+          originChainId,
+          destinationChainId,
+        });
+
+        const unfilledDepositsForDestinationChain = depositsForDestinationChain
+          .map((deposit) => {
+            return { deposit, unfilledAmount: destinationClient.getValidUnfilledAmountForDeposit(deposit) };
+          })
+          .filter((deposit) => deposit.unfilledAmount.gt(0));
+
+        if (unfilledDepositsForDestinationChain.length > 0)
+          assign(unfilledDeposits, [destinationChainId], unfilledDepositsForDestinationChain);
+        else
+          this.logger.debug({
+            at: "Dataworker",
+            message: `All deposits are filled`,
+            originChainId,
+            destinationChainId,
+          });
+
+        // Grab all valid fills submitted to the destination spoke pool.
+        const fillsOnDestinationChain = destinationClient.getFills();
+        const validFillsOnDestinationChain = fillsOnDestinationChain.filter((fill) => {
+          // For each fill, see if we can find a deposit sent from the origin client that matches it.
+          for (const deposit of depositsForDestinationChain) {
+            // Note1: It doesn't matter which client we call validateFillForDeposit() on as the logic is
+            // chain agnostic.
+            // Note2: All of the deposits returned by `getDepositsForDestinationChain` will include the expected realized
+            // lp fee % for the deposit quote time. If this fill does not have the same realized lp fee %, then it will
+            // be ignored.
+
+            if (destinationClient.validateFillForDeposit(fill, deposit)) return true;
+            else continue;
+          }
+
+          return false; // No deposit matched, this fill is invalid.
+        });
+
+        this.logger.debug({
+          at: "Dataworker",
+          message: `Found ${validFillsOnDestinationChain.length} fills on destination ${destinationChainId} matching origin ${originChainId}`,
+          originChainId,
+          destinationChainId,
+        });
+        validFillsOnDestinationChain.forEach((fill) =>
+          assign(fillsToRefund, [fill.repaymentChainId, fill.relayer], [fill])
+        );
+      }
+    }
+
+    return {
+      fillsToRefund,
+      unfilledDeposits,
+    };
   }
 
   async buildSlowRelayRoot(bundleBlockNumbers: BundleEvaluationBlockNumbers) {
-    // Filter out fills that fully fill a deposit. Can check against the list of deposits
-    //     linked to this destination chain ID.
-    // Order fills by remaining fill size
-    // Construct leaf for destination chain ID
+    this._loadData();
+
+    // For each destination chain ID key in unfilledDeposits
+    //     Order by fillAmountRemaining
+    //     Make Leaf for fill
     // Construct root
   }
 
   async buildRelayerRefundRoot(bundleBlockNumbers: BundleEvaluationBlockNumbers) {
-    // Group by refund address
-    // Order fills by total size
-    // Construct leaf for destination chain ID
+    this._loadData();
+
+    // For each repayment chain ID key in fillsToRefund
+    //     Group by refundAddress, and for each refund address
+    //         Order fills by fillAmount
+    //     Make Leaf for repayment chain ID
     // Construct root
   }
 
   async buildPoolRebalanceRoot(bundleBlockNumbers: BundleEvaluationBlockNumbers) {
-    // For each destination chain ID:
-    // Group fills by L1 token
-    // For each L1 token:
-    // Sum total relay refund amount ==> netSendAmount
-    // Sum total slow relay amount and add to netSendAmount
-    // Sum total realized LP fee % ==> bundleLpFee
-    // Construct leaves for destination chain ID with fills grouped by L1 token
-    // If there are too many L1 tokens for a single destination chain ID leaf,
-    //     then split up the L1 tokens across multiple leaves and give each
-    //     a unique groupIndex, starting at 0 and counting up by 1.
+    this._loadData();
+
+    // For each destination chain ID key in unfilledDeposits
+    //     Group by L1 token and for each L1 token:
+    //         Add unfilledAmount to netSendAmount for L1 token
+    //         Add realized LP fee to bundleLpFee for L1 token
+    //         Figure out how RunningBalances works
+    // For each repayment chain ID key in fillsToRefund
+    //     Group by L1 token and for each L1 token:
+    //         Add fillAmount to netSendAmount for L1 token
+    //         Add realized LP fee to bundleLpFee for L1 token
+    //         Figure out how RunningBalances works
+    // Join repayment chain ID and destination chain ID data together
+    // Make Leaf for destination chain ID. Optionally decide to split Leaf
+    // data into smaller pieces and form sub groups with unique groupIndex's
     // Construct root
+  }
+
+  async validateRootBundle(
+    bundleBlockNumbers: BundleEvaluationBlockNumbers,
+    poolRebalanceRoot: string,
+    relayerRefundRoot: string,
+    slowRelayRoot: string
+  ) {
+    this._loadData();
+
+    // Construct roots locally using class functions and compare with input roots.
+    // If any roots mismatch, efficiently pinpoint the errors to give details to the caller.
   }
 }
---
+++ test/Dataworker.loadData.ts
@@ -0,0 +1,270 @@
+import { deploySpokePoolWithToken, enableRoutesOnHubPool, expect, ethers, Contract } from "./utils";
+import { SignerWithAddress, setupTokensForWallet, getLastBlockTime } from "./utils";
+import { buildDeposit, buildFill } from "./utils";
+import { createSpyLogger, winston, deployAndConfigureHubPool, deployRateModelStore } from "./utils";
+import { SpokePoolClient, HubPoolClient, RateModelClient, MultiCallBundler } from "../src/clients";
+import { amountToLp, amountToDeposit, repaymentChainId, destinationChainId, originChainId } from "./constants";
+
+import { Dataworker } from "../src/dataworker/Dataworker"; // Tested
+import { toBN } from "../src/utils";
+
+let spokePool_1: Contract, erc20_1: Contract, spokePool_2: Contract, erc20_2: Contract;
+let hubPool: Contract, rateModelStore: Contract, l1Token: Contract;
+let owner: SignerWithAddress, depositor: SignerWithAddress, relayer: SignerWithAddress;
+let spyLogger: winston.Logger;
+
+let spokePoolClient_1: SpokePoolClient, spokePoolClient_2: SpokePoolClient;
+let rateModelClient: RateModelClient, hubPoolClient: HubPoolClient;
+let dataworkerInstance: Dataworker;
+let multiCallBundler: MultiCallBundler;
+
+describe("Dataworker: Load data used in all functions", async function () {
+  beforeEach(async function () {
+    [owner, depositor, relayer] = await ethers.getSigners();
+    ({ spokePool: spokePool_1, erc20: erc20_1 } = await deploySpokePoolWithToken(originChainId, destinationChainId));
+    ({ spokePool: spokePool_2, erc20: erc20_2 } = await deploySpokePoolWithToken(destinationChainId, originChainId));
+
+    // Only set cross chain contracts for one spoke pool to begin with.
+    ({ hubPool, l1Token } = await deployAndConfigureHubPool(owner, [
+      { l2ChainId: destinationChainId, spokePool: spokePool_2 },
+    ]));
+
+    // For each chain, enable routes to both erc20's so that we can fill relays
+    await enableRoutesOnHubPool(hubPool, [
+      { destinationChainId: originChainId, l1Token, destinationToken: erc20_1 },
+      { destinationChainId: destinationChainId, l1Token, destinationToken: erc20_2 },
+    ]);
+
+    ({ spyLogger } = createSpyLogger());
+    ({ rateModelStore } = await deployRateModelStore(owner, [l1Token]));
+    hubPoolClient = new HubPoolClient(spyLogger, hubPool);
+    rateModelClient = new RateModelClient(spyLogger, rateModelStore, hubPoolClient);
+
+    multiCallBundler = new MultiCallBundler(spyLogger, null); // leave out the gasEstimator for now.
+
+    spokePoolClient_1 = new SpokePoolClient(spyLogger, spokePool_1.connect(relayer), rateModelClient, originChainId);
+    spokePoolClient_2 = new SpokePoolClient(
+      spyLogger,
+      spokePool_2.connect(relayer),
+      rateModelClient,
+      destinationChainId
+    );
+
+    dataworkerInstance = new Dataworker(
+      spyLogger,
+      { [originChainId]: spokePoolClient_1, [destinationChainId]: spokePoolClient_2 },
+      hubPoolClient,
+      multiCallBundler
+    );
+
+    // Give owner tokens to LP on HubPool with.
+    await setupTokensForWallet(spokePool_1, owner, [l1Token], null, 100); // Seed owner to LP.
+    await l1Token.approve(hubPool.address, amountToLp);
+    await hubPool.addLiquidity(l1Token.address, amountToLp);
+
+    // Give depositors the tokens they'll deposit into spoke pools:
+    await setupTokensForWallet(spokePool_1, depositor, [erc20_1], null, 10);
+    await setupTokensForWallet(spokePool_2, depositor, [erc20_2], null, 10);
+
+    // Give relayers the tokens they'll need to relay on spoke pools:
+    await setupTokensForWallet(spokePool_1, relayer, [erc20_1, erc20_2, l1Token], null, 10);
+    await setupTokensForWallet(spokePool_2, relayer, [erc20_1, erc20_2, l1Token], null, 10);
+
+    // Set the spokePool's time to the provider time. This is done to enable the block utility time finder identify a
+    // "reasonable" block number based off the block time when looking at quote timestamps.
+    await spokePool_1.setCurrentTime(await getLastBlockTime(spokePool_1.provider));
+    await spokePool_2.setCurrentTime(await getLastBlockTime(spokePool_2.provider));
+  });
+
+  it("Default conditions", async function () {
+    // Throws error if spoke pool client not updated.
+    expect(() => dataworkerInstance._loadData()).to.throw(/not updated/);
+
+    // Before any deposits, returns empty dictionaries.
+    await updateAllClients();
+    expect(dataworkerInstance._loadData()).to.deep.equal({
+      unfilledDeposits: {},
+      fillsToRefund: {},
+    });
+  });
+  it("Returns unfilled deposits", async function () {
+    await updateAllClients();
+
+    const deposit1 = await buildDeposit(
+      rateModelClient,
+      hubPoolClient,
+      spokePool_1,
+      erc20_1,
+      l1Token,
+      depositor,
+      destinationChainId,
+      amountToDeposit
+    );
+    const deposit2 = await buildDeposit(
+      rateModelClient,
+      hubPoolClient,
+      spokePool_2,
+      erc20_2,
+      l1Token,
+      depositor,
+      originChainId,
+      amountToDeposit
+    );
+
+    // One completely unfilled deposit per destination chain ID.
+    await updateAllClients();
+    const data1 = dataworkerInstance._loadData();
+    expect(data1.unfilledDeposits).to.deep.equal({
+      [destinationChainId]: [{ unfilledAmount: amountToDeposit, deposit: deposit1 }],
+      [originChainId]: [{ unfilledAmount: amountToDeposit, deposit: deposit2 }],
+    });
+
+    // Two unfilled deposits per destination chain ID.
+    const deposit3 = await buildDeposit(
+      rateModelClient,
+      hubPoolClient,
+      spokePool_1,
+      erc20_1,
+      l1Token,
+      depositor,
+      destinationChainId,
+      amountToDeposit.mul(toBN(2))
+    );
+    const deposit4 = await buildDeposit(
+      rateModelClient,
+      hubPoolClient,
+      spokePool_2,
+      erc20_2,
+      l1Token,
+      depositor,
+      originChainId,
+      amountToDeposit.mul(toBN(2))
+    );
+    await updateAllClients();
+    const data2 = dataworkerInstance._loadData();
+    expect(data2.unfilledDeposits).to.deep.equal({
+      [destinationChainId]: [
+        { unfilledAmount: amountToDeposit, deposit: deposit1 },
+        { unfilledAmount: amountToDeposit.mul(toBN(2)), deposit: deposit3 },
+      ],
+      [originChainId]: [
+        { unfilledAmount: amountToDeposit, deposit: deposit2 },
+        { unfilledAmount: amountToDeposit.mul(toBN(2)), deposit: deposit4 },
+      ],
+    });
+
+    // Fills that don't match deposits do not affect unfilledAmount counter.
+    // Note: We switch the spoke pool address in the following fills from the fills that eventually do match with
+    //       the deposits.
+    await buildFill(spokePool_1, erc20_2, depositor, relayer, deposit1, 0.5);
+    await buildFill(spokePool_2, erc20_1, depositor, relayer, deposit2, 0.25);
+
+    // Two unfilled deposits (one partially filled) per destination chain ID.
+    const fill1 = await buildFill(spokePool_2, erc20_2, depositor, relayer, deposit1, 0.5);
+    const fill2 = await buildFill(spokePool_1, erc20_1, depositor, relayer, deposit2, 0.25);
+    await updateAllClients();
+    const data3 = dataworkerInstance._loadData();
+    expect(data3.unfilledDeposits).to.deep.equal({
+      [destinationChainId]: [
+        { unfilledAmount: amountToDeposit.sub(fill1.fillAmount), deposit: deposit1 },
+        { unfilledAmount: amountToDeposit.mul(toBN(2)), deposit: deposit3 },
+      ],
+      [originChainId]: [
+        { unfilledAmount: amountToDeposit.sub(fill2.fillAmount), deposit: deposit2 },
+        { unfilledAmount: amountToDeposit.mul(toBN(2)), deposit: deposit4 },
+      ],
+    });
+
+    // One completely filled deposit per destination chain ID.
+    await buildFill(spokePool_2, erc20_2, depositor, relayer, deposit3, 1);
+    await buildFill(spokePool_1, erc20_1, depositor, relayer, deposit4, 1);
+    await updateAllClients();
+    const data4 = dataworkerInstance._loadData();
+    expect(data4.unfilledDeposits).to.deep.equal({
+      [destinationChainId]: [{ unfilledAmount: amountToDeposit.sub(fill1.fillAmount), deposit: deposit1 }],
+      [originChainId]: [{ unfilledAmount: amountToDeposit.sub(fill2.fillAmount), deposit: deposit2 }],
+    });
+
+    // All deposits are fulfilled
+    await buildFill(spokePool_2, erc20_2, depositor, relayer, deposit1, 1);
+    await buildFill(spokePool_1, erc20_1, depositor, relayer, deposit2, 1);
+    await updateAllClients();
+    const data5 = dataworkerInstance._loadData();
+    expect(data5.unfilledDeposits).to.deep.equal({});
+  });
+  it("Returns fills to refund", async function () {
+    await updateAllClients();
+
+    // Submit a valid fill
+    const deposit1 = await buildDeposit(
+      rateModelClient,
+      hubPoolClient,
+      spokePool_1,
+      erc20_1,
+      l1Token,
+      depositor,
+      destinationChainId,
+      amountToDeposit
+    );
+    const deposit2 = await buildDeposit(
+      rateModelClient,
+      hubPoolClient,
+      spokePool_2,
+      erc20_2,
+      l1Token,
+      depositor,
+      originChainId,
+      amountToDeposit
+    );
+    const fill1 = await buildFill(spokePool_2, erc20_2, depositor, relayer, deposit1, 0.5);
+
+    // Should return one valid fill linked to the repayment chain ID
+    await updateAllClients();
+    const data1 = dataworkerInstance._loadData();
+    expect(data1.fillsToRefund).to.deep.equal({
+      [repaymentChainId]: { [relayer.address]: [fill1] },
+    });
+
+    // Submit two more fills: one for the same relayer and one for a different one.
+    const fill2 = await buildFill(spokePool_1, erc20_1, depositor, relayer, deposit2, 0.25);
+    const fill3 = await buildFill(spokePool_2, erc20_2, depositor, depositor, deposit1, 1);
+    await updateAllClients();
+    const data2 = dataworkerInstance._loadData();
+    expect(data2.fillsToRefund).to.deep.equal({
+      [repaymentChainId]: { [relayer.address]: [fill1, fill2], [depositor.address]: [fill3] },
+    });
+
+    // Submit fills without matching deposits. These should be ignored by the client.
+    // Note: Switch the deposit data to make fills invalid.
+    await buildFill(spokePool_2, erc20_2, depositor, relayer, deposit2, 0.5);
+    await buildFill(spokePool_2, erc20_2, depositor, depositor, deposit2, 1);
+    await updateAllClients();
+    const data3 = dataworkerInstance._loadData();
+    expect(data3.fillsToRefund).to.deep.equal(data2.fillsToRefund);
+
+    // Submit fills that match deposit in all properties except for realized lp fee % or l1 token. These should be
+    // ignored because the rate model client deems them invalid. These are the two properties added to the deposit
+    // object by the spoke pool client.
+    // Note: This fill has identical deposit data to fill2 except for the realized lp fee %.
+    await buildFill(
+      spokePool_1,
+      erc20_1,
+      depositor,
+      relayer,
+      { ...deposit2, realizedLpFeePct: deposit2.realizedLpFeePct.div(toBN(2)) },
+      0.25
+    );
+    // Note: This fill has identical deposit data to fill2 except for the destination token being different
+    await buildFill(spokePool_1, l1Token, depositor, relayer, deposit2, 0.25);
+    await updateAllClients();
+    const data4 = dataworkerInstance._loadData();
+    expect(data4.fillsToRefund).to.deep.equal(data2.fillsToRefund);
+  });
+});
+
+async function updateAllClients() {
+  await hubPoolClient.update();
+  await rateModelClient.update();
+  await spokePoolClient_1.update();
+  await spokePoolClient_2.update();
+}
