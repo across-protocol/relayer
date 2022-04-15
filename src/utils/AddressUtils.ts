5d8228db8139b1b0c8eff0d7d5fcccb772d5cd67
nicholaspai <npai.nyc@gmail.com>
2022-04-15 07:38:48 -0400

    lint-fix
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- src/utils/AddressUtils.ts
+++ src/utils/AddressUtils.ts
@@ -1,11 +1,11 @@
 import { BigNumber } from ".";
 
 export function compareAddresses(addressA: string, addressB: string) {
-    // Convert address strings to BigNumbers and then sort numerical value of the BigNumber, which sorts the addresses
-    // effectively by their hex value.
-    const bnAddressA = BigNumber.from(addressA);
-    const bnAddressB = BigNumber.from(addressB);
-    if (bnAddressA.gt(bnAddressB)) return 1;
-    else if (bnAddressA.lt(bnAddressB)) return -1;
-    else return 0;
-  }
+  // Convert address strings to BigNumbers and then sort numerical value of the BigNumber, which sorts the addresses
+  // effectively by their hex value.
+  const bnAddressA = BigNumber.from(addressA);
+  const bnAddressB = BigNumber.from(addressB);
+  if (bnAddressA.gt(bnAddressB)) return 1;
+  else if (bnAddressA.lt(bnAddressB)) return -1;
+  else return 0;
+}
