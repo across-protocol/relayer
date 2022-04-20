34d739aebe8fbdc45dac0bcf0400ea7e38bbf125
chrismaree <christopher.maree@gmail.com>
2022-04-20 14:12:58 +0200

    wip
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- src/utils/ProviderUtils.ts
+++ src/utils/ProviderUtils.ts
@@ -7,13 +7,17 @@ export function getProvider(networkId: number, nodeQuorumThreshold: number = 1) 
   return new ethers.providers.JsonRpcProvider(nodeUrl);
 }
 
+// Create a fallback provider. This provider type enforces that nodeQuorumThreshold of the total retry configs defined
+// all agree in the data they return before returning the data to the provider user. This applies in the context of both
+// events and state reads to ensure consistency and redundancy within the infrastructure. Set the priority to 1.
+// This makes all providers equally weighted and should all be considered. Stall timeout is how long to wait for a node
+// to reply before trying another node in the set provided. This would be the case when the number of nodes is more than
+// the nodeQuorumThreshold (i.e using a 2 of 3 setup).
 export function getFallbackProvider(networkId: number, nodeQuorumThreshold: number) {
   const nodeUrls = JSON.parse(process.env[`RETRY_CONFIG_${networkId}`]) || [];
   if (nodeUrls.length == 0) throw new Error(`No RETRY_CONFIG_ for network ${networkId}`);
-  // Create a fallback provider. Set the priority to 1. This makes all providers equally weighted.
-  // stallTimeout is how long to wait for a node to respond before trying another provider in the set.
-  // At least nodeQuorumThreshold number of nodes must agree before any results are used.
-  return new ethers.providers.FallbackProvider(
+
+  return new providers.FallbackProvider(
     nodeUrls.map((url) => {
       return { provider: new ethers.providers.JsonRpcProvider(url), priority: 1, stallTimeout: 2000, weight: 1 };
     }),
