b3e38babaeed3972d57a3930e99120b91d5c6546
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2022-05-06 16:53:53 -0400

    feat: HubPoolClient should be able to return cross chain contracts for a specific block height (#34)
    * improve: Add secondary and tertiary sorting indices for event sorting functions
    
    * lint
    
    * feat: Implement block range filtering for dataworker
    
    * feat: Should be able to return spoke pool for a specific block height
    
    * lint
    
    * Construct spoke pool clients at propose time
    
    * lint
    
    * Update Dataworker.ts
    
    * fix
    
    * Update Dataworker.ts
    
    * Update Dataworker.ts
    
    * Update Dataworker.ts
    
    * Update Dataworker.ts
    
    * Remove spoke pool client from dataworker setup

--- src/relayer/RelayerClientHelper.ts
+++ src/relayer/RelayerClientHelper.ts
@@ -1,10 +1,11 @@
 import winston from "winston";
-import { getSigner } from "../utils";
-import { TokenClient, ProfitClient } from "../clients";
+import { Contract, getDeployedContract, getDeploymentBlockNumber, getSigner } from "../utils";
+import { TokenClient, ProfitClient, SpokePoolClient } from "../clients";
 import { RelayerConfig } from "./RelayerConfig";
-import { Clients, constructClients, updateClients } from "../common";
+import { Clients, constructClients, updateClients, getSpokePoolSigners } from "../common";
 
 export interface RelayerClients extends Clients {
+  spokePoolClients: { [chainId: number]: SpokePoolClient };
   tokenClient: TokenClient;
   profitClient: ProfitClient;
 }
@@ -14,24 +15,57 @@ export async function constructRelayerClients(logger: winston.Logger, config: Re
 
   const commonClients = await constructClients(logger, config);
 
-  const tokenClient = new TokenClient(logger, baseSigner.address, commonClients.spokePoolClients);
+  // Create clients for each contract for each chain.
+  const spokePoolSigners = getSpokePoolSigners(baseSigner, config);
+  const spokePools = config.spokePoolChains.map((networkId, index) => {
+    return { networkId, contract: getDeployedContract("SpokePool", networkId, spokePoolSigners[index]) };
+  });
+  const spokePoolClients = {};
+  spokePools.forEach((obj: { networkId: number; contract: Contract }) => {
+    const spokePoolClientSearchSettings = {
+      fromBlock: Number(getDeploymentBlockNumber("SpokePool", obj.networkId)),
+      toBlock: null,
+      maxBlockLookBack: config.maxBlockLookBack[obj.networkId],
+    };
+    spokePoolClients[obj.networkId] = new SpokePoolClient(
+      logger,
+      obj.contract,
+      commonClients.configStoreClient,
+      obj.networkId,
+      spokePoolClientSearchSettings
+    );
+  });
+
+  const tokenClient = new TokenClient(logger, baseSigner.address, spokePoolClients);
 
   const profitClient = new ProfitClient(logger, commonClients.hubPoolClient, config.relayerDiscount);
 
-  return { ...commonClients, tokenClient, profitClient };
+  return { ...commonClients, tokenClient, profitClient, spokePoolClients };
 }
 
+// If this is the first run then the hubPoolClient will have no whitelisted routes. If this is the case then first
+// update the hubPoolClient and the rateModelClients followed by the spokePoolClients. Else, update all at once.
 export async function updateRelayerClients(logger: winston.Logger, clients: RelayerClients) {
   if (Object.keys(clients.hubPoolClient.getL1TokensToDestinationTokens()).length === 0) {
-    await updateClients(logger, clients);
-    // Profit client requires up to date HuPoolClient and rateModelClient.
+    await updateClients(clients);
+    // Profit and SpokePoolClient client requires up to date HubPoolClient and rateModelClient.
     // Token client requires up to date spokePool clients to fetch token routes.
-    await Promise.all([clients.profitClient.update(), clients.tokenClient.update()]);
+    await Promise.all([clients.profitClient.update(), updateSpokePoolClients(clients.spokePoolClients)]);
+    await clients.tokenClient.update();
   } else {
     logger.debug({ at: "ClientHelper", message: "Updating clients for standard run" });
-    await Promise.all([updateClients(logger, clients), clients.tokenClient.update(), clients.profitClient.update()]);
+    await Promise.all([
+      updateClients(clients),
+      updateSpokePoolClients(clients.spokePoolClients),
+      clients.tokenClient.update(),
+      clients.profitClient.update(),
+    ]);
   }
 
   // Run approval check last as needs up to date route info. If no new then returns with no async calls.
   await clients.tokenClient.setOriginTokenApprovals();
+}
+
+async function updateSpokePoolClients(spokePoolClients: { [chainId: number]: SpokePoolClient }) {
+  await Promise.all(Object.values(spokePoolClients).map((client) => client.update()));
 }
