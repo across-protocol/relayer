6f636bc662c940a96e6d8614cda96e277dff39e6
Chris Maree <christopher.maree@gmail.com>
2022-05-06 15:09:39 +0200

    Delete ClientHelper.ts

--- src/clients/ClientHelper.ts
+++
@@ -1,8 +0,0 @@
-import { SpokePoolClient, HubPoolClient, AcrossConfigStoreClient, MultiCallerClient } from ".";
-
-export interface Clients {
-  spokePoolClients: { [chainId: number]: SpokePoolClient };
-  hubPoolClient: HubPoolClient;
-  configStoreClient: AcrossConfigStoreClient;
-  multiCallerClient: MultiCallerClient;
-}
