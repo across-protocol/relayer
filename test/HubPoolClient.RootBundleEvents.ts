47d8950bbf8ee2b35ae2215c2b3047def126885c
nicholaspai <npai.nyc@gmail.com>
2022-05-12 20:20:00 -0400

    Update HubPoolClient.RootBundleEvents.ts

--- test/HubPoolClient.RootBundleEvents.ts
+++ test/HubPoolClient.RootBundleEvents.ts
@@ -32,7 +32,8 @@ describe("HubPoolClient: RootBundle Events", async function () {
       ethers,
       constants.MAX_REFUNDS_PER_RELAYER_REFUND_LEAF,
       constants.MAX_L1_TOKENS_PER_POOL_REBALANCE_LEAF,
-      constants.DEFAULT_POOL_BALANCE_TOKEN_TRANSFER_THRESHOLD
+      constants.DEFAULT_POOL_BALANCE_TOKEN_TRANSFER_THRESHOLD,
+      0
     ));
     hubPoolClient = new HubPoolClient(createSpyLogger().spyLogger, hubPool);
   });
