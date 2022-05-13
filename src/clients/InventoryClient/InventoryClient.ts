18c3aaf28b73b720cc16691bf7a0e1f9d6fcf494
chrismaree <christopher.maree@gmail.com>
2022-05-13 15:53:02 +0200

    lint
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- src/clients/InventoryClient/InventoryClient.ts
+++ src/clients/InventoryClient/InventoryClient.ts
@@ -7,18 +7,20 @@ export class InventoryClient {
     readonly logger: winston.Logger,
     readonly inventorySettings: InventorySettings,
     readonly tokenClient: TokenClient,
-    readonly hubClient: HubPoolClient,
+    readonly hubPoolClient: HubPoolClient,
     readonly enabledChains: number[]
   ) {}
 
   getCumulativeBalance(token: string): BigNumber {
     return this.enabledChains
-      .map((chainId) => this.tokenClient.getBalance(chainId, token))
+      .map((chainId) =>
+        this.tokenClient.getBalance(chainId, this.hubPoolClient.getDestinationTokenForL1Token(token, chainId))
+      )
       .reduce((acc, curr) => acc.add(curr), toBN(0));
   }
 
   async rebalanceInventoryIfNeeded() {
-    console.log("REBALANCE", this.getCumulativeBalance(""));
+    console.log("REBALANCE", this.getCumulativeBalance("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"));
   }
   async update() {}
 }
