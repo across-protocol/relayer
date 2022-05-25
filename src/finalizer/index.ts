0822b911453c2bbd8bb064cda2fdca00f3a87316
nicholaspai <npai.nyc@gmail.com>
2022-05-25 14:23:23 -0400

    Add to entrypoint

--- src/finalizer/index.ts
+++ src/finalizer/index.ts
@@ -1,9 +1,6 @@
-// NOTE: The "finalizers/" directory structure is just a strawman and is expected to change.
-import { delay, Logger } from "../utils";
+import { processCrash, processEndPollingLoop, startupLogLevel, Wallet } from "../utils";
 import { getProvider, getSigner, winston } from "../utils";
-import { constructClients, updateClients, updateSpokePoolClients } from "../common";
-import { RelayerConfig } from "../relayer/RelayerConfig";
-import { constructSpokePoolClientsWithLookback } from "../relayer/RelayerClientHelper";
+import { constructRelayerClients, RelayerClients, updateRelayerClients } from "../relayer/RelayerClientHelper";
 import {
   finalizeArbitrum,
   finalizePolygon,
@@ -13,24 +10,16 @@ import {
   getPosClient,
   retrieveTokenFromMainnetTokenBridger,
 } from "./utils";
+import { FinalizerConfig } from "./FinalizerConfig";
+let logger: winston.Logger;
 
-// How to run:
-// - Set same config you'd need to relayer in terms of L2 node urls
-// - ts-node ./src/finalizer/index.ts --wallet mnemonic
-
-export async function run(logger: winston.Logger, config: RelayerConfig): Promise<void> {
-  // Common set up with Dataworker/Relayer client helpers, can we refactor?
-  const baseSigner = await getSigner();
-  const hubSigner = baseSigner.connect(getProvider(config.hubPoolChainId));
-  const commonClients = await constructClients(logger, config);
-  const spokePoolClients = await constructSpokePoolClientsWithLookback(logger, commonClients, config, baseSigner);
-
-  await updateClients(commonClients);
-  await updateSpokePoolClients(spokePoolClients);
-
-  // TODO: Load chain ID's from config
-  const configuredChainIds = [137, 42161];
-
+export async function run(
+  logger: winston.Logger,
+  hubSigner: Wallet,
+  relayerClients: RelayerClients,
+  configuredChainIds: number[]
+): Promise<void> {
+  const spokePoolClients = relayerClients.spokePoolClients;
   // For each chain, look up any TokensBridged events emitted by SpokePool client that we'll attempt to finalize
   // on L1.
   for (const chainId of configuredChainIds) {
@@ -46,7 +35,7 @@ export async function run(logger: winston.Logger, config: RelayerConfig): Promis
         logger,
         tokensBridged,
         hubSigner,
-        commonClients.hubPoolClient
+        relayerClients.hubPoolClient
       );
       for (const l2Message of finalizableMessages) {
         await finalizeArbitrum(logger, l2Message.message, l2Message.token, l2Message.proofInfo);
@@ -57,39 +46,46 @@ export async function run(logger: winston.Logger, config: RelayerConfig): Promis
         message: "Looking for finalizable bridge events",
       });
       const posClient = await getPosClient(hubSigner);
-      const canWithdraw = await getFinalizableTransactions(tokensBridged, posClient, commonClients.hubPoolClient);
+      const canWithdraw = await getFinalizableTransactions(tokensBridged, posClient, relayerClients.hubPoolClient);
       if (canWithdraw.length === 0)
         logger.debug({
           at: "PolygonFinalizer",
           message: "No finalizable messages, will check for retrievals from token bridge",
         });
       for (const event of canWithdraw) {
-        await finalizePolygon(posClient, commonClients.hubPoolClient, event, logger);
+        await finalizePolygon(posClient, relayerClients.hubPoolClient, event, logger);
       }
       for (const l2Token of getL2TokensToFinalize(tokensBridged)) {
-        await retrieveTokenFromMainnetTokenBridger(logger, l2Token, hubSigner, commonClients.hubPoolClient);
+        await retrieveTokenFromMainnetTokenBridger(logger, l2Token, hubSigner, relayerClients.hubPoolClient);
       }
     }
   }
 }
 
-// Refactor below to external index.ts entrypoint
-if (require.main === module) {
-  const config = new RelayerConfig(process.env);
-  const logger = Logger;
-  run(logger, config)
-    .then(() => {
-      process.exit(0);
-    })
-    .catch(async (error) => {
-      console.error(error);
-      logger.error({
-        at: "InfrastructureEntryPoint",
-        message: "There was an error in the main entry point!",
-        error,
-        notificationPath: "across-error",
-      });
-      await delay(5);
-      await run(logger, config);
-    });
+export async function runFinalizer(_logger: winston.Logger) {
+  logger = _logger;
+
+  const config = new FinalizerConfig(process.env);
+  const baseSigner = await getSigner();
+  const hubSigner = baseSigner.connect(getProvider(config.hubPoolChainId));
+  const relayerClients = await constructRelayerClients(logger, config);
+
+  try {
+    await updateRelayerClients(relayerClients);
+
+    logger[startupLogLevel(config)]({ at: "Finalizer#index", message: "Finalizer started ⚰️", config });
+
+    for (;;) {
+      await updateRelayerClients(relayerClients);
+
+      // Validate and dispute pending proposal before proposing a new one
+      if (config.finalizerEnabled) await run(logger, hubSigner, relayerClients, config.finalizerChains);
+      else logger[startupLogLevel(config)]({ at: "Finalizer#index", message: "Finalizer disabled" });
+
+      if (await processEndPollingLoop(logger, "Finalizer", config.pollingDelay)) break;
+    }
+  } catch (error) {
+    if (await processCrash(logger, "Finalizer", config.pollingDelay, error)) process.exit(1);
+    await runFinalizer(logger);
+  }
 }
