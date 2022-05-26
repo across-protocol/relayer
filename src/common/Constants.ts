aa8957eaabdde42f2817f0874ac965b42dba515f
nicholaspai <npai.nyc@gmail.com>
2022-05-26 14:44:12 -0400

    Update Constants.ts

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -13,6 +13,6 @@ export const BUNDLE_END_BLOCK_BUFFERS = {
   1: 20, // At 15s/block, 20 blocks = 5 mins
   10: 150, // At a conservative 0.5 TPS, 300 seconds = 150 transactions. And 1 block per txn.
   137: 150, // At 2s/block, 300 seconds = 150 blocks
-  288: 150, // At a conservative 0.5 TPS, 300 seconds = 150 transactions. And 1 block per txn.
+  288: 10, // At 30s/block, 10 blocks = 5 mins
   42161: 150, // At a conservative 0.5 TPS, 300 seconds = 150 transactions. And 1 block per txn.
 };
