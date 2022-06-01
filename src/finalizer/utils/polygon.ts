7428b188e264f7d546e6925d376f919fabf4d237
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2022-06-01 09:59:45 -0400

    improve: Refactor finalizer (#87)
    * improve: Refactor finalizer + add simulation mode
    
    * lint
    
    * Works
    
    * lint-refactor
    
    * Update ConfigStoreClient.ts
    
    * logs
    
    * Update sendTokens.ts
    
    * Update sendTokens.ts
    
    * Update sendTokens.ts
    
    * fix optimism
    
    * improve logs
    
    * respond to matts comments
    
    * add delay
    
    * lint
    
    * quiet finalizer
    
    * fix

--- src/finalizer/utils/polygon.ts
+++ src/finalizer/utils/polygon.ts
@@ -1,8 +1,30 @@
 import { setProofApi, use, POSClient } from "@maticnetwork/maticjs";
 import { Web3ClientPlugin } from "@maticnetwork/maticjs-ethers";
-import { Contract, ERC20, ethers, getDeployedContract, getProvider, toBN, Wallet, winston } from "../../utils";
+import {
+  Contract,
+  convertFromWei,
+  delay,
+  ERC20,
+  ethers,
+  getDeployedContract,
+  getProvider,
+  groupObjectCountsByProp,
+  toBN,
+  Wallet,
+  winston,
+} from "../../utils";
 import { TokensBridged } from "../../interfaces";
-import { HubPoolClient } from "../../clients";
+import { HubPoolClient, MultiCallerClient } from "../../clients";
+
+// Note!!: This client will only work for PoS tokens. Matic also has Plasma tokens which have a different finalization
+// process entirely.
+
+const CHAIN_ID = 137;
+enum POLYGON_MESSAGE_STATUS {
+  NOT_CHECKPOINTED = "NOT_CHECKPOINTED",
+  ALREADY_EXITED = "ALREADY_EXITED",
+  CAN_EXIT = "CAN_EXIT",
+}
 
 export async function getPosClient(mainnetSigner: Wallet) {
   // Following from https://maticnetwork.github.io/matic.js/docs/pos
@@ -19,7 +41,7 @@ export async function getPosClient(mainnetSigner: Wallet) {
       },
     },
     child: {
-      provider: mainnetSigner.connect(getProvider(137)),
+      provider: mainnetSigner.connect(getProvider(CHAIN_ID)),
       defaultConfig: {
         from: mainnetSigner.address,
       },
@@ -28,6 +50,7 @@ export async function getPosClient(mainnetSigner: Wallet) {
 }
 
 export async function getFinalizableTransactions(
+  logger: winston.Logger,
   tokensBridged: TokensBridged[],
   posClient: POSClient,
   hubPoolClient: HubPoolClient
@@ -35,18 +58,28 @@ export async function getFinalizableTransactions(
   const isCheckpointed = await Promise.all(
     tokensBridged.map((event) => posClient.exitUtil.isCheckPointed(event.transactionHash))
   );
-  const withdrawExitedOrIsNotCheckpointed = await Promise.all(
-    tokensBridged.map((event, i) => {
-      if (!isCheckpointed[i]) return new Promise((resolve) => resolve(true));
+  const exitStatus = await Promise.all(
+    tokensBridged.map(async (event, i) => {
+      if (!isCheckpointed[i]) return { status: POLYGON_MESSAGE_STATUS.NOT_CHECKPOINTED };
       const l1TokenCounterpart = hubPoolClient.getL1TokenCounterpartAtBlock(
-        "137",
+        CHAIN_ID.toString(),
         event.l2TokenAddress,
         hubPoolClient.latestBlockNumber
       );
-      return posClient.erc20(l1TokenCounterpart, true).isWithdrawExited(event.transactionHash);
+      return posClient
+        .erc20(l1TokenCounterpart, true)
+        .isWithdrawExited(event.transactionHash)
+        .then((result) =>
+          result ? { status: POLYGON_MESSAGE_STATUS.ALREADY_EXITED } : { status: POLYGON_MESSAGE_STATUS.CAN_EXIT }
+        );
     })
   );
-  return tokensBridged.filter((_, i) => !withdrawExitedOrIsNotCheckpointed[i]);
+  logger.debug({
+    at: "PolygonFinalizer",
+    message: `Polygon message statuses`,
+    statusesGrouped: groupObjectCountsByProp(exitStatus, (message: { status: string }) => message.status),
+  });
+  return tokensBridged.filter((_, i) => exitStatus[i].status === POLYGON_MESSAGE_STATUS.CAN_EXIT);
 }
 
 export async function finalizePolygon(
@@ -56,7 +89,7 @@ export async function finalizePolygon(
   logger: winston.Logger
 ) {
   const l1TokenCounterpart = hubPoolClient.getL1TokenCounterpartAtBlock(
-    "137",
+    CHAIN_ID.toString(),
     event.l2TokenAddress,
     hubPoolClient.latestBlockNumber
   );
@@ -66,13 +99,25 @@ export async function finalizePolygon(
     l1TokenCounterpart,
     event,
   });
-  const result = await posClient.erc20(l1TokenCounterpart, true).withdrawExitFaster(event.transactionHash);
-  const receipt = await result.getReceipt(); // Wait for confirmation.
-  logger.debug({
-    at: "PolygonFinalizer",
-    message: "Executed exit!",
-    transaction: receipt.transactionHash,
-  });
+  const l1TokenInfo = hubPoolClient.getTokenInfo(1, l1TokenCounterpart);
+  const amountFromWei = convertFromWei(event.amountToReturn.toString(), l1TokenInfo.decimals);
+  try {
+    const txn = await posClient.erc20(l1TokenCounterpart, true).withdrawExitFaster(event.transactionHash);
+    const receipt = await txn.getReceipt();
+    logger.debug({
+      at: "PolygonFinalizer",
+      message: `Finalized Polygon withdrawal for ${amountFromWei} of ${l1TokenInfo.symbol} 🪃`,
+      transactionhash: receipt.transactionHash,
+    });
+    await delay(30);
+  } catch (error) {
+    logger.warn({
+      at: "PolygonFinalizer",
+      message: "Error creating exitTx",
+      error,
+      notificationPath: "across-error",
+    });
+  }
 }
 
 export function getMainnetTokenBridger(mainnetSigner: Wallet) {
@@ -85,7 +130,11 @@ export async function retrieveTokenFromMainnetTokenBridger(
   mainnetSigner: Wallet,
   hubPoolClient: HubPoolClient
 ): Promise<boolean> {
-  const l1Token = hubPoolClient.getL1TokenCounterpartAtBlock("137", l2Token, hubPoolClient.latestBlockNumber);
+  const l1Token = hubPoolClient.getL1TokenCounterpartAtBlock(
+    CHAIN_ID.toString(),
+    l2Token,
+    hubPoolClient.latestBlockNumber
+  );
   const mainnetTokenBridger = getMainnetTokenBridger(mainnetSigner);
   const token = new Contract(l1Token, ERC20.abi, mainnetSigner);
   const l1TokenInfo = hubPoolClient.getTokenInfo(1, l1Token);
@@ -94,6 +143,7 @@ export async function retrieveTokenFromMainnetTokenBridger(
   // WETH is sent to token bridger contract as ETH.
   const ethBalance = await mainnetTokenBridger.provider.getBalance(mainnetTokenBridger.address);
   const balanceToRetrieve = l1TokenInfo.symbol === "WETH" ? ethBalance : balance;
+  const balanceFromWei = ethers.utils.formatUnits(balanceToRetrieve.toString(), l1TokenInfo.decimals);
   if (balanceToRetrieve.eq(toBN(0))) {
     return false;
   } else {
@@ -101,17 +151,23 @@ export async function retrieveTokenFromMainnetTokenBridger(
       at: "PolygonFinalizer",
       message: `Retrieving ${balanceToRetrieve.toString()} ${l1TokenInfo.symbol} from PolygonTokenBridger`,
     });
-    const txn = await mainnetTokenBridger.retrieve(l1Token);
-    const receipt = await txn.wait(); // Wait for confirmation.
-    logger.info({
-      at: "PolygonFinalizer",
-      message: `Retrieved ${ethers.utils.formatUnits(balanceToRetrieve.toString(), l1TokenInfo.decimals)} ${
-        l1TokenInfo.symbol
-      } from PolygonTokenBridger 🏧!`,
-      transaction: receipt.transactionHash,
-      l1Token: l1TokenInfo.symbol,
-      amount: ethers.utils.formatUnits(balanceToRetrieve.toString(), l1TokenInfo.decimals),
-    });
+    try {
+      const txn = await mainnetTokenBridger.retrieve(l1Token);
+      const receipt = await txn.wait();
+      logger.info({
+        at: "PolygonFinalizer",
+        message: `Retrieved ${balanceFromWei} of ${l1TokenInfo.symbol} from PolygonTokenBridger 🪃`,
+        transactionhash: receipt.transactionHash,
+      });
+      await delay(30);
+    } catch (error) {
+      logger.warn({
+        at: "PolygonFinalizer",
+        message: "Error creating retrieveTx",
+        error,
+        notificationPath: "across-error",
+      });
+    }
   }
 }
 
@@ -122,3 +178,13 @@ export function getL2TokensToFinalize(events: TokensBridged[]) {
   }, {});
   return Object.keys(l2TokenCountInBridgeEvents).filter((token) => l2TokenCountInBridgeEvents[token] === true);
 }
+
+export const minimumRootChainAbi = [
+  {
+    inputs: [{ internalType: "bytes", name: "inputData", type: "bytes" }],
+    name: "exit",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
