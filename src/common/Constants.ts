8496a6d13a44b8a485e1b3768f8e33311c75ca09
nicholaspai <npai.nyc@gmail.com>
2022-06-01 09:58:04 -0400

    improve: Increase buffer

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -11,8 +11,8 @@ export const CHAIN_MAX_BLOCK_LOOKBACK = {
 };
 export const BUNDLE_END_BLOCK_BUFFERS = {
   1: 20, // At 15s/block, 20 blocks = 5 mins
-  10: 150, // At a conservative 0.5 TPS, 300 seconds = 150 transactions. And 1 block per txn.
-  137: 150, // At 2s/block, 300 seconds = 150 blocks
+  10: 3000, // At a conservative 10 TPS, 300 seconds = 3000 transactions. And 1 block per txn.
+  137: 300, // At 1s/block, 300 seconds = 300 blocks
   288: 10, // At 30s/block, 10 blocks = 5 mins
-  42161: 150, // At a conservative 0.5 TPS, 300 seconds = 150 transactions. And 1 block per txn.
+  42161: 1500, // At a conservative 5 TPS, 300 seconds = 3000 transactions. And 1 block per txn.
 };
