e167115542528e763bd04b9e2be8c3fb898abae0
Matt Rice <matthewcrice32@gmail.com>
2022-06-08 18:27:08 -0400

    feat: add resilient, quorum provider
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/utils/ProviderUtils.ts
+++ src/utils/ProviderUtils.ts
@@ -1,50 +1,163 @@
 import { ethers, providers } from "ethers";
+import deepEqual from "deep-equal";
 
-const stallTimeout = 15 * 1000;
+const defaultTimeout = 15 * 1000;
 
 function delay(s: number): Promise<void> {
   return new Promise<void>((resolve) => setTimeout(resolve, s * 1000));
 }
 
+function isPromiseFulfulled<T>(
+  promiseSettledResult: PromiseSettledResult<T>
+): promiseSettledResult is PromiseFulfilledResult<T> {
+  return promiseSettledResult.status === "fulfilled";
+}
+
+function formatProviderError(provider: ethers.providers.JsonRpcProvider, rawErrorText: string) {
+  return `Provider ${provider.connection.url} failed with error: ${rawErrorText}`;
+}
+
 class RetryProvider extends ethers.providers.JsonRpcProvider {
-  send(method: string, params: Array<any>): Promise<any> {
-    const delayS = 2;
-    const retries = 2;
-    let promise = super.send(method, params);
-    for (let i = 0; i < retries; i++) {
-      promise = promise.catch(() => delay(delayS).then(() => super.send(method, params)));
+  readonly providers: ethers.providers.JsonRpcProvider[];
+  constructor(
+    params: ConstructorParameters<typeof ethers.providers.JsonRpcProvider>[],
+    readonly nodeQuorumThreshold: number,
+    readonly retries: number,
+    readonly delay: number
+  ) {
+    super();
+    this.providers = params.map((inputs) => new ethers.providers.JsonRpcProvider(...inputs));
+    if (this.nodeQuorumThreshold < 1 || !Number.isInteger(this.nodeQuorumThreshold))
+      throw new Error(
+        `nodeQuorunThreshold cannot be < 1 and must be an integer. Currently set to ${this.nodeQuorumThreshold}`
+      );
+    if (this.retries < 0 || !Number.isInteger(this.retries))
+      throw new Error(`retries cannot be < 0 and must be an integer. Currently set to ${this.retries}`);
+    if (this.delay < 0) throw new Error(`delay cannot be < 0. Currently set to ${this.delay}`);
+    if (this.nodeQuorumThreshold > this.providers.length)
+      throw new Error(
+        `nodeQuorumThreshold (${this.nodeQuorumThreshold}) must be <= the number of providers (${this.providers.length})`
+      );
+  }
+
+  async send(method: string, params: Array<any>): Promise<any> {
+    const requiredProviders = this.providers.slice(0, this.nodeQuorumThreshold - 1);
+    const fallbackProviders = this.providers.slice(this.nodeQuorumThreshold - 1);
+    const errors: [ethers.providers.JsonRpcProvider, string][] = [];
+
+    const tryWithFallback = (
+      provider: ethers.providers.JsonRpcProvider
+    ): Promise<[ethers.providers.JsonRpcProvider, any]> => {
+      return this._trySend(provider, method, params)
+        .then((result): [ethers.providers.JsonRpcProvider, any] => [provider, result])
+        .catch((err) => {
+          // Append the provider and error to the error array.
+          errors.push([provider, err?.stack || err?.toString()]);
+
+          // If there are no new fallback providers to use, terminate the recursion by throwing an error.
+          // Otherwise, we can try to call another provider.
+          if (fallbackProviders.length === 0) {
+            throw new Error("Out of providers to fall back to.");
+          }
+
+          // This line does two things:
+          // 1. Removes a fallback provider from the array so it cannot be used as a fallback for another required
+          // provider.
+          // 2. Recursively calls this method with that provider so it goes through the same try logic as the previous one.
+          return tryWithFallback(fallbackProviders.shift());
+        });
+    };
+
+    const results = await Promise.allSettled(requiredProviders.map(tryWithFallback));
+
+    if (!results.every(isPromiseFulfulled)) {
+      // Format the error so that it's very clear which providers failed and succeeded.
+      const errorTexts = errors.map(([provider, errorText]) => formatProviderError(provider, errorText));
+      const successfulProviderUrls = results.filter(isPromiseFulfulled).map((result) => result.value[0].connection.url);
+      throw new Error(
+        `Not enough providers succeeded. Errors:\n${errorTexts.join("\n")}\n` +
+          `Successful Providers:\n${successfulProviderUrls.join("\n")}`
+      );
     }
 
+    const values = results.map((result) => result.value);
+
+    // Start at element 1 and begin comparing.
+    // If _all_ values are equal, we have hit quorum, so return.
+    if (values.slice(1).every(([, output]) => deepEqual(values[0][1], output))) return values[0][1];
+
+    // Iteratively add results until we have enough matching to return.
+    while (true) {
+      // Grab a new result from our fallback providers.
+      // If we run out of fallback providers, throw a useful error listing all of the successful and failed providers.
+      const [provider, newOutput] = await tryWithFallback(fallbackProviders.shift()).catch(() => {
+        const errorTexts = errors.map(([provider, errorText]) => formatProviderError(provider, errorText));
+        const successfulProviderUrls = values.map(([provider]) => provider.connection.url);
+        throw new Error(
+          "Not enough providers agreed to meet quorum.\n" +
+            "Providers that errored:\n" +
+            `${errorTexts.join("\n")}\n` +
+            "Providers that succeeded, but some failed to match:\n" +
+            successfulProviderUrls.join("\n")
+        );
+      });
+
+      // Filter only the elements that match the new element. If it meets quorum, return.
+      const matchingElements = values.filter(([, output]) => deepEqual(output, newOutput));
+      const totalMatching = matchingElements.length + 1;
+      if (totalMatching >= this.nodeQuorumThreshold) return newOutput;
+
+      // If we didn't return, append the new value to the array and loop back around.
+      values.push([provider, newOutput]);
+    }
+  }
+
+  _trySend(provider: ethers.providers.JsonRpcProvider, method: string, params: Array<any>): Promise<any> {
+    let promise = provider.send(method, params);
+    for (let i = 0; i < this.retries; i++) {
+      promise = promise.catch(() => delay(this.delay).then(() => super.send(method, params)));
+    }
     return promise;
   }
 }
 
-export function getProvider(networkId: number, nodeQuorumThreshold: number = 1) {
-  if (process.env[`RETRY_CONFIG_${networkId}`]) return getFallbackProvider(networkId, nodeQuorumThreshold);
-  else return getStandardProvider(networkId);
+export function getProvider(chainId: number) {
+  const { NODE_RETRIES, NODE_RETRY_DELAY, NODE_QUORUM, NODE_TIMEOUT } = process.env;
+
+  const timeout = Number(process.env[`NODE_TIMEOUT_${chainId}`] || NODE_TIMEOUT || defaultTimeout);
+
+  const constructorArgumentLists = getNodeUrlList(chainId).map((nodeUrl): [{ url: string; timeout: number }] => [
+    { url: nodeUrl, timeout },
+  ]);
+
+  // Default to 2 retries.
+  const retries = Number(process.env[`NODE_RETRIES_${chainId}`] || NODE_RETRIES || "2");
+
+  // Default to a delay of 1 second between retries.
+  const retryDelay = Number(process.env[`NODE_RETRY_DELAY_${chainId}`] || NODE_RETRY_DELAY || "1");
+
+  // Default to a node quorum of 1 node.
+  const nodeQuorumThreshold = Number(process.env[`NODE_QUORUM_${chainId}`] || NODE_QUORUM || "1");
+
+  return new RetryProvider(constructorArgumentLists, nodeQuorumThreshold, retries, retryDelay)
 }
 
-// Create a fallback provider. This provider type enforces that nodeQuorumThreshold of the total retry configs defined
-// all agree in the data they return before returning the data to the provider user. This applies in the context of both
-// events and state reads to ensure consistency and redundancy within the infrastructure. Set the priority to 1.
-// This makes all providers equally weighted and should all be considered. Stall timeout is how long to wait for a node
-// to reply before trying another node in the set provided. This would be the case when the number of nodes is more than
-// the nodeQuorumThreshold (i.e using a 2 of 3 setup).
-export function getFallbackProvider(networkId: number, nodeQuorumThreshold: number) {
-  const nodeUrls = JSON.parse(process.env[`RETRY_CONFIG_${networkId}`]) || [];
-  if (nodeUrls.length == 0) throw new Error(`No RETRY_CONFIG_ for network ${networkId}`);
+export function getNodeUrlList(chainId: number): string[] {
+  const retryConfigKey = `RETRY_CONFIG_${chainId}`;
+  if (process.env[retryConfigKey]) {
+    const nodeUrls = JSON.parse(process.env[retryConfigKey]) || [];
+    if (nodeUrls?.length === 0)
+      throw new Error(`Provided ${retryConfigKey}, but parsing it as json did not result in an array of urls.`);
+    return nodeUrls;
+  }
 
-  return new providers.FallbackProvider(
-    nodeUrls.map((url) => {
-      return { provider: new ethers.providers.JsonRpcProvider(url), priority: 1, stallTimeout, weight: 1 };
-    }),
-    nodeQuorumThreshold
+  const nodeUrlKey = `NODE_URL_${chainId}`;
+
+  if (process.env[nodeUrlKey]) {
+    return [process.env[nodeUrlKey]];
+  }
+
+  throw new Error(
+    `Cannot get node url(s) for ${chainId} because neither ${retryConfigKey} or ${nodeUrlKey} were provided.`
   );
-}
-
-// Returns a normal json RPC provider.
-export function getStandardProvider(networkId: number) {
-  const nodeUrl = process.env[`NODE_URL_${networkId}`];
-  if (!nodeUrl) throw new Error(`No NODE_URL_ for network ${networkId}`);
-  return new RetryProvider({ url: nodeUrl, timeout: stallTimeout });
 }
