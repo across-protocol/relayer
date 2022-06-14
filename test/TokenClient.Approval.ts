bba4bcd5d1fede79ae10a073f239c26b969833e5
nicholaspai <npai.nyc@gmail.com>
2022-06-14 11:34:10 -0400

    lint

--- test/TokenClient.Approval.ts
+++ test/TokenClient.Approval.ts
@@ -52,10 +52,10 @@ describe("TokenClient: Origin token approval", async function () {
     expect(lastSpyLogIncludes(spy, "All token approvals set for non-zero balances")).to.be.true;
 
     // Mint token client account some tokens and check that approvals are sent
-    await erc20_1.connect(owner).mint(owner.address, "1")
-    await erc20_2.connect(owner).mint(owner.address, "1")
-    await weth_1.connect(owner).deposit({ value: "1" })
-    await weth_2.connect(owner).deposit({ value: "1" })
+    await erc20_1.connect(owner).mint(owner.address, "1");
+    await erc20_2.connect(owner).mint(owner.address, "1");
+    await weth_1.connect(owner).deposit({ value: "1" });
+    await weth_2.connect(owner).deposit({ value: "1" });
     await updateAllClients();
     await tokenClient.setOriginTokenApprovals();
 
