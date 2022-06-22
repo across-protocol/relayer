0daf61b847b81d261547825e7dcb1edde91cd1cf
Matt Rice <matthewcrice32@gmail.com>
2022-06-22 02:45:28 -0400

    fix: fix error in eth funding transaction
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/clients/MultiCallerClient.ts
+++ src/clients/MultiCallerClient.ts
@@ -130,7 +130,7 @@ export class MultiCallerClient {
           const result = await runTransaction(
             this.logger,
             transaction.contract,
-            "multicall",
+            transaction.method,
             transaction.args,
             transaction.value
           );
