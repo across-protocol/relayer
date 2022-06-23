8e86396d1a8d44a8df36f8d441fb2c63d85fd1f9
nicholaspai <npai.nyc@gmail.com>
2022-06-22 23:10:58 -0400

    Update FillUtils.ts

--- src/utils/FillUtils.ts
+++ src/utils/FillUtils.ts
@@ -1,6 +1,6 @@
 import { HubPoolClient } from "../clients";
 import { Deposit, Fill, FillsToRefund, FillWithBlock, SpokePoolClientsByChain } from "../interfaces";
-import { BigNumber, assign, getRealizedLpFeeForFills, getRefundForFills, sortEventsDescending, toBN } from "./";
+import { BigNumber, assign, getRealizedLpFeeForFills, getRefundForFills, sortEventsDescending, toBN, sortEventsAscending } from "./";
 import { getBlockRangeForChain } from "../dataworker/DataworkerUtils";
 
 export function getRefundInformationFromFill(
@@ -106,6 +106,7 @@ export function getFillDataForSlowFillFromPreviousRootBundle(
 ) {
   // Find the first fill chronologically for matched deposit for the input fill.
   const firstFillForSameDeposit = allValidFills.find(
+  const firstFillForSameDeposit = sortEventsAscending(allValidFills).find(
     (_fill: FillWithBlock) => filledSameDeposit(_fill, fill) && isFirstFillForDeposit(_fill as Fill)
   );
 
