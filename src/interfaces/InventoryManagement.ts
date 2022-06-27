6710705c304fe9221f03fb1ce31115b991b90447
nicholaspai <npai.nyc@gmail.com>
2022-06-27 17:31:01 -0400

    Update InventoryManagement.ts

--- src/interfaces/InventoryManagement.ts
+++ src/interfaces/InventoryManagement.ts
@@ -7,7 +7,7 @@ export interface InventoryConfig {
         targetPct: BigNumber; // The desired amount of the given token on the L2 chainId.
         thresholdPct: BigNumber; // Threshold, below which, we will execute a rebalance.
         unwrapWethThreshold?: BigNumber; // Threshold for ETH on this chain to trigger WETH unwrapping to maintain ETH balance
-        unwrapWethTarget: BigNumber; // Amount of WETH to unwrap to refill ETH balance. Unused if unwrapWethThreshold
+        unwrapWethTarget?: BigNumber; // Amount of WETH to unwrap to refill ETH balance. Unused if unwrapWethThreshold
         // is undefined.
       };
     };
