42a95b7543150282f51a62ab54351dd8e521146e
nicholaspai <npai.nyc@gmail.com>
2022-06-29 13:05:28 -0400

    Update InventoryClient.ts

--- src/clients/InventoryClient/InventoryClient.ts
+++ src/clients/InventoryClient/InventoryClient.ts
@@ -346,7 +346,9 @@ export class InventoryClient {
       // Ignore chains that don't use ETH as native gas token.
       const chainsToCheckNativeBalance = this.getEnabledChains().filter(
         (chain) =>
-          chain !== 137 && this.inventoryConfig.tokenConfig[l1Weth][chain.toString()]?.unwrapWethThreshold !== undefined
+          chain !== 137 && 
+          this.inventoryConfig.tokenConfig[l1Weth][chain.toString()]?.unwrapWethThreshold !== undefined &&
+          this.inventoryConfig.tokenConfig[l1Weth][chain.toString()]?.unwrapWethTarget !== undefined
       );
       const nativeBalances = Object.fromEntries(
         await Promise.all(
