725b840a569d1fb3f1635dcfcab74955d57deeee
Kevin <kevinh@umaproject.org>
2022-07-11 16:42:10 -1000

    fix

--- src/clients/InventoryClient.ts
+++ src/clients/InventoryClient.ts
@@ -43,7 +43,7 @@ export class InventoryClient {
   getBalanceOnChainForL1Token(chainId: number | string, l1Token: string): BigNumber {
     // Skip if there's no configuration for l1Token on chainId. This is the case for BOBA and BADGER
     // as they're not present on all L2s.
-    if (this.inventoryConfig.tokenConfig[l1Token][chainId] === undefined) {
+    if (this.inventoryConfig.tokenConfig[l1Token][Number(chainId)] === undefined) {
       return toBN(0);
     }
 
