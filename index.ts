b67d9ee50caaa56c1f5eb7c97537e0d04701a056
Paul <108695806+pxrl@users.noreply.github.com>
2022-07-15 23:30:52 +0200

    improve: Add help option
    Ref: ACX-66

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import minimist from "minimist";
-import { delay, Logger, usage, winston } from "./src/utils";
+import { delay, help, Logger, usage, winston } from "./src/utils";
 import { runRelayer } from "./src/relayer";
 import { runDataworker } from "./src/dataworker";
 import { runMonitor } from "./src/monitor";
@@ -13,6 +13,7 @@ export async function run(args: { [k: string]: boolean | string }): Promise<void
   const cmds = {
     dataworker: runDataworker,
     finalizer: runFinalizer,
+    help: help,
     monitor: runMonitor,
     relayer: runRelayer,
   };
@@ -22,7 +23,8 @@ export async function run(args: { [k: string]: boolean | string }): Promise<void
    */
   const cmd = Object.keys(cmds).find((_cmd) => !!args[_cmd]);
 
-  if (cmd === undefined) usage(""); // no return
+  if (cmd === "help") cmds[cmd](); // no return
+  else if (cmd === undefined) usage(""); // no return
   else if (typeof args["wallet"] !== "string" || args["wallet"].length === 0) {
     // todo: Update usage() to provide a hint that wallet is missing/malformed.
     usage(""); // no return
@@ -33,8 +35,9 @@ export async function run(args: { [k: string]: boolean | string }): Promise<void
 
 if (require.main === module) {
   const opts = {
-    boolean: ["dataworker", "finalizer", "monitor", "relayer"],
+    boolean: ["dataworker", "finalizer", "help", "monitor", "relayer"],
     string: ["wallet"],
+    alias: { h: "help" },
     unknown: usage,
   };
   const args = minimist(process.argv.slice(2), opts);
--- src/utils/usage.ts
+++ src/utils/usage.ts
@@ -16,3 +16,29 @@ export function usage(badInput: string = undefined) {
   // eslint-disable-next-line no-process-exit
   process.exit(badInput === undefined ? 0 : 9);
 }
+
+export function help() {
+  const botRepoUrl = "https://github.com/across-protocol/relayer-v2";
+  const relayerDocsUrl = "https://docs.across.to/v2/developers/running-a-relayer";
+  const helpStr = `
+    Across v2 Bot
+
+    Description:
+    \tThis application performs off-chain duties in support of the Across v2 bridge.
+
+    \tBasic sub-functions include:
+    \t  Monitor:    Monitor all relay and bundle events.
+    \t  Relayer:    Perform transaction relays based on eligible SpokePool deposits.
+
+    \tAdvanced sub-functions include:
+    \t  Dataworker: Monitor and produce relay bundles for the Hub Pool.
+    \t  Finalizer:  Finalize and collect canonical bridge transfers.
+
+    Links:
+    \tRepository: ${botRepoUrl}
+    \tRelayer Instructions: ${relayerDocsUrl}
+  `.slice(0, -1);
+
+  console.log(helpStr);
+  usage(); // no return
+}
