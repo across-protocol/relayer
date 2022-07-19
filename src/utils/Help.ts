dbbfc6ce6b096a0425149371a96d9a29f31ccad0
Paul <108695806+pxxrl@users.noreply.github.com>
2022-07-19 21:32:08 +0200

    improve: Add a few more details to the help text
    Intent is to steer the average user away from playing with the
    dataworker and finalizer functionalities, as well as to advise of the
    potential dangers of operating a relayer.
    
    Ref: ACX-66

--- src/utils/Help.ts
+++ src/utils/Help.ts
@@ -24,15 +24,32 @@ export function help() {
     Across v2 Bot
 
     Description:
-    \tThis application performs off-chain duties in support of the Across v2 bridge.
+    \tThis application performs various duties in support of the Across v2
+    \tecosystem. The application implements four key functionalities, divided
+    \tcoarsely between *Basic* and *Advanced* use cases.
 
-    \tBasic sub-functions include:
-    \t  Monitor:    Monitor all relay and bundle events.
-    \t  Relayer:    Perform transaction relays based on eligible SpokePool deposits.
+    \tBasic functionalities are designed for widespread use. These include:
 
-    \tAdvanced sub-functions include:
+    \t  Monitor:    Monitor and report on all relay and bundle events.
+    \t  Relayer:    Perform transaction relays for eligible SpokePool deposits.
+
+    \tOperating a relayer can be a profitable activity and helps to improve the
+    \tspeed and reliability of Across for its users. Note that as with any
+    \tautomated operations involving funds, operating a relayer implies some
+    \tunavoidable level of operational risk. Loss of funds is a possibility.
+    \tBefore operating a relayer, please research and understand the implicit
+    \trisks associated, and ensure this is compatible with your risk profile.
+
+    \tAdvanced functionalities implement the heavy lifting that is required to
+    \tkeep Across operating. These include:
+
     \t  Dataworker: Monitor and produce relay bundles for the Hub Pool.
     \t  Finalizer:  Finalize and collect canonical bridge transfers.
+
+    \tRunning a dataworker or finalizer is predominantly a benevolent activity
+    \tthat will typically not generate a direct profit. Advanced functionalities
+    \tare intended for use for key Across ecosystem stakeholders, rather than
+    \tindividuals.
 
     Links:
     \tRepository: ${botRepoUrl}
