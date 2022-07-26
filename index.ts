587b9039d27cab2fd0775e08934dd78c8628413f
Paul <108695806+pxrl@users.noreply.github.com>
2022-07-26 18:15:26 +0200

    feat: New arg parsing and help/usage functions (#216)
     - Implement new argument parsing with minimist.
     - Add --help (-h) flags for printing basic bot help.
     - Add usage function to print usage information on error and help.
     - Identify a couple of opportunities for future improvements.

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { delay, Logger, winston } from "./src/utils";
-
+import minimist from "minimist";
+import { delay, help, Logger, usage, winston } from "./src/utils";
 import { runRelayer } from "./src/relayer";
 import { runDataworker } from "./src/dataworker";
 import { runMonitor } from "./src/monitor";
@@ -7,17 +7,42 @@ import { runFinalizer } from "./src/finalizer";
 
 let logger: winston.Logger;
 
-export async function run(): Promise<void> {
+export async function run(args: { [k: string]: boolean | string }): Promise<void> {
   logger = Logger;
-  if (process.argv.includes("--relayer")) await runRelayer(Logger);
-  else if (process.argv.includes("--dataworker")) await runDataworker(Logger);
-  else if (process.argv.includes("--finalizer")) await runFinalizer(Logger);
-  else if (process.argv.includes("--monitor")) await runMonitor(Logger);
-  else console.log("Select either relayer, dataworker, or monitor to run!");
+
+  const cmds = {
+    dataworker: runDataworker,
+    finalizer: runFinalizer,
+    help: help,
+    monitor: runMonitor,
+    relayer: runRelayer,
+  };
+
+  /* todo Make the mode of operation an operand, rather than an option.
+   * i.e. ts-node ./index.ts [options] <relayer|...>
+   */
+  const cmd = Object.keys(cmds).find((_cmd) => !!args[_cmd]);
+
+  if (cmd === "help") cmds[cmd](); // no return
+  else if (cmd === undefined) usage(""); // no return
+  else if (typeof args["wallet"] !== "string" || args["wallet"].length === 0) {
+    // todo: Update usage() to provide a hint that wallet is missing/malformed.
+    usage(""); // no return
+  } else {
+    await cmds[cmd](logger);
+  }
 }
 
 if (require.main === module) {
-  run()
+  const opts = {
+    boolean: ["dataworker", "finalizer", "help", "monitor", "relayer"],
+    string: ["wallet", "keys"],
+    alias: { h: "help" },
+    unknown: usage,
+  };
+  const args = minimist(process.argv.slice(2), opts);
+
+  run(args)
     .then(() => {
       // eslint-disable-next-line no-process-exit
       process.exit(0);
@@ -30,6 +55,6 @@ if (require.main === module) {
         notificationPath: "across-error",
       });
       await delay(5);
-      await run();
+      await run(args);
     });
 }
