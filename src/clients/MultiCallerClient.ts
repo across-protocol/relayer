64414cae1838e0a26bce41f27d11ce8f511559d4
Paul <108695806+pxrl@users.noreply.github.com>
2022-08-10 11:27:43 +0000

    improve: Minor correction on markdown generation
    Driveby correction.

--- src/clients/MultiCallerClient.ts
+++ src/clients/MultiCallerClient.ts
@@ -136,7 +136,7 @@ export class MultiCallerClient {
         let mrkdwn = "";
         valueTransactions.forEach((transaction, i) => {
           mrkdwn += "*Transaction excluded from batches because it contained value:*\n";
-          mrkdwn += `  ${i + 1}. ${transaction.message || "0 message"}: ` + `${transaction.mrkdwn || "0 mrkdwn"}\n`;
+          mrkdwn += `  ${i + 1}. ${transaction.message || "0 message"}: ${transaction.mrkdwn || "0 mrkdwn"}\n`;
         });
         Object.keys(chunkedTransactions).forEach((chainId) => {
           mrkdwn += `*Transactions sent in batch on ${getNetworkName(chainId)}:*\n`;
