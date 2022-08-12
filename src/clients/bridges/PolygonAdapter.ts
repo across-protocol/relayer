1ad12d47af75b3d988f5797abbec85c76b85e4a9
Kevin <106104250+kevinuma@users.noreply.github.com>
2022-08-12 11:39:20 -0500

    improve(bridge adapters): Use the same event search config as SpokePoolClients (#246)

--- src/clients/bridges/PolygonAdapter.ts
+++ src/clients/bridges/PolygonAdapter.ts
@@ -67,8 +67,6 @@ const tokenToBridge = {
   }, // MATIC
 };
 
-const firstL1BlockToSearch = 10167767;
-
 const atomicDepositorAddress = "0x26eaf37ee5daf49174637bdcd2f7759a25206c34";
 
 export class PolygonAdapter extends BaseAdapter {
@@ -77,12 +75,12 @@ export class PolygonAdapter extends BaseAdapter {
     readonly spokePoolClients: { [chainId: number]: SpokePoolClient },
     readonly monitoredAddresses: string[]
   ) {
-    super(spokePoolClients, 137, firstL1BlockToSearch);
+    super(spokePoolClients, 137);
   }
 
   // On polygon a bridge transaction looks like a transfer from address(0) to the target.
   async getOutstandingCrossChainTransfers(l1Tokens: string[]) {
-    await this.updateBlockSearchConfig();
+    this.updateSearchConfigs();
     this.log("Getting cross-chain txs", { l1Tokens, l1Config: this.l1SearchConfig, l2Config: this.l2SearchConfig });
 
     const promises = [];
