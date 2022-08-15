73fab4aefcfed60aa3418afb76806dbc93e9a36f
Paul <108695806+pxrl@users.noreply.github.com>
2022-08-15 11:23:30 +0000

    improve(ProfitClient): Test various address formats
    Verify that the ProfitClient isn't mangling the inputs. It should be
    able to handle whatever input format the caller chooses to use.
    
    Ref: ACX-125

--- test/ProfitClient.PriceRetrieval.ts
+++ test/ProfitClient.PriceRetrieval.ts
@@ -8,13 +8,16 @@ let hubPoolClient: MockHubPoolClient, owner: SignerWithAddress, spy: sinon.Sinon
 let profitClient: ProfitClient; // tested
 
 const mainnetTokens: Array<L1Token> = [
-  { symbol: "WETH", address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", decimals: 18 },
-  { symbol: "USDC", address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals: 6 },
+  // Checksummed addresses
+  { symbol: "WETH", address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", decimals: 18 },
+  { symbol: "USDC", address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", decimals: 6 },
+  // Lower-case addresses
   { symbol: "WBTC", address: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", decimals: 8 },
   { symbol: "BOBA", address: "0x42bbfa2e77757c645eeaad1655e0911a7553efbc", decimals: 18 },
-  { symbol: "DAI", address: "0x6b175474e89094c44da98b954eedeac495271d0f", decimals: 18 },
-  { symbol: "UMA", address: "0x04fa0d235c4abf4bcf4787af4cf447de572ef828", decimals: 18 },
-  { symbol: "BADGER", address: "0x3472a5a71965499acd81997a54bba8d852c6e53d", decimals: 18 },
+  // Upper-case addresses
+  { symbol: "DAI", address: "0X6B175474E89094C44DA98B954EEDEAC495271D0F", decimals: 18 },
+  { symbol: "UMA", address: "0X04FA0D235C4ABF4BCF4787AF4CF447DE572EF828", decimals: 18 },
+  { symbol: "BADGER", address: "0X3472A5A71965499ACD81997A54BBA8D852C6E53D", decimals: 18 },
 ];
 
 describe("ProfitClient: Price Retrieval", async function () {
