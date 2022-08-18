1f2932cc0c5042f8c6dee1b20768bf836009020c
Kevin <106104250+kevinuma@users.noreply.github.com>
2022-08-18 11:25:39 -0500

    fix(arbitrum adapter): Correctly filter events by l1Token when multiple share the same gateway contract (#264)

--- src/clients/bridges/ArbitrumAdapter.ts
+++ src/clients/bridges/ArbitrumAdapter.ts
@@ -1,4 +1,12 @@
-import { assign, BigNumber, Contract, runTransaction, spreadEventWithBlockNumber, winston } from "../../utils";
+import {
+  assign,
+  BigNumber,
+  Contract,
+  runTransaction,
+  spreadEvent,
+  spreadEventWithBlockNumber,
+  winston,
+} from "../../utils";
 import { toBN, toWei, paginatedEventQuery, Promise } from "../../utils";
 import { SpokePoolClient } from "../../clients";
 import { BaseAdapter } from "./BaseAdapter";
@@ -8,23 +16,23 @@ import { arbitrumL2Erc20GatewayInterface, arbitrumL1Erc20GatewayInterface } from
 const l1Gateways = {
   "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "0xcEe284F754E854890e311e3280b767F80797180d", // USDC
   "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2": "0xd92023E9d9911199a6711321D1277285e6d4e2db", // WETH
-  "0x6B175474E89094C44Da98b954EedeAC495271d0F": "0xd3b5b60020504bc3489d6949d545893982ba3011", // DAI
-  "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599": "0xa3a7b6f88361f48403514059f1f16c8e78d60eec", // WBTC
+  "0x6B175474E89094C44Da98b954EedeAC495271d0F": "0xD3B5b60020504bc3489D6949d545893982BA3011", // DAI
+  "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599": "0xa3A7B6F88361F48403514059F1F16C8E78d60EeC", // WBTC
   "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828": "0xa3A7B6F88361F48403514059F1F16C8E78d60EeC", // UMA
   "0x3472A5A71965499acd81997a54BBA8D852C6E53d": "0xa3A7B6F88361F48403514059F1F16C8E78d60EeC", // BADGER
   "0xba100000625a3754423978a60c9317c58a424e3D": "0xa3A7B6F88361F48403514059F1F16C8E78d60EeC", // BAL
 };
 
-const l1GatewayRouter = "0x72ce9c846789fdb6fc1f34ac4ad25dd9ef7031ef";
+const l1GatewayRouter = "0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef";
 
 const l2Gateways = {
   "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "0x096760F208390250649E3e8763348E783AEF5562", // USDC
   "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2": "0x6c411aD3E74De3E7Bd422b94A27770f5B86C623B", // WETH
-  "0x6B175474E89094C44Da98b954EedeAC495271d0F": "0x467194771dae2967aef3ecbedd3bf9a310c76c65", // DAI
-  "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599": "0x09e9222e96e7b4ae2a407b98d48e330053351eee", // WBTC
-  "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828": "0x09e9222e96e7b4ae2a407b98d48e330053351eee", // UMA
-  "0x3472A5A71965499acd81997a54BBA8D852C6E53d": "0x09e9222e96e7b4ae2a407b98d48e330053351eee", // BADGER
-  "0xba100000625a3754423978a60c9317c58a424e3D": "0x09e9222e96e7b4ae2a407b98d48e330053351eee", // BAL
+  "0x6B175474E89094C44Da98b954EedeAC495271d0F": "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65", // DAI
+  "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599": "0x09e9222E96E7B4AE2a407B98d48e330053351EEe", // WBTC
+  "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828": "0x09e9222E96E7B4AE2a407B98d48e330053351EEe", // UMA
+  "0x3472A5A71965499acd81997a54BBA8D852C6E53d": "0x09e9222E96E7B4AE2a407B98d48e330053351EEe", // BADGER
+  "0xba100000625a3754423978a60c9317c58a424e3D": "0x09e9222E96E7B4AE2a407B98d48e330053351EEe", // BAL
 };
 
 // TODO: replace these numbers using the arbitrum SDK. these are bad values that mean we will over pay but transactions
@@ -62,10 +70,16 @@ export class ArbitrumAdapter extends BaseAdapter {
 
         const l1Bridge = this.getL1Bridge(l1Token);
         const l2Bridge = this.getL2Bridge(l1Token);
-        const searchFilter = [undefined, monitoredAddress];
+        // l1Token is not an indexed field on deposit events in L1 but is on finalization events on Arb.
+        // This unfortunately leads to fetching of all deposit events for all tokens multiple times, one per l1Token.
+        // There's likely not much we can do here as the deposit events don't have l1Token as an indexed field.
+        // https://github.com/OffchainLabs/arbitrum/blob/master/packages/arb-bridge-peripherals/contracts/tokenbridge/ethereum/gateway/L1ArbitrumGateway.sol#L51
+        const l1SearchFilter = [undefined, monitoredAddress];
+        // https://github.com/OffchainLabs/arbitrum/blob/d75568fa70919364cf56463038c57c96d1ca8cda/packages/arb-bridge-peripherals/contracts/tokenbridge/arbitrum/gateway/L2ArbitrumGateway.sol#L40
+        const l2SearchFilter = [l1Token, monitoredAddress, undefined];
         promises.push(
-          paginatedEventQuery(l1Bridge, l1Bridge.filters.DepositInitiated(...searchFilter), this.l1SearchConfig),
-          paginatedEventQuery(l2Bridge, l2Bridge.filters.DepositFinalized(...searchFilter), this.l2SearchConfig)
+          paginatedEventQuery(l1Bridge, l1Bridge.filters.DepositInitiated(...l1SearchFilter), this.l1SearchConfig),
+          paginatedEventQuery(l2Bridge, l2Bridge.filters.DepositFinalized(...l2SearchFilter), this.l2SearchConfig)
         );
         validTokens.push(l1Token);
       }
@@ -91,7 +105,10 @@ export class ArbitrumAdapter extends BaseAdapter {
       // l2DepositFinalizedEvents state from the BaseAdapter.
       eventsToProcess.forEach((result, index) => {
         const l1Token = validTokens[Math.floor(index / 2)];
-        const events = result.map((event) => {
+        // l1Token is not an indexed field on Aribtrum gateway's deposit events, so these events are for all tokens.
+        // Therefore, we need to filter unrelated deposits of other tokens.
+        const filteredEvents = result.filter((event) => spreadEvent(event)["l1Token"] === l1Token);
+        const events = filteredEvents.map((event) => {
           const eventSpread = spreadEventWithBlockNumber(event);
           return {
             amount: eventSpread[index % 2 === 0 ? "_amount" : "amount"],
