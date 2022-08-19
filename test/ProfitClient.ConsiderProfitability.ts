ea87a2ed02ede4ffe4e3006ec0d1f1a30040dca3
Kevin <kevinh@umaproject.org>
2022-08-19 13:13:43 -0500

    Comments

--- test/ProfitClient.ConsiderProfitability.ts
+++ test/ProfitClient.ConsiderProfitability.ts
@@ -23,8 +23,8 @@ let hubPoolClient: MockHubPoolClient, spyLogger: winston.Logger, profitClient: M
 const mainnetWeth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const mainnetUsdc = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 
-describe("ProfitClient: Consider relay profit", () => {
-  beforeEach(async () => {
+describe("ProfitClient: Consider relay profit", async function () {
+  beforeEach(async function () {
     ({ spyLogger } = createSpyLogger());
 
     hubPoolClient = new MockHubPoolClient(null, null);
@@ -43,7 +43,7 @@ describe("ProfitClient: Consider relay profit", () => {
     });
   });
 
-  it("Considers gas cost when computing protfitability", () => {
+  it("Considers gas cost when computing protfitability", async function () {
     // Create a relay that is clearly profitable. Currency of the relay is WETH with a fill amount of 0.1 WETH, price per
     // WETH set at 3000 and relayer fee % of 0.1% which is a revenue of 0.3.
     // However, since WMATIC costs $0.4, the profit is only 1.2 - 0.3 = $0.9 after gas, which is unprofitable.
@@ -55,7 +55,7 @@ describe("ProfitClient: Consider relay profit", () => {
     expect(profitClient.isFillProfitable(relay, relaySize)).to.be.false;
   });
 
-  it("Handles non-standard token decimals when considering a relay profitability", () => {
+  it("Handles non-standard token decimals when considering a relay profitability", async function () {
     // Create a relay that is clearly profitable. Currency of the relay is USDC with a fill amount of 1000 USDC with a
     // price per USDC of 1 USD. Set the relayer Fee to 10% should make this clearly relayable.
     const relaySize = toBN(1000).mul(toBN(10).pow(6)); // 1000e6 for 1000 USDC.
@@ -85,14 +85,14 @@ describe("ProfitClient: Consider relay profit", () => {
     expect(profitClient.isFillProfitable(marginallyUsdcL2ProfitableRelay, relaySize)).to.be.true;
   });
 
-  it("Considers deposits with relayer fee below min required unprofitable", () => {
+  it("Considers deposits with relayer fee below min required unprofitable", async function () {
     const profitableWethL1Relay = { relayerFeePct: toBNWei("0.01"), destinationChainId: 1 } as Deposit;
     // Full profit discount but with a min fee of 0.03%.
     const profitClientWithMinFee = new MockProfitClient(spyLogger, hubPoolClient, {}, true, [], false, toBNWei("0.03"));
     expect(profitClientWithMinFee.isFillProfitable(profitableWethL1Relay, toBNWei(1))).to.be.false;
   });
 
-  it("Captures unprofitable fills", () => {
+  it("Captures unprofitable fills", async function () {
     const deposit = { relayerFeePct: toBNWei("0.003"), originChainId: 1, depositId: 42 } as Deposit;
     profitClient.captureUnprofitableFill(deposit, toBNWei(1));
     expect(profitClient.getUnprofitableFills()).to.deep.equal({ 1: [{ deposit, fillAmount: toBNWei(1) }] });
--- test/fixtures/Dataworker.Fixture.ts
+++ test/fixtures/Dataworker.Fixture.ts
@@ -175,13 +175,7 @@ export async function setupDataworker(
     [repaymentChainId]: spokePoolClient_3,
     1: spokePoolClient_4,
   };
-  const profitClient = new clients.ProfitClient(
-    spyLogger,
-    hubPoolClient,
-    spokePoolClients,
-    false,
-    [],
-  );
+  const profitClient = new clients.ProfitClient(spyLogger, hubPoolClient, spokePoolClients, false, []);
   const bundleDataClient = new BundleDataClient(
     spyLogger,
     {
