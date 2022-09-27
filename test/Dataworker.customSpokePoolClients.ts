a8b4b9e092b3bc6fb0b304f5cd6e92c4ea4a343d
nicholaspai <npai.nyc@gmail.com>
2022-09-27 12:43:55 -0400

    add unit tests to Dataworker functions
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

---
+++ test/Dataworker.customSpokePoolClients.ts
@@ -0,0 +1,65 @@
+import { lastSpyLogIncludes, lastSpyLogLevel, spyLogIncludes, spyLogLevel } from "./utils";
+import { expect, ethers, Contract } from "./utils";
+import { SpokePoolClient, MultiCallerClient } from "../src/clients";
+import { utf8ToHex } from "./constants";
+import { CHAIN_ID_TEST_LIST } from "./constants";
+import { setupFastDataworker } from "./fixtures/Dataworker.Fixture";
+import { MAX_UINT_VAL } from "../src/utils";
+
+// Tested
+import { Dataworker } from "../src/dataworker/Dataworker";
+
+let spy: sinon.SinonSpy;
+let l1Token_1: Contract, hubPool: Contract;
+
+let dataworkerInstance: Dataworker, multiCallerClient: MultiCallerClient;
+let spokePoolClients: { [chainId: number]: SpokePoolClient };
+
+let updateAllClients: () => Promise<void>;
+
+describe("Dataworker: Using SpokePool clients with short lookback windows", async function () {
+  beforeEach(async function () {
+    ({ hubPool, l1Token_1, dataworkerInstance, spokePoolClients, multiCallerClient, updateAllClients, spy } =
+      await setupFastDataworker(
+        ethers,
+        1 // Use very short lookback
+      ));
+  });
+
+  it("Cannot propose", async function () {
+    await updateAllClients();
+
+    // Attempting to propose a root fails because bundle block range has a start blocks < spoke clients fromBlock's
+    // (because lookback is set to low)
+    await dataworkerInstance.proposeRootBundle(spokePoolClients);
+    expect(lastSpyLogIncludes(spy, "Cannot propose bundle with some chain's startBlock < client start block")).to.be
+      .true;
+    expect(lastSpyLogLevel(spy)).to.equal("warn");
+    expect(multiCallerClient.transactionCount()).to.equal(0);
+  });
+  it("Cannot validate", async function () {
+    await updateAllClients();
+
+    // propose arbitrary bundle
+    await l1Token_1.approve(hubPool.address, MAX_UINT_VAL);
+    await hubPool.proposeRootBundle(
+      Array(CHAIN_ID_TEST_LIST.length).fill(await hubPool.provider.getBlockNumber()),
+      1,
+      utf8ToHex("BogusRoot"),
+      utf8ToHex("BogusRoot"),
+      utf8ToHex("BogusRoot")
+    );
+    await updateAllClients();
+    await dataworkerInstance.validatePendingRootBundle(spokePoolClients);
+    expect(lastSpyLogIncludes(spy, "Skipping dispute")).to.be.true;
+    expect(
+      spyLogIncludes(
+        spy,
+        -2,
+        "Cannot validate bundle with some chain's startBlock < client start block. Set a larger DATAWORKER_FAST_LOOKBACK"
+      )
+    ).to.be.true;
+    expect(spyLogLevel(spy, -2)).to.equal("error");
+    expect(multiCallerClient.transactionCount()).to.equal(0);
+  });
+});
