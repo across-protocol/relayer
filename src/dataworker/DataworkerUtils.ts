19422e3347a461836883fe128a077ef2a6e4de79
nicholaspai <npai.nyc@gmail.com>
2022-09-29 17:08:53 -0400

    Update DataworkerUtils.ts

--- src/dataworker/DataworkerUtils.ts
+++ src/dataworker/DataworkerUtils.ts
@@ -81,9 +81,10 @@ export function blockRangesAreInvalidForSpokeClients(
       Number(chainId),
       chainIdListForBundleEvaluationBlockNumbers
     );
-    // If `earliestMatchedFillBlocks` is not defined for chain, then block range is valid for chain.
+    // If `earliestMatchedFillBlocks` is not defined for chain, then block range is valid for chain. Also, check
+    // that spoke clients earliest queried block is not before the bundle start block.
     return earliestMatchedFillBlocks[chainId] !== undefined
-      ? blockRangeForChain[0] < earliestMatchedFillBlocks[chainId]
+      ? blockRangeForChain[0] < earliestMatchedFillBlocks[chainId] || blockRangeForChain[0] < spokePoolClients[chainId].eventSearchConfig.fromBlock
       : false;
   });
 }
