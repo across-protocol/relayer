4b887638d5476e79182033e0f39073fdd857a66e
nicholaspai <npai.nyc@gmail.com>
2022-09-29 17:14:10 -0400

    Revert "Update DataworkerUtils.ts"
    This reverts commit 19422e3347a461836883fe128a077ef2a6e4de79.

--- src/dataworker/DataworkerUtils.ts
+++ src/dataworker/DataworkerUtils.ts
@@ -81,10 +81,9 @@ export function blockRangesAreInvalidForSpokeClients(
       Number(chainId),
       chainIdListForBundleEvaluationBlockNumbers
     );
-    // If `earliestMatchedFillBlocks` is not defined for chain, then block range is valid for chain. Also, check
-    // that spoke clients earliest queried block is not before the bundle start block.
+    // If `earliestMatchedFillBlocks` is not defined for chain, then block range is valid for chain.
     return earliestMatchedFillBlocks[chainId] !== undefined
-      ? blockRangeForChain[0] < earliestMatchedFillBlocks[chainId] || blockRangeForChain[0] < spokePoolClients[chainId].eventSearchConfig.fromBlock
+      ? blockRangeForChain[0] < earliestMatchedFillBlocks[chainId]
       : false;
   });
 }
