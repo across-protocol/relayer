f54b6951922a7e67a710d502c8e5cb93559db36f
Paul <108695806+pxrl@users.noreply.github.com>
2022-10-20 12:33:57 +0200

    fix(ProfitClient): Additional test fixes
    It seems that there are side-effects when the Relayer tests are run as
    part of the broader set of tests, resulting in additional token
    addresses being queried via the ProfitClient. This seems to be dependent
    on how the tests are started:
    
     $ yarn test
     $ yarn test test/Relayer.*

--- test/mocks/MockProfitClient.ts
+++ test/mocks/MockProfitClient.ts
@@ -16,6 +16,14 @@ export class MockProfitClient extends ProfitClient {
     GAS_TOKEN_BY_CHAIN_ID[1337] = WETH;
 
     this.setTokenPrices({
+      // A collection of various token addresses that are used during test.
+      // Haven't been able to identify where some of these addresses come from...
+      "0xBBeeB24180F4Fd09C7738eB5d09e1067263534Fd": toBNWei(1),
+      "0x3946560dD834D3cE930aDbbE0260FB05ef3B8b92": toBNWei(1),
+      "0xD2D44DeD37881Fe7A98B7bfF2A6eB024171715c6": toBNWei(1),
+      "0xDDF91FE22B61E408107570675f89362947048580": toBNWei(1),
+      "0xd9fEc8238711935D6c8d79Bef2B9546ef23FC046": toBNWei(1),
+      "0x198e48AfAF7b7eb1e6CcFbb14458A83FFc618967": toBNWei(1),
       "0x9c65f85425c619A6cB6D29fF8d57ef696323d188": toBNWei(1),
       "0x5FeaeBfB4439F3516c74939A9D04e95AFE82C4ae": toBNWei(1),
       "0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E": toBNWei(1),
