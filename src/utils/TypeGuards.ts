2bedc2d17d632ef0b82d12eedfe83c189547ff71
Matt Rice <matthewcrice32@gmail.com>
2022-11-02 23:54:15 -0400

    improve: typescript strict in src/dataworker/Dataworker.ts (#351)

--- src/utils/TypeGuards.ts
+++ src/utils/TypeGuards.ts
@@ -13,3 +13,15 @@ export function isPromiseRejected<T>(
 export function isDefined<T>(input: T | null | undefined): input is T {
   return input !== null && input !== undefined;
 }
+
+// This function allows you to test for the key type in an object literal.
+// For instance, this would compile in typescript strict:
+//   const myObj = { a: 1, b: 2, c: 3 } as const;
+//   const d: string = "a";
+//   const myNumber = isKeyOf(d, myObj) ? myObj[d] : 4;
+export function isKeyOf<T extends V, V extends number | string | symbol>(
+  input: V,
+  obj: Record<T, unknown>
+): input is T {
+  return input in obj;
+}
