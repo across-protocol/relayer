6eb4dfe54d37c42d6ba94415ad5eb91c0c84ad4c
Matt Rice <matthewcrice32@gmail.com>
2022-11-22 11:32:31 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/clients/BalanceAllocator.ts
+++ src/clients/BalanceAllocator.ts
@@ -1,4 +1,5 @@
 import { BigNumber, ERC20, ethers, ZERO_ADDRESS } from "../utils";
+import lodash from "lodash";
 
 // This type is used to map used and current balances of different users.
 interface BalanceMap {
@@ -17,37 +18,45 @@ export class BalanceAllocator {
   constructor(readonly providers: { [chainId: number]: ethers.providers.Provider }) {}
 
   // Note: The caller is suggesting that `tokens` for a request are interchangeable.
+  // The tokens whose balances are depleted first should be placed at the front of the array.
   async requestBalanceAllocations(
     requests: { chainId: number; tokens: string[]; holder: string; amount: BigNumber }[]
   ): Promise<boolean> {
+
+    // Validate that all input tokens are unique.
+    const tokens = requests.map(({ tokens }) => tokens.map(token => token.toLowerCase())).flat();
+    if (!lodash.uniq(tokens)) throw new Error("BalanceAllocator::requestBalanceAllocations input tokens not unique!");
+
     // Do all async work up-front to avoid atomicity problems with updating used.
     const requestsWithbalances = await Promise.all(
       requests.map(async (request) => {
-        const balances = await Promise.all(
-          request.tokens.map((token) => this.getBalance(request.chainId, token, request.holder))
-        );
+        const balances = Object.fromEntries(await Promise.all(
+          request.tokens.map(async (token): Promise<[string, BigNumber]> => [token, await this.getBalance(request.chainId, token, request.holder)])
+        ));
 
         // Replace `tokens` property with one of the interchangeable tokens.
         const returnedRequest = {
           ...request,
-          token: request.tokens[0],
-          balance: balances.reduce((acc, curr) => acc.add(curr), BigNumber.from(0)),
+          balances,
         };
-        delete returnedRequest.tokens;
         return returnedRequest;
       })
     );
 
     // Determine if the entire group will be successful.
-    const success = requestsWithbalances.every(({ chainId, token, holder, amount, balance }) => {
-      const used = this.getUsed(chainId, token, holder);
-      return balance.gte(used.add(amount));
+    const success = requestsWithbalances.every(({ chainId, tokens, holder, amount, balances }) => {
+      const availableBalance = tokens.reduce((acc, token) => acc.add(balances[token].sub(this.getUsed(chainId, token, holder))), BigNumber.from(0));
+      return availableBalance.gte(amount);
     });
 
     // If the entire group is successful commit to using these tokens.
     if (success)
-      requestsWithbalances.forEach(({ chainId, token, holder, amount }) =>
-        this.addUsed(chainId, token, holder, amount)
+      requestsWithbalances.forEach(({ chainId, tokens, holder, balances, amount }) =>
+        tokens.forEach(token => {
+          const used = amount.gt(balances[token]) ? balances[token] : amount;
+          this.addUsed(chainId, token, holder, used);
+          amount = amount.sub(used);
+        })
       );
 
     // Return success.
