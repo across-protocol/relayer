fd736f73d78aae06e6f4f34ff9cab8c709829497
nicholaspai <npai.nyc@gmail.com>
2022-12-17 12:16:45 -0600

    Update RelayerClientHelper.ts

--- src/relayer/RelayerClientHelper.ts
+++ src/relayer/RelayerClientHelper.ts
@@ -93,7 +93,7 @@ export async function updateRelayerClients(clients: RelayerClients, config: Rela
 
   // We can update the inventory client at the same time as checking for eth wrapping as these do not depend on each other.
   await Promise.all([
-    clients.acrossApiClient.update(config.ignoreLimits), // Note: AcrossAPIClient requires HubPoolClient to be updated.
+    clients.acrossApiClient.update(config.ignoreLimits),
     clients.inventoryClient.update(),
     clients.inventoryClient.wrapL2EthIfAboveThreshold(),
     clients.inventoryClient.setL1TokenApprovals(),
