e5ef9e7c6e0bc24ed80748f989004f4b2b8129e5
nicholaspai <npai.nyc@gmail.com>
2022-12-27 21:17:14 -0500

    Fix test

--- src/utils/ConfigStoreUtils.ts
+++ src/utils/ConfigStoreUtils.ts
@@ -1,9 +1,11 @@
 import { AcrossConfigStoreClient } from "../clients";
 import { CONFIG_STORE_VERSION } from "../common";
+require("dotenv").config();
 
 export function checkConfigStoreVersion(client: AcrossConfigStoreClient): void {
-  if (client.configStoreVersion !== CONFIG_STORE_VERSION)
+  const localVersion = process.env.CONFIG_STORE_VERSION ?? CONFIG_STORE_VERSION;
+  if (client.configStoreVersion !== localVersion)
     throw new Error(
-      `Config store version mismatch: { on-chain: ${client.configStoreVersion}, local: ${CONFIG_STORE_VERSION} }`
+      `Config store version mismatch: { on-chain: ${client.configStoreVersion}, local: ${localVersion} }`
     );
 }
