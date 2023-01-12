a630d9458f3b6d5cdfc0f5dfc85c5fb06ff0ffef
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2023-01-12 15:49:07 -0500

    improve: Set 90 day expiry on redis keys (#420)
    * improve: Set 90 day expiry on redis keys
    
    RedisClient currently stores two keys:
    - block for timestamp
    - utilizations at block
    
    These are used by ConfigStoreClient to compute a realized lp fee % and by dataworker to construct spoke pool clients' `fromBlocks`. They're also used by the validateRootBundle script which shouldnt ever be used to validate bundles older than 90 days
    
    Expiring these keys after 90 days seems very safe assuming bundle block ranges stay steady
    
    * set default t 0
    
    * fix

--- src/utils/UmaUtils.ts
+++ src/utils/UmaUtils.ts
@@ -1,28 +1,33 @@
-import { Contract, ethers, isEventOlder, sortEventsDescending } from ".";
+import { Contract, ethers, getBlockForTimestamp, getCurrentTime, isEventOlder, sortEventsDescending } from ".";
 import * as uma from "@uma/contracts-node";
-import { HubPoolClient } from "../clients";
+import { AcrossConfigStoreClient } from "../clients";
 import { ProposedRootBundle, SortableEvent } from "../interfaces";
-import { BlockFinder } from "@uma/financial-templates-lib";
 
 export async function getDvmContract(mainnetProvider: ethers.providers.Provider): Promise<Contract> {
   return new Contract(await uma.getVotingAddress(1), uma.getAbi("Voting"), mainnetProvider);
 }
 export async function getDisputedProposal(
   dvm: Contract,
-  hubPoolClient: HubPoolClient,
+  configStoreClient: AcrossConfigStoreClient,
   disputeRequestTimestamp: number,
   disputeRequestBlock?: number
 ): Promise<ProposedRootBundle> {
   const filter = dvm.filters.PriceRequestAdded();
-  const blockFinder = new BlockFinder(dvm.provider.getBlock.bind(dvm.provider));
   const priceRequestBlock =
     disputeRequestBlock !== undefined
       ? disputeRequestBlock
-      : (await blockFinder.getBlockForTimestamp(disputeRequestTimestamp)).number;
+      : await getBlockForTimestamp(
+          configStoreClient.hubPoolClient.chainId,
+          configStoreClient.hubPoolClient.chainId,
+          disputeRequestTimestamp,
+          getCurrentTime(),
+          configStoreClient.blockFinder,
+          configStoreClient.redisClient
+        );
   const disputes = await dvm.queryFilter(filter, priceRequestBlock, priceRequestBlock);
   const dispute = disputes.find((e) => e.args.time.toString() === disputeRequestTimestamp.toString());
   if (!dispute) throw new Error("Could not find PriceRequestAdded event on DVM matching price request time");
-  return sortEventsDescending(hubPoolClient.getProposedRootBundles()).find((e) =>
+  return sortEventsDescending(configStoreClient.hubPoolClient.getProposedRootBundles()).find((e) =>
     isEventOlder(e as SortableEvent, dispute as SortableEvent)
   );
 }
