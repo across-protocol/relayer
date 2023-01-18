45cb5b0c462c630c0e3318e0c1147f6f37d724e0
Paul <108695806+pxrl@users.noreply.github.com>
2023-01-18 23:45:44 +0100

    feat: Add TransactionClient (#426)
    This factors out some of the transaction submission functions from the
    MultiCallerClient implementation. As a result, the MultiCallerClient can
    become more compact and focused on the task of sorting transactions and
    bundling the eligible ones.
    
    More to come.

---
+++ test/mocks/MockTransactionClient.ts
@@ -0,0 +1,44 @@
+import { ethers } from "ethers";
+import { AugmentedTransaction, TransactionClient } from "../../src/clients";
+import { TransactionResponse } from "../../src/utils";
+import { winston } from "../utils";
+
+export const txnClientPassResult = "pass";
+
+export class MockedTransactionClient extends TransactionClient {
+  constructor(logger: winston.Logger) {
+    super(logger);
+  }
+
+  // Forced failures are appended to any list of transaction arguments.
+  txnFailureReason(txn: AugmentedTransaction): string {
+    return txn.args.slice(-1)[0]?.result;
+  }
+
+  txnFailure(txn: AugmentedTransaction): boolean {
+    const result = this.txnFailureReason(txn);
+    return result && result !== txnClientPassResult;
+  }
+
+  protected override async _submit(
+    txn: AugmentedTransaction,
+    nonce: number | null = null
+  ): Promise<TransactionResponse> {
+    if (this.txnFailure(txn)) return Promise.reject(this.txnFailureReason(txn));
+
+    const _nonce = nonce ?? 1;
+    const txnResponse = {
+      chainId: txn.chainId,
+      nonce: _nonce,
+      hash: ethers.utils.id(`Across-v2-${txn.contract.address}-${txn.method}-${_nonce}`),
+    } as TransactionResponse;
+
+    this.logger.debug({
+      at: "MockMultiCallerClient#submitTxns",
+      message: "Transaction submission succeeded!",
+      txn: txnResponse,
+    });
+
+    return txnResponse;
+  }
+}
