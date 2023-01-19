33677af09622c33365fd98442ee2bd7e73f61d8e
Matt Rice <matthewcrice32@gmail.com>
2023-01-19 17:45:16 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- test/EventUtils.ts
+++ test/EventUtils.ts
@@ -12,33 +12,49 @@ describe("EventUtils", async function () {
     expect(() => getPaginatedBlockRanges({ fromBlock: 0, toBlock: 4, maxBlockLookBack: 0 })).to.throw();
 
     // Lookback larger than range returns full range
-    expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 4, maxBlockLookBack: 5 })).to.deep.equal([[0, 4]]);
+    expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 4, maxBlockLookBack: 6 })).to.deep.equal([[0, 4]]);
 
     // Range of 0 returns block with fromBlock == toBlock
-    expect(getPaginatedBlockRanges({ fromBlock: 1, toBlock: 1, maxBlockLookBack: 2 })).to.deep.equal([[1, 1]]);
+    expect(getPaginatedBlockRanges({ fromBlock: 1, toBlock: 1, maxBlockLookBack: 3 })).to.deep.equal([[0, 1]]);
 
     // Lookback of 1
-    expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 4, maxBlockLookBack: 1 })).to.deep.equal([
+    expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 4, maxBlockLookBack: 2 })).to.deep.equal([
       [0, 1],
       [2, 3],
       [4, 4],
     ]);
 
     // Lookback equal to range returns full range
-    expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 4, maxBlockLookBack: 4 })).to.deep.equal([[0, 4]]);
+    expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 4, maxBlockLookBack: 5 })).to.deep.equal([[0, 4]]);
 
     // Range evenly divided by max block lookback:
     expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 100, maxBlockLookBack: 50 })).to.deep.equal([
-      [0, 50],
-      [51, 100],
+      [0, 49],
+      [50, 99],
+      [100, 100]
     ]);
 
     // Range divided by max block lookback with remainder:
     expect(getPaginatedBlockRanges({ fromBlock: 0, toBlock: 100, maxBlockLookBack: 30 })).to.deep.equal([
-      [0, 30],
-      [31, 61],
-      [62, 92],
-      [93, 100],
+      [0, 29],
+      [30, 59],
+      [60, 89],
+      [90, 100],
     ]);
+
+    // Range divided by max block lookback with remainder:
+    expect(getPaginatedBlockRanges({ fromBlock: 172, toBlock: 200, maxBlockLookBack: 12 })).to.deep.equal([
+      [168, 179],
+      [180, 191],
+      [192, 200],
+    ]);
+
+    // Consistent range (for caching purposes) for many sub-ranges
+    for (let i = 0; i < 1000; i++) {
+      const start = Math.floor(Math.random() * 100 + 100);
+      expect(getPaginatedBlockRanges({ fromBlock: start, toBlock: 199, maxBlockLookBack: 100 })).to.deep.equal([
+        [100, 199],
+      ]);
+    }
   });
 });
