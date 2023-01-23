e7faf0f2711edcf9168ca56a51a86c6f788451d6
Matt Rice <matthewcrice32@gmail.com>
2023-01-23 18:11:27 -0500

    feat: cachable block queries (#429)

--- src/utils/EventUtils.ts
+++ src/utils/EventUtils.ts
@@ -49,14 +49,19 @@ export async function paginatedEventQuery(
 
   try {
     return (
-      await Promise.map(
-        paginatedRanges,
-        async ([fromBlock, toBlock]) => {
-          return contract.queryFilter(filter, fromBlock, toBlock);
-        },
-        { concurrency: searchConfig.concurrency | defaultConcurrency }
+      (
+        await Promise.map(
+          paginatedRanges,
+          ([fromBlock, toBlock]) => {
+            return contract.queryFilter(filter, fromBlock, toBlock);
+          },
+          { concurrency: searchConfig.concurrency | defaultConcurrency }
+        )
       )
-    ).flat();
+        .flat()
+        // Filter events by block number because ranges can include blocks that are outside the range specified for caching reasons.
+        .filter((event) => event.blockNumber >= searchConfig.fromBlock && event.blockNumber <= searchConfig.toBlock)
+    );
   } catch (error) {
     if (retryCount < maxRetries) {
       await delay(retrySleepTime);
@@ -65,25 +70,57 @@ export async function paginatedEventQuery(
   }
 }
 
-export function getPaginatedBlockRanges(searchConfig: EventSearchConfig): number[][] {
-  const nextSearchConfig = { fromBlock: searchConfig.fromBlock, toBlock: searchConfig.toBlock };
+/**
+ * @dev Warning: this is a specialized function!! Its functionality is not obvious.
+ * This function attempts to return block ranges to repeat ranges as much as possible. To do so, it may include blocks that
+ * are outside the provided range. The guarantee is that it will always include _at least_ the blocks requested.
+ * @param eventSearchConfig contains fromBlock, toBlock, and maxBlockLookBack.
+ * The range is inclusive, so the results will include events in the fromBlock and in the toBlock.
+ * maxBlockLookback defined the maximum number of blocks to search. Because the range is inclusive, the maximum diff
+ * in the returned pairs is maxBlockLookBack - 1. This is a bit non-intuitive here, but this is meant so that this
+ * parameter more closely aligns with the more commonly understood definition of a max query range that node providers
+ * use.
+ * @returns an array of disjoint fromBlock, toBlock ranges that should be queried. These cover at least the entire
+ * input range, but can include blocks outside of the desired range, so results should be filtered. Results
+ * are ordered from smallest to largest.
+ */
+export function getPaginatedBlockRanges({
+  fromBlock,
+  toBlock,
+  maxBlockLookBack,
+}: EventSearchConfig): [number, number][] {
+  // If the maxBlockLookBack is undefined, we can look back as far as we like. Just return the entire range.
+  if (maxBlockLookBack === undefined) return [[fromBlock, toBlock]];
 
-  if (searchConfig.maxBlockLookBack !== undefined)
-    nextSearchConfig.toBlock = Math.min(searchConfig.toBlock, searchConfig.fromBlock + searchConfig.maxBlockLookBack);
+  // If the fromBlock is > toBlock, then return no ranges.
+  if (fromBlock > toBlock) return [];
 
-  if (searchConfig.maxBlockLookBack === 0) throw new Error("Cannot set maxBlockLookBack = 0");
+  // A maxBlockLookBack of 0 is not allowed.
+  if (maxBlockLookBack <= 0) throw new Error("Cannot set maxBlockLookBack <= 0");
 
-  const returnValue: number[][] = [];
-  while (nextSearchConfig.fromBlock <= searchConfig.toBlock) {
-    returnValue.push([nextSearchConfig.fromBlock, nextSearchConfig.toBlock]);
-    nextSearchConfig.fromBlock = nextSearchConfig.toBlock + 1;
-    if (searchConfig.maxBlockLookBack !== undefined)
-      nextSearchConfig.toBlock = Math.min(
-        searchConfig.toBlock,
-        nextSearchConfig.fromBlock + searchConfig.maxBlockLookBack
-      );
+  // Floor the requestedFromBlock to the nearest smaller multiple of the maxBlockLookBack to enhance caching.
+  // This means that a range like 5 - 45 with a maxBlockLookBack of 20 would look like:
+  // 0-19, 20-39, 40-45.
+  // This allows us to get the max number of repeated node queries. The maximum number of "nonstandard" queries per
+  // call of this function is 1.
+  const flooredStartBlock = Math.floor(fromBlock / maxBlockLookBack) * maxBlockLookBack;
+
+  // Note: range is inclusive, so we have to add one to the number of blocks to query.
+  const iterations = Math.ceil((toBlock + 1 - flooredStartBlock) / maxBlockLookBack);
+
+  const ranges: [number, number][] = [];
+  for (let i = 0; i < iterations; i++) {
+    // Each inner range start is just a multiple of the maxBlockLookBack added to the start block.
+    const innerFromBlock = flooredStartBlock + maxBlockLookBack * i;
+
+    // The innerFromBlock is just the max range from the innerFromBlock or the outer toBlock, whichever is smaller.
+    // The end block should never be larger than the outer toBlock. This is to avoid querying blocks that are in the
+    // future.
+    const innerToBlock = Math.min(innerFromBlock + maxBlockLookBack - 1, toBlock);
+    ranges.push([innerFromBlock, innerToBlock]);
   }
-  return returnValue;
+
+  return ranges;
 }
 
 export function spreadEventWithBlockNumber(event: Event): SortableEvent {
