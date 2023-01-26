824110be05e99f7b6eca3599ab30c2fc7650d224
nicholaspai <npai.nyc@gmail.com>
2023-01-25 21:47:42 -0500

    add unit tests
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- src/clients/SpokePoolClient.ts
+++ src/clients/SpokePoolClient.ts
@@ -41,6 +41,8 @@ const FILL_DEPOSIT_COMPARISON_KEYS = [
   "destinationToken",
 ] as const;
 
+type BINARY_SEARCH_FALLBACK = "LOW" | "HIGH" | "NONE";
+
 export class SpokePoolClient {
   private depositHashes: { [depositHash: string]: DepositWithBlock } = {};
   private depositHashesToFills: { [depositHash: string]: FillWithBlock[] } = {};
@@ -265,9 +267,7 @@ export class SpokePoolClient {
     let high = initHigh;
     do {
       const mid = Math.floor((high + low) / 2);
-      console.log(`Searching between [${low}, ${high}] for deposit ID ${targetDepositId}, mid: ${mid}`);
       const searchedDepositId = await this.spokePool.numberOfDeposits({ blockTag: mid });
-      console.log(`Searched deposit ID: ${searchedDepositId}`);
       if (targetDepositId > searchedDepositId) low = mid + 1;
       else if (targetDepositId < searchedDepositId) high = mid - 1;
       else return mid;
