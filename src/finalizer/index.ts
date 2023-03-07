2b36a45677b839fd1376bb0c13470bd39b8d9e0a
nicholaspai <npai.nyc@gmail.com>
2023-03-07 11:09:26 -0500

    Update index.ts

--- src/finalizer/index.ts
+++ src/finalizer/index.ts
@@ -29,8 +29,6 @@ import {
   Clients,
   ProcessEnv,
   FINALIZER_TOKENBRIDGE_LOOKBACK,
-  DISABLED_CHAINS,
-  CHAIN_ID_LIST_INDICES,
 } from "../common";
 import { Multicall2Ethers__factory } from "@uma/contracts-node";
 import * as optimismSDK from "@eth-optimism/sdk";
