0cf680137016301b23713fe3298c7ed9fafa4305
nicholaspai <npai.nyc@gmail.com>
2023-03-13 16:14:55 -0400

    Update Dataworker.executeSlowRelay.ts

--- test/Dataworker.executeSlowRelay.ts
+++ test/Dataworker.executeSlowRelay.ts
@@ -112,12 +112,7 @@ describe("Dataworker: Execute slow relays", async function () {
     await hubPool.setCurrentTime(Number(await hubPool.getCurrentTime()) + Number(await hubPool.liveness()) + 1);
     await updateAllClients();
     const expectedPoolRebalanceRoot3 = await dataworkerInstance.getExecutablePoolRebalanceLeaves(spokePoolClients);
-    await dataworkerInstance.executePoolRebalanceLeaves(
-      expectedPoolRebalanceRoot3.unexecutedLeaves,
-      expectedPoolRebalanceRoot3.expectedTrees,
-      spokePoolClients,
-      new BalanceAllocator(providers)
-    );
+    expect(expectedPoolRebalanceRoot3).to.be.undefined;
 
     // Should be 1 leaf since this is _only_ a second partial fill repayment and doesn't involve the deposit chain.
     await multiCallerClient.executeTransactionQueue();
