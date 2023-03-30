dd411b522ece19d091397957715bca6235853c26
nicholaspai <npai.nyc@gmail.com>
2023-03-29 23:34:17 -0400

    Revert "fix tests"
    This reverts commit 1ac37a8548977a4513ca0526ac88b8a90d5cfef1.

--- test/MultiCallerClient.ts
+++ test/MultiCallerClient.ts
@@ -1,14 +1,67 @@
 import {
   AugmentedTransaction,
+  MultiCallerClient, // tested
   knownRevertReasons,
   unknownRevertReason,
   unknownRevertReasonMethodsToIgnore,
 } from "../src/clients";
-import {  txnClientPassResult } from "./mocks/MockTransactionClient";
-import { MockedMultiCallerClient } from "./mocks/MockMultiCallerClient";
+import { TransactionSimulationResult } from "../src/utils";
+import { MockedTransactionClient, txnClientPassResult } from "./mocks/MockTransactionClient";
 import { CHAIN_ID_TEST_LIST as chainIds } from "./constants";
-import { createSpyLogger, Contract, expect, randomAddress, winston, toBN, smock, assertPromiseError } from "./utils";
+import {
+  createSpyLogger,
+  Contract,
+  expect,
+  randomAddress,
+  winston,
+  toBN,
+  ethers,
+  smock,
+  assertPromiseError,
+} from "./utils";
 import { getAbi } from "@uma/contracts-node";
+
+class MockedMultiCallerClient extends MultiCallerClient {
+  public ignoredSimulationFailures: TransactionSimulationResult[] = [];
+  public loggedSimulationFailures: TransactionSimulationResult[] = [];
+
+  constructor(logger: winston.Logger, chunkSize: { [chainId: number]: number } = {}, public multisend?: Contract) {
+    super(logger, chunkSize);
+    this.txnClient = new MockedTransactionClient(logger);
+  }
+
+  simulationFailureCount(): number {
+    return this.loggedSimulationFailures.length + this.ignoredSimulationFailures.length;
+  }
+
+  clearSimulationFailures(): void {
+    this.ignoredSimulationFailures = [];
+    this.loggedSimulationFailures = [];
+  }
+
+  private txnCount(txnQueue: { [chainId: number]: AugmentedTransaction[] }): number {
+    return Object.values(txnQueue).reduce((count, txnQueue) => (count += txnQueue.length), 0);
+  }
+
+  async _getMultisender(_: any): Promise<Contract | undefined> {
+    return this.multisend;
+  }
+
+  valueTxnCount(): number {
+    return this.txnCount(this.valueTxns);
+  }
+
+  multiCallTransactionCount(): number {
+    return this.txnCount(this.txns);
+  }
+
+  protected override logSimulationFailures(txns: TransactionSimulationResult[]): void {
+    this.clearSimulationFailures();
+    txns.forEach((txn) => {
+      (this.canIgnoreRevertReason(txn) ? this.ignoredSimulationFailures : this.loggedSimulationFailures).push(txn);
+    });
+  }
+}
 
 // encodeFunctionData is called from within MultiCallerClient.buildMultiCallBundle.
 function encodeFunctionData(_method: string, args: ReadonlyArray<any> = []): string {
