0f557eda1fb2ff402d0226129ca521f2c266f114
nicholaspai <npai.nyc@gmail.com>
2023-04-10 10:15:54 -0400

    wip

--- test/mocks/MockSpokePoolClient.ts
+++ test/mocks/MockSpokePoolClient.ts
@@ -3,20 +3,20 @@ import { SpokePoolClient } from "../../src/clients";
 export class MockedSpokePoolClient extends SpokePoolClient {
   // Each index in the array is assumed to be a block number, with the mocked deposit ID at the block heigh
   // as the value.
-  public depositIds: number[] = [];
+  public depositIdAtBlock: number[] = [];
 
   setDepositIds(_depositIds: number[]) {
-    this.depositIds = [];
+    this.depositIdAtBlock = [];
     if (_depositIds.length === 0) return;
     let lastDepositId = _depositIds[0];
     for (let i = 0; i < _depositIds.length; i++) {
       if (_depositIds[i] < lastDepositId) throw new Error("deposit ID must be equal to or greater than previous");
-      this.depositIds[i] = _depositIds[i];
+      this.depositIdAtBlock[i] = _depositIds[i];
       lastDepositId = _depositIds[i];
     }
   }
 
   async _getDepositIdAtBlock(blockTag: number): Promise<number> {
-    return this.depositIds[blockTag];
+    return this.depositIdAtBlock[blockTag];
   }
 }
