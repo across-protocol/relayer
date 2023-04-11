8d809563910b18c5b941bab4a280d262ca9f56c6
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-04-11 17:31:43 -0400

    feat(UBA): :sparkles: add stub for new UBAFeeCalculator
    This change adds the UBAFeeCalculator client class. This class will eventually work with @pxrl's logic to extract and sequence an array of UBA flows as well as logic that is used to calculate the latest running validated balance.

---
+++ src/utils/UBAFeeCalculator.ts
@@ -0,0 +1,107 @@
+import { BigNumber, ethers } from "ethers";
+import { UbaFlow, UbaRunningRequest, isUbaInflow, isUbaOutflow } from "../interfaces";
+import { toBN } from "./FormattingUtils";
+
+/**
+ * @file UBAFeeCalculator.ts
+ * @description UBA Fee Calculator
+ * @author Across Bots Team
+ */
+export default class UBAFeeCalculator {
+  private readonly provider: ethers.providers.Provider;
+  private blockNumber: number;
+  private lastValidatedRunningBalance?: BigNumber;
+  private recentRequestFlow: UbaFlow[];
+  private runningBalance?: BigNumber;
+
+  constructor(provider: ethers.providers.Provider, initialBlockNumber: number) {
+    this.provider = provider;
+    this.blockNumber = initialBlockNumber;
+  }
+
+  /**
+   * @description Recalculates running balance and from a given block number
+   * @param blockNumber An optional block number to start from - otherwise will start from the lastest blockNumber
+   */
+  public async recalculateRunningBalance(blockNumber?: number): Promise<void> {
+    // Initially set the blockNumber to a new block with either the given input or the last blockNumber available
+    this.blockNumber = blockNumber ?? (await this.provider.getBlockNumber());
+    // Clear the recent request flow
+    this.recentRequestFlow = [];
+    // Resolve the most recent flows
+    this.recentRequestFlow = await this.getRecentRequestFlow();
+    // Recalculate the last validated running balance
+    this.lastValidatedRunningBalance = await this.calculateRunningBalance();
+    // Resolve the running balance
+    this.runningBalance = this.getRunningBalance();
+  }
+
+  /**
+   * @description Get the recent request flow
+   * @param action The action to get the fee for
+   * @returns The relevant fee
+   */
+  public async getUBAFee(action: UbaRunningRequest): Promise<BigNumber> {
+    // Destructure the action
+    const { amount, type } = action;
+    // First verify that both the last validated running balance and the runningBalance is
+    // set to a non-undefined value
+    if (this.lastValidatedRunningBalance === undefined || this.runningBalance === undefined) {
+      await this.recalculateRunningBalance();
+    }
+    // Set the amount to add to the running balance
+    const amountToModify = amount.mul(type === "deposit" ? 1 : -1);
+    // Resolve the new modified balance
+    const modifiedBalance = this.runningBalance.add(amountToModify);
+    // Return the running balance
+    return modifiedBalance;
+  }
+
+  /**
+   * @description Get the running balance
+   * @returns Promise<BigNumber>
+   * @private
+   * @method getRunningBalance
+   * @memberof UBAFeeCalculator
+   */
+  private getRunningBalance(): BigNumber {
+    // Reduce over the recent request flow and add the amount to
+    // the last validated running balance. If there is no last validated running balance
+    // then set the initial value to 0
+    return this.recentRequestFlow.reduce((acc, flow) => {
+      if (isUbaInflow(flow)) {
+        return acc.add(toBN(flow.amount));
+      } else if (isUbaOutflow(flow)) {
+        return acc.sub(toBN(flow.amount));
+      }
+    }, this.lastValidatedRunningBalance ?? toBN(0));
+  }
+
+  /**
+   * @description Get the most recent request flow
+   * @returns Promise<UbaFlow[]>
+   * @todo Implement this function
+   * @private
+   * @async
+   * @method getRecentRequestFlow
+   * @memberof UBAFeeCalculator
+   */
+  private async getRecentRequestFlow(): Promise<UbaFlow[]> {
+    this.blockNumber;
+    throw new Error("Not implemented");
+  }
+  /**
+   * @description Calculate the last validated running balance
+   * @returns Promise<BigNumber>
+   * @todo Implement this function
+   * @private
+   * @async
+   * @method calculateRunningBalance
+   * @memberof UBAFeeCalculator
+   * @returns {BigNumber}
+   */
+  private async calculateRunningBalance(): Promise<BigNumber> {
+    this.blockNumber;
+    throw new Error("Not implemented");
+  }
+}
