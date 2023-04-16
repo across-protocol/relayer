3b378cd20919fb3245b6a1b8f970aeb30d1f7739
nicholaspai <npai.nyc@gmail.com>
2023-04-16 19:53:09 -0400

    Update runRelayerOnGoerli.sh

--- scripts/runRelayerOnGoerli.sh
+++ scripts/runRelayerOnGoerli.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-echo "Running relayer on Goerli. Will fill any deposits for origin chain [1/5] to destination chain [5/1]"
+echo "Running relayer on Goerli. Will fill any deposits for origin chain to destination chain 5"
 echo "HubPool should have set SpokePools for chains 1 and 5 to same Goerli SpokePool."
 if [ "$NODE_URLS_5" != "$NODE_URLS_1" ]; then
   echo "NODE_URLS_5 and NODE_URLS_1 are not the same. Exiting."
@@ -9,7 +9,8 @@ fi
 
 export RELAYER_IGNORE_LIMITS="true"
 export RELAYER_TOKENS='["0x40153DdFAd90C49dbE3F5c9F96f2a5B25ec67461"]'
-export MAX_RELAYER_DEPOSIT_LOOK_BACK=172800
+export RELAYER_DESTINATION_CHAINS='[5]'
+export MAX_RELAYER_DEPOSIT_LOOK_BACK=345600
 export NODE_DISABLE_PROVIDER_CACHING=true
 export IGNORE_PROFITABILITY=true
 export MAX_BLOCK_LOOK_BACK='{ "1": 20000, "10": 10000, "137": 3499, "288": 4990, "42161": 10000, "5": 10000 }'
