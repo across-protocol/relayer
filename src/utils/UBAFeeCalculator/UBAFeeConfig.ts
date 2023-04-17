301920191c74c4782fe99995e8655c9a58ccf4cd
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-04-17 17:22:50 -0400

    feat: include utilities for calculating UBA fee

--- src/utils/UBAFeeCalculator/UBAFeeConfig.ts
+++ src/utils/UBAFeeCalculator/UBAFeeConfig.ts
@@ -1,28 +1,72 @@
 import { BigNumber } from "ethers";
-import UBAPiecewiseFunction from "./UBAPiecewiseFn";
 
 type ChainId = number;
-type Token = string;
+type RouteCombination = string;
+
+type TupleParameter = [BigNumber, BigNumber];
+type DefaultOverrideStructure<PrimaryValue, OverrideKeyType extends string | number | symbol> = {
+  default: PrimaryValue;
+  override?: Record<OverrideKeyType, PrimaryValue>;
+};
 
 /**
  * Defines the configuration needed to calculate the UBA fees
  */
-interface UBAConfig {
+class UBAConfig {
   /**
    * A baseline fee that is applied to all transactions to allow LPs to earn a fee
    */
-  baselineFee: BigNumber;
+  private readonly baselineFee: DefaultOverrideStructure<BigNumber, RouteCombination>;
   /**
    * A record of piecewise functions for each chain that define the utilization fee to ensure that
    * the bridge responds to periods of high utilization
    */
-  utilizationFee: Record<Token, UBAPiecewiseFunction<BigNumber>>;
+  private readonly utilizationFee: BigNumber;
   /**
    * A record of piecewise functions for each chain and token that define the balancing fee to ensure
    * either a positive or negative penalty to bridging a token to a chain that is either under or
    * over utilized
    */
-  balancingFee: Record<ChainId, Record<Token, UBAPiecewiseFunction<BigNumber>>>;
+  private readonly balancingFee: DefaultOverrideStructure<TupleParameter[], ChainId>;
+
+  constructor(
+    baselineFee: DefaultOverrideStructure<BigNumber, RouteCombination>,
+    utilizationFee: BigNumber,
+    balancingFee: DefaultOverrideStructure<TupleParameter[], ChainId>
+  ) {
+    this.baselineFee = baselineFee;
+    this.utilizationFee = utilizationFee;
+    this.balancingFee = balancingFee;
+  }
+
+  /**
+   * @description Get the baseline fee for a given route combination
+   * @param destinationChainId The destination chain id
+   * @param originChainId The origin chain id
+   * @returns The baseline fee
+   */
+  public getBaselineFee(destinationChainId: number, originChainId: number): BigNumber {
+    const routeCombination = `${originChainId}-${destinationChainId}`;
+    return this.baselineFee.override?.[routeCombination] ?? this.baselineFee.default;
+  }
+
+  /**
+   * @description Get the utilization fee for a given chain
+   * @returns The utilization fee
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getUtilizationFee(): BigNumber {
+    return this.utilizationFee;
+  }
+
+  /**
+   * @description Get the balancing fee tuples for a given chain
+   *  @param chainId The chain id
+   * @returns The balancing fee
+   */
+  public getBalancingFeeTuples(chainId: number): TupleParameter[] {
+    return this.balancingFee.override?.[chainId] ?? this.balancingFee.default;
+  }
 }
 
 export default UBAConfig;
---
+++ src/utils/UBAFeeCalculator/UBAFeeUtility.ts
@@ -0,0 +1,168 @@
+import { BigNumber } from "ethers";
+import { toBN } from "../FormattingUtils";
+
+/**
+ * Computes a linear integral over a piecewise function
+ * @param cutoffArray An array of tuples that define the cutoff points and values of the piecewise function
+ * @param index The index of the cutoffArray that we're currently in
+ * @param integralStart Where we're starting the integral
+ * @param integralEnd Where we're ending the integral
+ * @returns The integral of the piecewise function over the given range
+ */
+export function performLinearIntegration(
+  cutoffArray: [BigNumber, BigNumber][],
+  index: number,
+  integralStart: BigNumber,
+  integralEnd: BigNumber
+): BigNumber {
+  const lengthUnderCurve = integralEnd.sub(integralStart);
+  const resolveValue = (index: number): BigNumber => cutoffArray[index][1];
+
+  let feeIntegral = resolveValue(Math.min(index, cutoffArray.length - 1)).mul(lengthUnderCurve);
+  // If we're not in the bounds of this array, we need to perform an additional computation
+  if (index > 0 && index < cutoffArray.length) {
+    const [currCutoff, currValue] = cutoffArray[index];
+    const [prevCutoff, prevValue] = cutoffArray[index - 1];
+    const slope = prevValue.sub(currValue).div(prevCutoff.sub(currCutoff));
+    // We need to compute a discrete integral at this point. We have the following
+    // psuedo code:
+    // fee_integral = (
+    //     fx_i*(integral_end - integral_start) +
+    //     slope*(
+    //         (integral_end**2/2 - x_i*integral_end) -
+    //         (integral_start**2/2 - x_i*integral_start)
+    //     )
+    // )
+    // NOT: we define the variables above [x_i, fx_i ] as [currCutoff, currValue] in the code below
+    feeIntegral = feeIntegral.add(
+      slope.mul(
+        integralEnd
+          .pow(2)
+          .div(2)
+          .sub(currCutoff.mul(integralEnd))
+          .sub(integralStart.pow(2).div(2).sub(currCutoff.mul(integralStart)))
+      )
+    );
+  }
+  return feeIntegral;
+}
+
+export function getBounds(cutoffArray: [BigNumber, BigNumber][], index: number): [BigNumber, BigNumber] {
+  if (index === 0) {
+    return [BigNumber.from(-Infinity), cutoffArray[0][0]];
+  } else if (index >= cutoffArray.length) {
+    return [cutoffArray[cutoffArray.length - 1][0], BigNumber.from(Infinity)];
+  } else {
+    return [cutoffArray[index - 1][0], cutoffArray[index][0]];
+  }
+}
+
+export function getInterval(
+  cutoffArray: [BigNumber, BigNumber][],
+  target: BigNumber
+): [number, [BigNumber, BigNumber]] {
+  let result: [number, [BigNumber, BigNumber]] = [-1, [BigNumber.from(-Infinity), BigNumber.from(Infinity)]];
+  for (let i = 0; i <= cutoffArray.length; i++) {
+    const [lowerBound, upperBound] = getBounds(cutoffArray, i);
+    if (target.gte(lowerBound) && target.lt(upperBound)) {
+      result = [i, [lowerBound, upperBound]];
+      break;
+    }
+  }
+  return result;
+}
+
+export function getRefundBalancingFee(
+  cutoffArray: [BigNumber, BigNumber][],
+  runningBalance: BigNumber,
+  modificationAmount: BigNumber
+): BigNumber {
+  const [balanceIndex, [balanceLowerBound]] = getInterval(cutoffArray, runningBalance);
+  const [balanceLessModificationIndex, [, balanceLessModificationUpperBound]] = getInterval(
+    cutoffArray,
+    runningBalance.sub(modificationAmount)
+  );
+  let totalFee = toBN(0);
+  for (let index = balanceIndex; index >= balanceLessModificationIndex; index--) {
+    let integralStart: BigNumber;
+    let integralEnd: BigNumber;
+
+    // If everything is in the same interval, we can just compute the integral
+    // from balance to balance - modificationAmount
+    if (index === balanceIndex && index === balanceLessModificationIndex) {
+      integralStart = runningBalance;
+      integralEnd = runningBalance.sub(modificationAmount);
+    }
+    // If not in the same interval, then when we are in the balance
+    // interval, start at balance and go to the lb (because balance-modification)
+    // is lower
+    else if (index === balanceIndex) {
+      integralStart = runningBalance;
+      integralEnd = balanceLowerBound;
+    }
+    // If not in the same interval, then when we are in the balance-less-modification
+    // interval, start at balance-less-modification and go to the ub (because balance)
+    // is higher
+    else if (index === balanceLessModificationIndex) {
+      integralStart = balanceLessModificationUpperBound;
+      integralEnd = runningBalance.sub(modificationAmount);
+    }
+    // If not in the same interval, then when we are in the middle interval, start at
+    // the lb and go to the ub
+    else {
+      const [lowerBound, upperBound] = getBounds(cutoffArray, index);
+      integralStart = lowerBound;
+      integralEnd = upperBound;
+    }
+    totalFee = totalFee.add(performLinearIntegration(cutoffArray, index, integralStart, integralEnd));
+  }
+  return totalFee;
+}
+
+export function getDepositBalancingFee(
+  cutoffArray: [BigNumber, BigNumber][],
+  runningBalance: BigNumber,
+  modificationAmount: BigNumber
+): BigNumber {
+  const [balanceIndex, [, balanceUpperBound]] = getInterval(cutoffArray, runningBalance);
+  const [balancePlusModificationIndex, [balancePlusModificationLowerBound]] = getInterval(
+    cutoffArray,
+    runningBalance.add(modificationAmount)
+  );
+  let totalFee = toBN(0);
+
+  // If everything is in the same interval, we can just compute the integral
+  // from balance to balance + modificationAmount
+  for (let index = balanceIndex; balanceIndex < balancePlusModificationIndex; index++) {
+    let integralStart: BigNumber;
+    let integralEnd: BigNumber;
+    // If everything is in the same interval, we can just compute the integral
+    // from balance to balance + modificationAmount (this is the same as the refund case except in reverse)
+    if (index === balanceIndex && index === balancePlusModificationIndex) {
+      integralStart = runningBalance;
+      integralEnd = runningBalance.add(modificationAmount);
+    }
+    // If not in the same interval, then when we are in the balance
+    // interval, start at balance and go to the ub (because balance+modification)
+    // is higher
+    else if (index === balanceIndex) {
+      integralStart = runningBalance;
+      integralEnd = balanceUpperBound;
+    }
+    // If not in the same interval, then when we are in the balance-plus-modification
+    // interval, start at balance-plus-modification and go to the lb
+    else if (index === balancePlusModificationIndex) {
+      integralStart = balancePlusModificationLowerBound;
+      integralEnd = runningBalance.add(modificationAmount);
+    }
+    // Otherwise, integrate over the entire interval
+    else {
+      const [lowerBound, upperBound] = getBounds(cutoffArray, index);
+      integralStart = lowerBound;
+      integralEnd = upperBound;
+    }
+    totalFee = totalFee.add(performLinearIntegration(cutoffArray, index, integralStart, integralEnd));
+  }
+
+  return totalFee;
+}
