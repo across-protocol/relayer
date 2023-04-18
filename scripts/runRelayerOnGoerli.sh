b5b334f259d3d1bb3a0c923be893688b30153192
nicholaspai <npai.nyc@gmail.com>
2023-04-18 11:11:26 -0400

    Fix

--- scripts/runRelayerOnGoerli.sh
+++ scripts/runRelayerOnGoerli.sh
@@ -2,9 +2,12 @@
 #!/bin/bash
 echo "Running relayer on Goerli. Will fill any deposits for origin chain [5/421613] to destination chain [421613/5]"
 
+echo "Killing redis server if it's running to avoid production data corruption"
+redis-cli shutdown
+
 export RELAYER_IGNORE_LIMITS="true"
 export RELAYER_TOKENS='["0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"]'
-export MAX_RELAYER_DEPOSIT_LOOK_BACK=345600
+export MAX_RELAYER_DEPOSIT_LOOK_BACK=7200
 export NODE_DISABLE_PROVIDER_CACHING=true
 export IGNORE_PROFITABILITY=true
 export MAX_BLOCK_LOOK_BACK='{ "1": 20000, "10": 10000, "137": 3499, "288": 4990, "42161": 10000, "5": 10000, "421613": 10000 }'
