04e73ad2e009a9fba9449112f07024dccfc0f483
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-04-20 11:17:31 -0400

    feat: :sparkles: add refresh and manual version of uba calc

--- src/utils/UBAFeeCalculator/index.ts
+++ src/utils/UBAFeeCalculator/index.ts
@@ -1,3 +1,15 @@
-export { default as UBAFeeCalculator } from "./UBAFeeCalculator";
+import { BigNumber } from "ethers";
+import { UbaFlow } from "../../interfaces";
+
 export { default as UBAFeeConfig } from "./UBAFeeConfig";
+export { default as UBAFeeCalculator } from "./UBAFeeCalculator";
+export { default as UBAFeeCalculatorWithRefresh } from "./UBAFeeCalculatorWithRefresh";
 export * as UBAFeeUtility from "./UBAFeeUtility";
+
+export type UBASpokeBalanceType = {
+  chainId: number;
+  blockNumber: number;
+  lastValidatedRunningBalance?: BigNumber;
+  recentRequestFlow: UbaFlow[];
+  runningBalance?: BigNumber;
+};
