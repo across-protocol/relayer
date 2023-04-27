8b1844eec761e46232b1c27eaa57fbc551d06fff
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-04-27 11:09:18 -0400

    feat(UBA): :sparkles: add UBA Fee Calculator

---
+++ src/utils/UBAFeeCalculator/index.ts
@@ -0,0 +1,18 @@
+import { BigNumber } from "ethers";
+import { UbaFlow } from "../../interfaces";
+
+export { default as UBAFeeConfig } from "./UBAFeeConfig";
+export { default as UBAFeeCalculator } from "./UBAFeeCalculator";
+export { default as UBAFeeCalculatorWithRefresh } from "./UBAFeeCalculatorWithRefresh";
+export * as UBAFeeUtility from "./UBAFeeUtility";
+
+export type UBASpokeBalanceType = {
+  chainId: number;
+  blockNumber: number;
+  lastValidatedRunningBalance?: BigNumber;
+  recentRequestFlow: UbaFlow[];
+};
+
+export type UBAFeeResult = { lpFee: BigNumber; relayerFee: BigNumber; totalUBAFee: BigNumber };
+
+export type UBAFlowRange = { startIndex: number; endIndex: number };
