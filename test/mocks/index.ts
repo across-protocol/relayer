f7a2b17ff5ef034e5bacac926503e3347fb5ed9d
Paul <108695806+pxrl@users.noreply.github.com>
2023-05-23 21:55:33 +0200

    chore: Use MockHubPoolClient from sdk-v2

--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -1,6 +1,9 @@
+import { clients } from "@across-protocol/sdk-v2";
+
+export class MockHubPoolClient extends clients.mocks.MockHubPoolClient {};
+
 export * from "./MockBundleDataClient";
 export * from "./MockConfigStoreClient";
-export * from "./MockHubPoolClient";
 export * from "./MockProfitClient";
 export * from "./MockAdapterManager";
 export * from "./MockSpokePoolClient";
