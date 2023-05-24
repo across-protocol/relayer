95b8a560add8a773324e7e2b588014868bb32fc0
Paul <108695806+pxrl@users.noreply.github.com>
2023-05-24 11:12:30 +0200

    Use MockSpokePoolClient from sdk-v2

--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -1,12 +1,12 @@
 import { clients } from "@across-protocol/sdk-v2";
 
 export class MockHubPoolClient extends clients.mocks.MockHubPoolClient {};
+export class MockSpokePoolClient extends clients.mocks.MockSpokePoolClient {};
 
 export * from "./MockBundleDataClient";
 export * from "./MockConfigStoreClient";
 export * from "./MockProfitClient";
 export * from "./MockAdapterManager";
-export * from "./MockSpokePoolClient";
 export * from "./MockTokenClient";
 export * from "./MockTransactionClient";
 export * from "./MockInventoryClient";
