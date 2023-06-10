ee5bbcfcdedcf62b4e59dddf2cb689dde07e4345
nicholaspai <npai.nyc@gmail.com>
2023-06-10 08:29:24 -0400

    fix: Temporarily filter out optimism with L2 txns with multiple withdrawals
    eth-optimism SDK can't handle them. I've raised an issue with their team [here](https://github.com/ethereum-optimism/optimism/issues/5983)

--- src/finalizer/utils/optimism.ts
+++ src/finalizer/utils/optimism.ts
@@ -107,13 +107,13 @@ export async function getOptimismFinalizableMessages(
   ).filter((m) => m !== undefined);
   // Temporarily filter out messages with multiple withdrawals until eth-optimism sdk can handle them:
   // https://github.com/ethereum-optimism/optimism/issues/5983
-  const messagesWithSingleWithdrawals = bedrockMessages.filter(
+  const messagesWithMultipleWithdrawals = crossChainMessages.filter(
     (message, i) =>
       !crossChainMessages.some(
         (otherMessage, j) => i !== j && otherMessage.event.transactionHash === message.event.transactionHash
       )
   );
-  const messageStatuses = await getMessageStatuses(chainId, messagesWithSingleWithdrawals, crossChainMessenger);
+  const messageStatuses = await getMessageStatuses(chainId, messagesWithMultipleWithdrawals, crossChainMessenger);
   logger.debug({
     at: "OptimismFinalizer",
     message: "Optimism message statuses",
