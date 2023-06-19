c0e6c598c9f193b913e572e1b9ba2c6933e95ec0
Paul <108695806+pxrl@users.noreply.github.com>
2023-06-19 18:07:58 +0200

    Revert "Restore cache"
    This reverts commit 6baa0d27f68e780f3805d63c71aca496ff7d5fef.

--- src/clients/HubPoolClient.ts
+++ src/clients/HubPoolClient.ts
@@ -1,8 +1,9 @@
 import { clients } from "@across-protocol/sdk-v2";
 import { ConfigStoreClient } from "./";
 import { Contract } from "ethers";
+import winston from "winston";
+import { MakeOptional, EventSearchConfig } from "../utils";
 import { IGNORED_HUB_EXECUTED_BUNDLES, IGNORED_HUB_PROPOSED_BUNDLES } from "../common";
-import { BigNumber, getRedis, shouldCache, getCurrentTime, setRedisKey, EventSearchConfig, MakeOptional, winston } from "../utils"
 
 export class HubPoolClient extends clients.HubPoolClient {
   constructor(
@@ -17,29 +18,5 @@ export class HubPoolClient extends clients.HubPoolClient {
       ignoredHubExecutedBundles: IGNORED_HUB_EXECUTED_BUNDLES,
       ignoredHubProposedBundles: IGNORED_HUB_PROPOSED_BUNDLES,
     });
-  }
-
-  protected async getUtilization(
-    l1Token: string,
-    blockNumber: number,
-    amount: BigNumber,
-    timestamp: number
-  ): Promise<{ current: BigNumber; post: BigNumber }> {
-    const redisClient = await getRedis(this.logger);
-    if (!redisClient) {
-      return await this.getPostRelayPoolUtilization(l1Token, blockNumber, amount);
-    }
-    const key = `utilization_${l1Token}_${blockNumber}_${amount.toString()}`;
-    const result = await redisClient.get(key);
-    if (result === null) {
-      const { current, post } = await this.getPostRelayPoolUtilization(l1Token, blockNumber, amount);
-      if (shouldCache(getCurrentTime(), timestamp)) {
-        await setRedisKey(key, `${current.toString()},${post.toString()}`, redisClient, 60 * 60 * 24 * 90);
-      }
-      return { current, post };
-    } else {
-      const [current, post] = result.split(",").map(BigNumber.from);
-      return { current, post };
-    }
   }
 }
