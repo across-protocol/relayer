4d0bca9b6c6a35ecd642fe98d944cb6a2695b79f
Paul <108695806+pxrl@users.noreply.github.com>
2023-07-12 16:23:19 +0200

    feat(UBA): Support generating refundRequest() txns (#778)
    This allows the relayer to generate a refundRequest transaction from a
    FillWithBlock event. The maxCount argument is currently pegged to
    UINT256_MAX but will need to be updated so that it's (relatively)
    configurable via the environment.

--- src/clients/UBAClient.ts
+++ src/clients/UBAClient.ts
@@ -1,7 +1,12 @@
 import winston from "winston";
 import { clients, relayFeeCalculator } from "@across-protocol/sdk-v2";
+import { FillWithBlock, RefundRequestWithBlock } from "../interfaces";
 import { HubPoolClient } from "./HubPoolClient";
 import { SpokePoolClient } from "./SpokePoolClient";
+
+const { getFills, getRefundRequests } = clients;
+type SpokePoolEventFilter = clients.SpokePoolEventFilter;
+type SpokePoolFillFilter = clients.SpokePoolFillFilter;
 
 type RelayFeeCalculatorConfig = relayFeeCalculator.RelayFeeCalculatorConfig;
 
@@ -23,5 +28,13 @@ export class UBAClient extends clients.UBAClient {
       maxBundleStates,
       logger
     );
+  }
+
+  async getFills(chainId: number, filter: SpokePoolFillFilter = {}): Promise<FillWithBlock[]> {
+    return getFills(chainId, this.spokePoolClients, filter);
+  }
+
+  async getRefundRequests(chainId: number, filter: SpokePoolEventFilter = {}): Promise<RefundRequestWithBlock[]> {
+    return getRefundRequests(chainId, this.chainIdIndices, this.hubPoolClient, this.spokePoolClients, filter);
   }
 }
