c75c011ec76ce4f1752b6c0353de7affa2cd573e
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-07-24 15:10:50 -0400

    test: add test to resolve timing

--- test/UBA.getUBAFeeConfig.ts
+++ test/UBA.getUBAFeeConfig.ts
@@ -1,15 +1,21 @@
-import { clients, interfaces, utils } from "@across-protocol/sdk-v2";
+import { clients, interfaces } from "@across-protocol/sdk-v2";
 import { MockConfigStoreClient, MockHubPoolClient } from "./mocks";
-import { ethers, createSpyLogger, deployConfigStore, hubPoolFixture, expect } from "./utils";
+import { ethers, createSpyLogger, hubPoolFixture, expect, deployConfigStore } from "./utils";
+import { findLast, range } from "lodash";
+import { L1Token } from "../src/interfaces";
 
 describe("UBAClientUtilities.getUBAConfig", () => {
   let hubPoolClient: MockHubPoolClient;
   const validChainId = 1;
-  let validTokenSymbol: string;
+  const validToken: L1Token = {
+    address: "0x0000000000000000000000000000000000000000",
+    symbol: "DAI",
+    decimals: 18,
+  };
 
   function getUBAFeeConfig(
     chainId: number = validChainId,
-    tokenSymbol: string = validTokenSymbol,
+    tokenSymbol: string = validToken.symbol,
     blockNumber?: number
   ) {
     return clients.getUBAFeeConfig(hubPoolClient, chainId, tokenSymbol, blockNumber);
@@ -20,17 +26,22 @@ describe("UBAClientUtilities.getUBAConfig", () => {
     const { spyLogger } = createSpyLogger();
 
     const { hubPool, dai: l1Token } = await hubPoolFixture();
-    const { configStore } = await deployConfigStore(owner, [l1Token]);
 
-    const configStoreClient = new MockConfigStoreClient(spyLogger, configStore);
+    const { configStore, deploymentBlock } = await deployConfigStore(owner, [l1Token]);
+
+    const configStoreClient = new MockConfigStoreClient(
+      createSpyLogger().spyLogger,
+      configStore,
+      { fromBlock: deploymentBlock },
+      0,
+      undefined,
+      undefined,
+      true
+    );
+
     hubPoolClient = new MockHubPoolClient(spyLogger, hubPool, configStoreClient);
 
-    hubPoolClient.addL1Token({
-      address: "0x",
-      symbol: "DAI",
-      decimals: 18,
-    });
-    validTokenSymbol = hubPoolClient.getL1Tokens()[0].symbol;
+    hubPoolClient.addL1Token(validToken);
   });
 
   describe("Failing Conditions", () => {
@@ -45,23 +56,128 @@ describe("UBAClientUtilities.getUBAConfig", () => {
   });
   describe("Passing Conditions", () => {
     it("should return the correct UBA config", async () => {
-      await hubPoolClient.configStoreClient.update();
-      await hubPoolClient.update();
+      const realisticConfigs: interfaces.UBAOnChainConfigType[] = [
+        {
+          alpha: {
+            default: "10",
+            "1-10": "100000000000000",
+            "1-137": "100000000000000",
+            "1-42161": "100000000000000",
+          },
+          gamma: {
+            default: [
+              ["500000000000000000", "0"],
+              ["650000000000000000", "500000000000000"],
+              ["750000000000000000", "1000000000000000"],
+              ["850000000000000000", "2500000000000000"],
+              ["900000000000000000", "5000000000000000"],
+              ["950000000000000000", "50000000000000000"],
+            ],
+          },
+          omega: { "10": [["0", "0"]], "137": [["0", "0"]], "42161": [["0", "0"]], default: [["0", "0"]] },
+          rebalance: {
+            "10": { threshold_upper: "200000000", target_upper: "100000000" },
+            "137": { threshold_upper: "100000000", target_upper: "0" },
+            "42161": { threshold_upper: "200000000", target_upper: "100000000" },
+          },
+        },
+        {
+          alpha: {
+            default: "20",
+            "1-10": "100000000000000",
+            "1-137": "100000000000000",
+            "1-42161": "100000000000000",
+          },
+          gamma: {
+            default: [
+              ["500000000000000000", "0"],
+              ["650000000000000000", "500000000000000"],
+              ["750000000000000000", "1000000000000000"],
+              ["850000000000000000", "2500000000000000"],
+              ["900000000000000000", "5000000000000000"],
+              ["950000000000000000", "50000000000000000"],
+            ],
+          },
+          omega: { "10": [["0", "0"]], "137": [["0", "0"]], "42161": [["0", "0"]], default: [["0", "0"]] },
+          rebalance: {
+            "10": { threshold_upper: "200000000", target_upper: "100000000" },
+            "137": { threshold_upper: "100000000", target_upper: "0" },
+            "42161": { threshold_upper: "200000000", target_upper: "100000000" },
+          },
+        },
+        {
+          alpha: {
+            default: "30",
+            "1-10": "100000000000000",
+            "1-137": "100000000000000",
+            "1-42161": "100000000000000",
+          },
+          gamma: {
+            default: [
+              ["500000000000000000", "0"],
+              ["650000000000000000", "500000000000000"],
+              ["750000000000000000", "1000000000000000"],
+              ["850000000000000000", "2500000000000000"],
+              ["900000000000000000", "5000000000000000"],
+              ["950000000000000000", "50000000000000000"],
+            ],
+          },
+          omega: { "10": [["0", "0"]], "137": [["0", "0"]], "42161": [["0", "0"]], default: [["0", "0"]] },
+          rebalance: {
+            "10": { threshold_upper: "200000000", target_upper: "100000000" },
+            "137": { threshold_upper: "100000000", target_upper: "0" },
+            "42161": { threshold_upper: "200000000", target_upper: "100000000" },
+          },
+        },
+      ];
 
       const mockedConfigStore = hubPoolClient.configStoreClient as MockConfigStoreClient;
 
-      const rawConfig = (
-        utils.parseJSONWithNumericString(
-          '{"rateModel":{"UBar":"750000000000000000","R0":"21000000000000000","R1":"0","R2":"600000000000000000"},"routeRateModel":{"1-10":{"UBar":"0","R0":"0","R1":"0","R2":"0"},"1-137":{"UBar":"0","R0":"0","R1":"0","R2":"0"},"1-288":{"UBar":"0","R0":"0","R1":"0","R2":"0"},"1-42161":{"UBar":"0","R0":"0","R1":"0","R2":"0"}},"transferThreshold":"0","spokeTargetBalances":{"10":{"threshold":"500000000000000000000","target":"350000000000000000000"},"137":{"target":"0","threshold":"10000000000000000000"},"42161":{"threshold":"600000000000000000000","target":"400000000000000000000"}},"uba":{"incentivePoolAdjustment": {}, "ubaRewardMultiplier": {}, "alpha":{"default":200000000000000,"1-10":0,"1-137":0,"1-42161":0},"gamma":{"default":[[500000000000000000,0],[650000000000000000,500000000000000],[750000000000000000,1000000000000000],[850000000000000000,2500000000000000],[900000000000000000,5000000000000000],[950000000000000000,50000000000000000]]},"omega":{"10":[[0,0]],"137":[[0,0]],"42161":[[0,0]],"default":[[0,0]]},"rebalance":{"10":{"threshold_lower":0,"target_lower":0,"threshold_upper":500000000000000000000,"target_upper":350000000000000000000},"137":{"threshold_lower":0,"target_lower":0,"threshold_upper":25000000000000000000,"target_upper":15000000000000000000},"42161":{"threshold_lower":0,"target_lower":0,"threshold_upper":600000000000000000000,"target_upper":400000000000000000000},"default":{"threshold_lower":0,"target_lower":0,"threshold_upper":0,"target_upper":0}}}}'
-        ) as Record<string, unknown>
-      )["uba"];
+      const blockNumbers: number[] = [];
+      for (const config of realisticConfigs) {
+        const { blockNumber } = mockedConfigStore.updateTokenConfig(
+          validToken.address,
+          JSON.stringify({ uba: config })
+        );
+        blockNumbers.push(blockNumber);
+        // Sleep for 1 second to ensure that the block number is different
+        await new Promise((resolve) => setTimeout(resolve, 500));
+      }
 
-      const config = clients.parseUBAConfigFromOnChain(rawConfig as interfaces.UBAOnChainConfigType);
-      mockedConfigStore.setUBAConfig(config);
+      // Find the starting and ending block range of all the block numbers
+      // Add an additional 10 blocks to the end to ensure that the config is correctly resolved post update
+      const startingBlock = Math.min(...blockNumbers);
+      const endingBlock = Math.max(...blockNumbers) + 10;
 
-      const ubaConfig = getUBAFeeConfig();
+      // Sort the configs by their respective block numbers. This is to ensure that the configs are correctly resolved
+      // in the correct order
+      const sortedConfigs = realisticConfigs
+        .map((config, idx) => ({ config, block: blockNumbers[idx] }))
+        .sort((a, b) => a.block - b.block);
 
-      expect(ubaConfig).to.not.be.undefined;
+      await hubPoolClient.configStoreClient.update();
+      await hubPoolClient.update();
+
+      // Ensure that their length is 3
+      expect(hubPoolClient.configStoreClient.ubaConfigUpdates).to.be.lengthOf(3);
+
+      // Iterate through 30 blocks and ensure that the config is correctly resolved
+      for (const blockNumber of range(startingBlock, endingBlock)) {
+        const correctConfig = findLast(sortedConfigs, ({ block }) => block <= blockNumber);
+        expect(correctConfig).to.not.be.undefined;
+        // Should never happen but just in case
+        if (!correctConfig) {
+          throw new Error("correctConfig is undefined");
+        }
+        const parsedConfig = clients.parseUBAFeeConfig(
+          validChainId,
+          validToken.symbol,
+          clients.parseUBAConfigFromOnChain(correctConfig.config)
+        );
+
+        const resolvedConfig = getUBAFeeConfig(undefined, undefined, blockNumber);
+        expect(resolvedConfig).to.deep.equal(parsedConfig);
+      }
     });
   });
 });
