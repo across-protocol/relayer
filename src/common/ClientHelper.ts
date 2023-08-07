bc9a5bd645718e7fc6990e50d8f61965bd1e973f
nicholaspai <npai.nyc@gmail.com>
2023-08-07 14:39:32 -0400

    Update ClientHelper.ts

--- src/common/ClientHelper.ts
+++ src/common/ClientHelper.ts
@@ -82,13 +82,14 @@ export async function constructSpokePoolClientsWithLookback(
     )
   );
 
+  // @dev: If toBlocks = {} then  construct spoke pool clients that query until the latest blocks.
   return await constructSpokePoolClientsWithStartBlocks(
     logger,
     hubPoolClient,
     config,
     baseSigner,
     fromBlocks,
-    {},
+    config.toBlockOverride,
     enabledChains
   );
 }
