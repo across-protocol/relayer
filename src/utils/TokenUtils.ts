2bbfba5b7451d637f0880a53ed47fffacf995047
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-08-15 09:57:22 -0400

    chore: defined function instead of variable

--- src/utils/TokenUtils.ts
+++ src/utils/TokenUtils.ts
@@ -4,12 +4,12 @@ const { TOKEN_SYMBOLS_MAP, CHAIN_IDs, ZERO_ADDRESS } = constants;
 
 export const { fetchTokenInfo } = utils;
 
-export const getL2TokenAddresses = (l1TokenAddress: string): { [chainId: number]: string } => {
+export function getL2TokenAddresses(l1TokenAddress: string): { [chainId: number]: string } {
   return Object.values(TOKEN_SYMBOLS_MAP).find((details) => {
     return details.addresses[CHAIN_IDs.MAINNET] === l1TokenAddress;
   })?.addresses;
-};
+}
 
-export const getEthAddressForChain = (chainId: number): string => {
+export function getEthAddressForChain(chainId: number): string {
   return CONTRACT_ADDRESSES[chainId]?.eth?.address ?? ZERO_ADDRESS;
 };
