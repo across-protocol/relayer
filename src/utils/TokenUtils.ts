f83e9ee6c9ca4e20c224cbd45601784c220e0996
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-08-16 09:18:24 -0400

    feat: include new markdown on bridged transfers (#884)
    * docs: add documentation to fn
    
    * chore: cleaned up return type
    
    * chore: defined function instead of variable
    
    * feat: fn to resolve decimals from a token symbol
    
    * feat: include new markdown
    
    * improve: set specific chain id
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>

--- src/utils/TokenUtils.ts
+++ src/utils/TokenUtils.ts
@@ -1,15 +1,27 @@
 import { constants, utils } from "@across-protocol/sdk-v2";
 import { CONTRACT_ADDRESSES } from "../common";
+import { BigNumberish, utils as ethersUtils } from "ethers";
 const { TOKEN_SYMBOLS_MAP, CHAIN_IDs, ZERO_ADDRESS } = constants;
 
 export const { fetchTokenInfo } = utils;
 
-export const getL2TokenAddresses = (l1TokenAddress: string): { [chainId: number]: string } => {
+export function getL2TokenAddresses(l1TokenAddress: string): { [chainId: number]: string } {
   return Object.values(TOKEN_SYMBOLS_MAP).find((details) => {
     return details.addresses[CHAIN_IDs.MAINNET] === l1TokenAddress;
   })?.addresses;
-};
+}
 
-export const getEthAddressForChain = (chainId: number): string => {
+export function getEthAddressForChain(chainId: number): string {
   return CONTRACT_ADDRESSES[chainId]?.eth?.address ?? ZERO_ADDRESS;
-};
+}
+
+/**
+ * Format the given amount of tokens to the correct number of decimals for the given token symbol.
+ * @param symbol The token symbol to format the amount for.
+ * @param amount The amount to format.
+ * @returns The formatted amount as a decimal-inclusive string.
+ */
+export function formatUnitsForToken(symbol: string, amount: BigNumberish): string {
+  const decimals = (TOKEN_SYMBOLS_MAP[symbol]?.decimals as number) ?? 18;
+  return ethersUtils.formatUnits(amount, decimals);
+}
