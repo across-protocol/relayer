e93a95d8d65d48bb3debcf6d60cfb41f3d82a280
Paul <108695806+pxrl@users.noreply.github.com>
2023-10-09 17:21:35 +0200

    Nuke local WETH address

--- src/common/ContractAddresses.ts
+++ src/common/ContractAddresses.ts
@@ -424,7 +424,6 @@ export const CONTRACT_ADDRESSES: {
       ],
     },
     weth: {
-      address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
       abi: [
         {
           constant: false,
