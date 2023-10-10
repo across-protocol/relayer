9b82079103b7a4db7a4f693f5765f9893ae36fea
nicholaspai <npai.nyc@gmail.com>
2023-10-10 11:09:42 -0400

    lint
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- src/clients/bridges/AdapterManager.ts
+++ src/clients/bridges/AdapterManager.ts
@@ -84,7 +84,8 @@ export class AdapterManager {
     await utils.mapAsync(
       this.chainsToWrapEtherOn.filter((chainId) => isDefined(this.spokePoolClients[chainId])),
       async (chainId) => {
-        const wrapThreshold = inventoryConfig?.wrapEtherThresholdPerChain?.[chainId] ?? inventoryConfig.wrapEtherThreshold;
+        const wrapThreshold =
+          inventoryConfig?.wrapEtherThresholdPerChain?.[chainId] ?? inventoryConfig.wrapEtherThreshold;
         const wrapTarget = inventoryConfig?.wrapEtherTargetPerChain?.[chainId] ?? inventoryConfig.wrapEtherTarget;
         assert(
           wrapThreshold.gte(wrapTarget),
