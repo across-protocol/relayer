948013fedeea7bf4207951913aacaa4f4304045f
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-10-13 11:57:53 -0400

    feat: ✨ global cache namespace (#993)
    * fix: :ambulance: there was a double stringify as well
    
    * improve: removed extra log throw
    
    * feat: :sparkles: global cache namespace
    
    This PR adds global cache namespacing to all redis keys. This is strictly an additive change by which any key will be prefixed with a new global namespace modifier.
    
    * fix: disable indefinite caching
    
    * chore: resolve compatibility issue
    
    * improve: add ability to not include namespace

--- src/utils/RedisUtils.ts
+++ src/utils/RedisUtils.ts
@@ -1,13 +1,40 @@
-import { assert, toBN, BigNumberish } from "./";
+import { assert, toBN, BigNumberish, isDefined } from "./";
 import { REDIS_URL_DEFAULT } from "../common/Constants";
 import { createClient } from "redis4";
 import winston from "winston";
 import { Deposit, Fill, CachingMechanismInterface } from "../interfaces";
 import dotenv from "dotenv";
 import { RedisCache } from "../caching/RedisCache";
+import { constants } from "@across-protocol/sdk-v2";
 dotenv.config();
 
-export type RedisClient = ReturnType<typeof createClient>;
+const globalNamespace: string | undefined = process.env.GLOBAL_CACHE_NAMESPACE
+  ? String(process.env.GLOBAL_CACHE_NAMESPACE)
+  : undefined;
+
+export type _RedisClient = ReturnType<typeof createClient>;
+
+export class RedisClient {
+  constructor(private readonly client: _RedisClient, private readonly namespace?: string) {}
+
+  async get(key: string): Promise<string | undefined> {
+    return this.client.get(`${this.namespace}:${key}}`);
+  }
+
+  async set(key: string, val: string, expirySeconds = constants.DEFAULT_CACHING_TTL): Promise<void> {
+    const modifiedKey = isDefined(this.namespace) ? `${this.namespace}:${key}` : key;
+    if (expirySeconds > 0) {
+      // EX: Expire key after expirySeconds.
+      await this.client.set(modifiedKey, val, { EX: expirySeconds });
+    } else {
+      await this.client.set(modifiedKey, val);
+    }
+  }
+
+  async disconnect(): Promise<void> {
+    await this.client.disconnect();
+  }
+}
 
 // Avoid caching calls that are recent enough to be affected by things like reorgs.
 // Current time must be >= 15 minutes past the event timestamp for it to be stable enough to cache.
@@ -30,7 +57,7 @@ export async function getRedis(logger?: winston.Logger, url = REDIS_URL): Promis
           dbSize: await redisClient.dbSize(),
         });
       }
-      redisClients[url] = redisClient;
+      redisClients[url] = new RedisClient(redisClient, globalNamespace);
     } catch (err) {
       if (logger) {
         logger.debug({
@@ -58,14 +85,9 @@ export async function setRedisKey(
   key: string,
   val: string,
   redisClient: RedisClient,
-  expirySeconds = 0
+  expirySeconds = constants.DEFAULT_CACHING_TTL
 ): Promise<void> {
-  if (expirySeconds > 0) {
-    // EX: Expire key after expirySeconds.
-    await redisClient.set(key, val, { EX: expirySeconds });
-  } else {
-    await redisClient.set(key, val);
-  }
+  await redisClient.set(key, val, expirySeconds);
 }
 
 export function getRedisDepositKey(depositOrFill: Deposit | Fill): string {
