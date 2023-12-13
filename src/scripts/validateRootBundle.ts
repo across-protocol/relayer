f0995aeefb8cc11c685cccb3d1b6832b71a7267a
Paul <108695806+pxrl@users.noreply.github.com>
2023-12-13 01:36:28 +0100

    fix: Use disputer address in validateRootBundle script
    The validateRootBundle script inherits a lot of core functionality,
    including the TokenClient. The TokenClient automatically seeks to put
    the necessary token approvals in place for handling the ABT token, but
    that's not strictly required for the validateRootBundle script since it
    never actually makes any transactions. Work around this by specifying
    the proposer address when instantiating the VoidSigner instance.

--- src/scripts/validateRootBundle.ts
+++ src/scripts/validateRootBundle.ts
@@ -202,7 +202,10 @@ export async function validate(_logger: winston.Logger, baseSigner: Signer): Pro
 
 export async function run(_logger: winston.Logger): Promise<void> {
   try {
-    const baseSigner = await getSigner({ keyType: "void", cleanEnv: true });
+    // This script inherits the TokenClient, and it attempts to update token approvals.
+    // The disputer bot already has the necessary token approvals in place, so use its address.
+    const roAddress = "0xf7bac63fc7ceacf0589f25454ecf5c2ce904997c";
+    const baseSigner = await getSigner({ keyType: "void", cleanEnv: true, roAddress });
     await validate(_logger, baseSigner);
   } finally {
     await disconnectRedisClients(logger);
