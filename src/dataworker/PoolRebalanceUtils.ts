f95da1125dd8a711cd0c30572fa7568d58a0c50a
Paul <108695806+pxrl@users.noreply.github.com>
2024-02-12 12:21:02 +0100

    Add v3 slow fill leaf generation

--- src/dataworker/PoolRebalanceUtils.ts
+++ src/dataworker/PoolRebalanceUtils.ts
@@ -620,8 +620,6 @@ export function generateMarkdownForRootBundle(
       slowFill.realizedLpFeePct = `${formatFeePct(leaf.relayData.realizedLpFeePct)}%`;
       slowFill.payoutAdjustmentPct = `${formatFeePct(toBN(leaf.payoutAdjustmentPct))}%`;
     } else {
-      assert(sdkUtils.isV3SlowFillLeaf(leaf)); // tsc hinting.
-
       // Scale amounts to 18 decimals for realizedLpFeePct computation.
       const scaleBy = toBN(10).pow(18 - outputTokenDecimals);
       const inputAmount = leaf.relayData.inputAmount.mul(scaleBy);
