63b5bf9a65e61c6c34b853bb803fc077a75c0a16
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2024-02-21 12:24:29 -0500

    feat: Enable V3 in prod (#1210)
    * fix: Update SDK so HubPoolClient.batchCompute handles multiple origin chains
    
    I also added a test for this scenario
    
    * Revert "feat: Enable v3 in prod (#1207)"
    
    This reverts commit 1a648b1a32f18905c153666eaea68d09c4e380da.
    
    * Revert "Revert "feat: Enable v3 in prod (#1207)""
    
    This reverts commit 54991edb6671626fe496a11c8fe29186223d7c8d.
    
    * Update DataworkerUtils.ts
    
    * Update DataworkerUtils.ts
    
    * Revert "Revert "feat: Enable v3 in prod (#1207)""
    
    This reverts commit 54991edb6671626fe496a11c8fe29186223d7c8d.
    
    * Back out version bump
    
    ---------
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>

--- src/dataworker/DataworkerClientHelper.ts
+++ src/dataworker/DataworkerClientHelper.ts
@@ -87,6 +87,10 @@ export async function constructSpokePoolClientsForFastDataworker(
     "EnabledDepositRoute",
     "RelayedRootBundle",
     "ExecutedRelayerRefundRoot",
+    "V3FundsDeposited",
+    "RequestedSpeedUpV3Deposit",
+    "RequestedV3SlowFill",
+    "FilledV3Relay",
   ]);
   return spokePoolClients;
 }
