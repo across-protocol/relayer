88cc4b3155555ec8c8310196593a1682052c5197
Paul <108695806+pxrl@users.noreply.github.com>
2024-03-21 23:59:47 +0100

    Restore comment

--- src/relayer/Relayer.ts
+++ src/relayer/Relayer.ts
@@ -241,7 +241,8 @@ export class Relayer {
   // a) it exceeds the minimum number of required block confirmations,
   // b) the token balance client has enough tokens to fill it,
   // c) the fill is profitable.
-  // If all hold true then complete the fill. Otherwise, if slow fills are enabled, request a slow fill.
+  // If all hold true then complete the fill. If there is insufficient balance to complete the fill and slow fills are
+  // enabled then request a slow fill instead.
   async evaluateFill(
     deposit: V3DepositWithBlock,
     fillStatus: number,
