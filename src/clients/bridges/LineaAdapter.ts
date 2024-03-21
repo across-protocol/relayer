2f6ad64b66f4841007c73c647178458480ddcbe2
nicholaspai <npai.nyc@gmail.com>
2024-03-21 09:37:17 -0400

    Update LineaAdapter.ts

--- src/clients/bridges/LineaAdapter.ts
+++ src/clients/bridges/LineaAdapter.ts
@@ -255,7 +255,7 @@ export class LineaAdapter extends BaseAdapter {
     const isUsdc = this.isUsdc(l1Token);
     const l1Bridge = this.getL1Bridge(l1Token);
     const l1BridgeMethod = isWeth ? "bridgeWethToLinea" : isUsdc ? "depositTo" : "bridgeToken";
-    const l1BridgeArgs = isUsdc || isWeth ? [amount, address] : [l1Token, amount, address];
+    const l1BridgeArgs = isUsdc ? [amount, address] : isWeth ? [address, amount] : [l1Token, amount, address];
     return this._sendTokenToTargetChain(
       l1Token,
       l2Token,
