6612c8b80c32127dfe7c63e111d2481bc04b76dd
Paul <108695806+pxrl@users.noreply.github.com>
2024-04-04 09:59:10 +0200

    feat: Support specifying read-only address for void signers
    This allows the various bot personalities to masquerade any arbitrary
    EOA and will make it significantly easier to test inventory
    configuration as another relayer.
    
    The default read-only address is set to the main RL relayer, so to
    it's now possible to simulate it with the following incantation:
    
      yarn relay --wallet void

--- src/utils/CLIUtils.ts
+++ src/utils/CLIUtils.ts
@@ -1,7 +1,7 @@
 import minimist from "minimist";
 import { Signer } from "ethers";
+import { constants as sdkConsts } from "@across-protocol/sdk-v2";
 import { SignerOptions, getSigner } from "./SignerUtils";
-import { isDefined } from "./TypeGuards";
 
 const keyTypes = ["secret", "mnemonic", "privateKey", "gckms", "void"];
 
@@ -10,10 +10,19 @@ const keyTypes = ["secret", "mnemonic", "privateKey", "gckms", "void"];
  * @returns A signer based on the CLI args.
  */
 export function retrieveSignerFromCLIArgs(): Promise<Signer> {
+  const opts = {
+    string: ["wallet", "keys", "address"],
+    default: {
+      wallet: "secret",
+      address: sdkConsts.DEFAULT_SIMULATED_RELAYER_ADDRESS,
+    },
+  };
+
   // Call into the process' argv to retrieve the CLI args.
-  const args = minimist(process.argv.slice(2));
+  const args = minimist(process.argv.slice(2), opts);
+
   // Resolve the wallet type & verify that it is valid.
-  const keyType = (args.wallet as string) ?? "secret";
+  const keyType = args.wallet ?? "secret";
   if (!isValidKeyType(keyType)) {
     throw new Error(`Unsupported key type (${keyType}); expected one of: ${keyTypes.join(", ")}.`);
   }
@@ -21,9 +30,11 @@ export function retrieveSignerFromCLIArgs(): Promise<Signer> {
   // Build out the signer options to pass to the signer utils.
   const signerOptions: SignerOptions = {
     keyType,
-    gckmsKeys: isDefined(args.keys) ? [args.keys] : [],
+    gckmsKeys: args.keys ?? [],
+    roAddress: args.address,
     cleanEnv: false, // TODO: We don't want to clean the env for now. This will be changed in the future.
   };
+
   // Return the signer.
   return getSigner(signerOptions);
 }
@@ -33,6 +44,6 @@ export function retrieveSignerFromCLIArgs(): Promise<Signer> {
  * @param keyType The key type to check.
  * @returns True if the key type is valid, false otherwise.
  */
-function isValidKeyType(keyType: unknown): keyType is "secret" | "mnemonic" | "privateKey" | "gckms" | "void" {
-  return keyTypes.includes(keyType as string);
+function isValidKeyType(keyType: string): keyType is "secret" | "mnemonic" | "privateKey" | "gckms" | "void" {
+  return keyTypes.includes(keyType);
 }
