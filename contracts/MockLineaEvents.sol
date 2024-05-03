35b5b478bcdd47237b2ed16af119d8539fc8d5d9
nicholaspai <npai.nyc@gmail.com>
2024-05-03 16:34:36 -0400

    lint

--- contracts/MockLineaEvents.sol
+++ contracts/MockLineaEvents.sol
@@ -4,9 +4,7 @@
 pragma solidity ^0.8.0;
 
 contract LineaWethBridge {
-    event MessageClaimed(
-        bytes32 indexed _messageHash
-    );
+    event MessageClaimed(bytes32 indexed _messageHash);
     event MessageSent(
         address indexed _from,
         address indexed _to,
@@ -16,37 +14,15 @@ contract LineaWethBridge {
         bytes _calldata,
         bytes32 indexed _messageHash
     );
-    function emitMessageSent(
-        address from,
-        address to,
-        uint256 value
-    ) external {
-        emit MessageSent(
-            from,
-            to,
-            0,
-            value,
-            0,
-            new bytes(0),
-            bytes32(0)
-        );
+
+    function emitMessageSent(address from, address to, uint256 value) external {
+        emit MessageSent(from, to, 0, value, 0, new bytes(0), bytes32(0));
     }
-    function emitMessageSentWithMessageHash(
-        address from,
-        address to,
-        uint256 value,
-        bytes32 messageHash
-    ) external {
-        emit MessageSent(
-            from,
-            to,
-            0,
-            value,
-            0,
-            new bytes(0),
-            messageHash
-        );
+
+    function emitMessageSentWithMessageHash(address from, address to, uint256 value, bytes32 messageHash) external {
+        emit MessageSent(from, to, 0, value, 0, new bytes(0), messageHash);
     }
+
     function emitMessageClaimed(bytes32 messageHash) external {
         emit MessageClaimed(messageHash);
     }
@@ -56,37 +32,29 @@ contract LineaUsdcBridge {
     event Deposited(address indexed depositor, uint256 amount, address indexed to);
     event ReceivedFromOtherLayer(address indexed recipient, uint256 amount);
 
-    function emitDeposited (
-        address depositor,
-        address to
-    ) external {
+    function emitDeposited(address depositor, address to) external {
         emit Deposited(depositor, 0, to);
     }
 
-    function emitReceivedFromOtherLayer (
-        address recipient
-    ) external {
+    function emitReceivedFromOtherLayer(address recipient) external {
         emit ReceivedFromOtherLayer(recipient, 0);
     }
 }
 
 contract LineaERC20Bridge {
     event BridgingInitiated(address indexed sender, address recipient, address indexed token, uint256 indexed amount);
-    event BridgingFinalized(address indexed nativeToken, address indexed bridgedToken, uint256 indexed amount, address recipient);
+    event BridgingFinalized(
+        address indexed nativeToken,
+        address indexed bridgedToken,
+        uint256 indexed amount,
+        address recipient
+    );
 
-    function emitBridgingInitiated (
-        address sender,
-        address recipient,
-        address token
-    ) external {
+    function emitBridgingInitiated(address sender, address recipient, address token) external {
         emit BridgingInitiated(sender, recipient, token, 0);
     }
 
-    function emitBridgingFinalized (
-        address l1Token,
-        address recipient
-    ) external {
+    function emitBridgingFinalized(address l1Token, address recipient) external {
         emit BridgingFinalized(l1Token, address(0), 0, recipient);
     }
 }
-
--- contracts/MockOpStackEvents.sol
+++ contracts/MockOpStackEvents.sol
@@ -4,12 +4,7 @@
 pragma solidity ^0.8.0;
 
 contract OpStackWethBridge {
-    event ETHDepositInitiated(
-        address indexed _from,
-        address indexed _to,
-        uint256 _amount,
-        bytes _data
-    );
+    event ETHDepositInitiated(address indexed _from, address indexed _to, uint256 _amount, bytes _data);
     event DepositFinalized(
         address indexed _l1Token,
         address indexed _l2Token,
@@ -18,30 +13,12 @@ contract OpStackWethBridge {
         uint256 _amount,
         bytes _data
     );
-    function emitDepositInitiated(
-        address from,
-        address to,
-        uint256 amount
-    ) external {
-        emit ETHDepositInitiated(
-            from,
-            to,
-            amount,
-            new bytes(0)
-        );
+
+    function emitDepositInitiated(address from, address to, uint256 amount) external {
+        emit ETHDepositInitiated(from, to, amount, new bytes(0));
     }
-    function emitDepositFinalized(
-        address from,
-        address to,
-        uint256 amount
-    ) external {
-        emit DepositFinalized(
-            address(0),
-            address(0),
-            from,
-            to,
-            amount,
-            new bytes(0)
-        );
+
+    function emitDepositFinalized(address from, address to, uint256 amount) external {
+        emit DepositFinalized(address(0), address(0), from, to, amount, new bytes(0));
     }
 }
