1378d526db186679d593329b0fcdc2a245c828a0
Paul <108695806+pxrl@users.noreply.github.com>
2024-05-08 22:32:32 +0200

    improve(ClientHelper): Cache SpokePool registration blocks (#1450)
    The registration block for each SpokePool deployment is searched on each
    bot run. These values don't change, so cache them after 1 week.

--- src/common/ClientHelper.ts
+++ src/common/ClientHelper.ts
@@ -41,6 +41,46 @@ async function getSpokePoolSigners(
 }
 
 /**
+ * Resolve the spoke chain activation block for a SpokePool deployment. Prefer sourcing the
+ * block number from cache, but fall back to resolution via RPC queries and cache the result.
+ * @param chainId Chain ID for the SpokePool deployment.
+ * @param hubPoolClient HubPoolClient instance.
+ * @returns SpokePool activation block number on chainId.
+ */
+export async function resolveSpokePoolActivationBlock(
+  chainId: number,
+  hubPoolClient: HubPoolClient,
+  blockNumber?: number
+): Promise<number> {
+  const spokePoolAddr = hubPoolClient.getSpokePoolForBlock(chainId, blockNumber);
+  const key = `relayer_${chainId}_spokepool_${spokePoolAddr}_activation_block`;
+
+  const redis = await getRedisCache(hubPoolClient.logger);
+  if (isDefined(redis)) {
+    const activationBlock = await redis.get(key);
+    const numericActivationBlock = Number(activationBlock);
+    if (Number.isInteger(numericActivationBlock) && numericActivationBlock > 0) {
+      return numericActivationBlock;
+    }
+  }
+
+  // Get the timestamp of the block where the SpokePool was activated on mainnet, and resolve that
+  // to a block number on the SpokePool chain. Use this block as the lower bound for the search.
+  const blockFinder = undefined;
+  const mainnetActivationBlock = hubPoolClient.getSpokePoolActivationBlock(chainId, spokePoolAddr);
+  const { timestamp } = await hubPoolClient.hubPool.provider.getBlock(mainnetActivationBlock);
+  const hints = { lowBlock: getDeploymentBlockNumber("SpokePool", chainId) };
+  const activationBlock = await getBlockForTimestamp(chainId, timestamp, blockFinder, redis, hints);
+
+  const cacheAfter = 5 * 24 * 3600; // 5 days
+  if (isDefined(redis) && getCurrentTime() - timestamp > cacheAfter) {
+    await redis.set(key, activationBlock.toString());
+  }
+
+  return activationBlock;
+}
+
+/**
  * Construct spoke pool clients that query from [latest-lookback, latest]. Clients on chains that are disabled at
  * latest-lookback will be set to undefined.
  * @param baseSigner Signer to set for spoke pool contracts.
@@ -163,16 +203,9 @@ export async function constructSpokePoolClientsWithStartBlocks(
   const spokePoolSigners = await getSpokePoolSigners(baseSigner, enabledChains);
   const spokePools = await Promise.all(
     enabledChains.map(async (chainId) => {
-      // Grab latest spoke pool as of `toBlockOverride[1]`. If `toBlockOverride[1]` is undefined, then grabs current
-      // spoke pool.
-      const latestSpokePool = hubPoolClient.getSpokePoolForBlock(chainId, toBlockOverride[1]);
-      const spokePoolContract = new Contract(latestSpokePool, SpokePool.abi, spokePoolSigners[chainId]);
-      const spokePoolActivationBlock = hubPoolClient.getSpokePoolActivationBlock(chainId, latestSpokePool);
-      const time = (await hubPoolClient.hubPool.provider.getBlock(spokePoolActivationBlock)).timestamp;
-
-      // Improve BlockFinder efficiency by clamping its search space lower bound to the SpokePool deployment block.
-      const hints = { lowBlock: getDeploymentBlockNumber("SpokePool", chainId) };
-      const registrationBlock = await getBlockForTimestamp(chainId, time, blockFinder, redis, hints);
+      const spokePoolAddr = hubPoolClient.getSpokePoolForBlock(chainId, toBlockOverride[1]);
+      const spokePoolContract = SpokePool.connect(spokePoolAddr, spokePoolSigners[chainId]);
+      const registrationBlock = await resolveSpokePoolActivationBlock(chainId, hubPoolClient, toBlockOverride[1]);
       return { chainId, contract: spokePoolContract, registrationBlock };
     })
   );
