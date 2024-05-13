ca03f39ad8b0bf8630ad6ac6c1d5fe6ae80026d8
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2024-05-13 12:22:24 -0400

    improve(finalizer): log finalization strategy (#1516)

--- src/finalizer/index.ts
+++ src/finalizer/index.ts
@@ -461,6 +461,7 @@ export async function runFinalizer(_logger: winston.Logger, baseSigner: Signer):
         message: `Time to loop: ${Math.round((loopEndPostFinalizations - loopStart) / 1000)}s`,
         timeToUpdateSpokeClients: Math.round((loopStartPostSpokePoolUpdates - loopStart) / 1000),
         timeToFinalize: Math.round((loopEndPostFinalizations - loopStartPostSpokePoolUpdates) / 1000),
+        strategy: config.finalizationStrategy,
       });
 
       if (await processEndPollingLoop(logger, "Dataworker", config.pollingDelay)) {
