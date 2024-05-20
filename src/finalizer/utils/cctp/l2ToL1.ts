8f7c36f0cd93d5cdb039f768ed3d8056f32cbf4c
nicholaspai <npai.nyc@gmail.com>
2024-05-19 21:17:38 -0400

    Update l2ToL1.ts

--- src/finalizer/utils/cctp/l2ToL1.ts
+++ src/finalizer/utils/cctp/l2ToL1.ts
@@ -76,7 +76,7 @@ async function resolveRelatedTxnReceipts(
     .filter(
       (bridgeEvent) =>
         bridgeEvent.blockNumber >= latestBlockToFinalize &&
-        bridgeEvent.l2TokenAddress === TOKEN_SYMBOLS_MAP._USDC.addresses[sourceChainId]
+        compareAddressesSimple(bridgeEvent.l2TokenAddress, TOKEN_SYMBOLS_MAP._USDC.addresses[sourceChainId])
     )
     .forEach((bridgeEvent) => uniqueTxnHashes.add(bridgeEvent.transactionHash));
 
