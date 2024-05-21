ccd24d28d1c6881052c2b836ff81389d1afb727f
Bennett <bennett@umaproject.org>
2024-05-21 11:59:53 -0500

    do not prioritize repayment on Mode
    Signed-off-by: Bennett <bennett@umaproject.org>

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -333,5 +333,6 @@ export const RELAYER_DEFAULT_SPOKEPOOL_INDEXER = "./dist/src/libexec/RelayerSpok
 
 export const DEFAULT_ARWEAVE_GATEWAY = { url: "arweave.net", port: 443, protocol: "https" };
 
-// Chains with slow (> 2 day liveness) canonical L2-->L1 bridges.
-export const SLOW_WITHDRAWAL_CHAINS = [CHAIN_IDs.BASE, CHAIN_IDs.ARBITRUM, CHAIN_IDs.OPTIMISM, CHAIN_IDs.MODE];
+// Chains with slow (> 2 day liveness) canonical L2-->L1 bridges that we prioritize taking repayment on.
+// This does not include all  7-day withdrawal chains because we don't necessarily prefer being repaid on some of these 7-day chains, like Mode.
+export const SLOW_WITHDRAWAL_CHAINS = [CHAIN_IDs.BASE, CHAIN_IDs.ARBITRUM, CHAIN_IDs.OPTIMISM];
