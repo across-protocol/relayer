baba5915bcc817e924653086b8e8f203cba318d1
nicholaspai <npai.nyc@gmail.com>
2024-05-23 17:31:02 -0400

    Update opStack.ts

--- src/finalizer/utils/opStack.ts
+++ src/finalizer/utils/opStack.ts
@@ -55,8 +55,7 @@ export async function opStackFinalizer(
   // - Don't submit proofs for finalizations older than 1 day
   // - Don't try to withdraw tokens that are not past the 7 day challenge period
   const redis = await getRedisCache(logger);
-  const latestBlockToProve = await 
-    getBlockForTimestamp(chainId, getCurrentTime() - 7 * 60 * 60 * 24, undefined, redis);
+  const latestBlockToProve = await getBlockForTimestamp(chainId, getCurrentTime() - 7 * 60 * 60 * 24, undefined, redis);
   const { recentTokensBridgedEvents = [], olderTokensBridgedEvents = [] } = groupBy(
     spokePoolClient.getTokensBridged(),
     (e) => {
