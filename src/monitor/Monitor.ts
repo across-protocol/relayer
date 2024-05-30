07ac934780891d887a1d21bb8e9d3f0d24363bfb
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2024-05-30 16:46:32 -0400

    improve(Monitor): Allow stuck rebalance monitor grace period to be configured (#1567)

--- src/monitor/Monitor.ts
+++ src/monitor/Monitor.ts
@@ -40,7 +40,12 @@ import { MonitorClients, updateMonitorClients } from "./MonitorClientHelper";
 import { MonitorConfig } from "./MonitorConfig";
 import { CombinedRefunds } from "../dataworker/DataworkerUtils";
 
-export const REBALANCE_FINALIZE_GRACE_PERIOD = 40 * 60; // 40 minutes, which is 50% of the way through an 80 minute
+export const REBALANCE_FINALIZE_GRACE_PERIOD = process.env.REBALANCE_FINALIZE_GRACE_PERIOD
+  ? Number(process.env.REBALANCE_FINALIZE_GRACE_PERIOD)
+  : 60 * 60;
+// 60 minutes, which is the length of the challenge window, so if a rebalance takes longer than this to finalize,
+// then its finalizing after the subsequent challenge period has started, which is sub-optimal.
+
 // bundle frequency.
 export const ALL_CHAINS_NAME = "All chains";
 const ALL_BALANCE_TYPES = [
