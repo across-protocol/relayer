e072c8ea8ca3ca7b492a4bb00e53b62f98cf9271
Paul <108695806+pxrl@users.noreply.github.com>
2024-05-31 13:58:43 +0200

    Fix merge conflict

--- src/libexec/RelayerSpokePoolIndexer.ts
+++ src/libexec/RelayerSpokePoolIndexer.ts
@@ -213,16 +213,10 @@ async function run(argv: string[]): Promise<void> {
   };
   const args = minimist(argv, minimistOpts);
 
-<<<<<<< pxrl/relocateWorker
   const { chainId, finality = 32, lookback = "5400", relayer = null, maxBlockRange = 10_000 } = args;
   assert(Number.isInteger(chainId), "chainId must be numeric ");
   assert(Number.isInteger(finality), "finality must be numeric ");
-=======
-  const { chainId, finality = 32, lookback = 7200, relayer = null, maxBlockRange = 10_000 } = args;
-  assert(Number.isInteger(chainId), "chainId must be numeric ");
-  assert(Number.isInteger(finality), "finality must be numeric ");
   assert(Number.isInteger(lookback), "lookback must be numeric");
->>>>>>> pxrl/relayerQuorum
   assert(Number.isInteger(maxBlockRange), "maxBlockRange must be numeric");
   assert(!isDefined(relayer) || ethersUtils.isAddress(relayer), `relayer address is invalid (${relayer})`);
 
@@ -250,7 +244,6 @@ async function run(argv: string[]): Promise<void> {
     );
   }
 
-  const quorum = getChainQuorum(chainId);
   const opts = {
     finality,
     quorum,
