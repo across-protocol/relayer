d15ddd3beeb63e01378764f64778267e9d45293c
Paul <108695806+pxrl@users.noreply.github.com>
2024-06-04 09:40:05 +0200

    improve(relayer): Deferred waiting on transaction submission (#1555)
    This change modifies the relayer to avoid getting completely stuck on a
    hanging/slow transaction execution. It does this by retaining promises
    for transaction execution, rather than waiting on each chain's promise
    to resolve. This will mitigate an often-seen issue where transaction
    simulation turtles hugely due to RPC issues and/or gas estimation
    complexities.
    
    In the event that transaction execution any chain does stall, it will
    not be possible to make subsequent fills on that chain. That's OK for
    now, but will ultimately be addressed by reducing the worst-case
    blowouts on transaction simulation and execution (tryMulticall() will be
    a big help here).

--- src/relayer/Relayer.ts
+++ src/relayer/Relayer.ts
@@ -16,6 +16,7 @@ import {
   isDefined,
   winston,
   fixedPointAdjustment,
+  TransactionResponse,
 } from "../utils";
 import { RelayerClients } from "./RelayerClientHelper";
 import { RelayerConfig } from "./RelayerConfig";
@@ -35,6 +36,7 @@ type RepaymentChainProfitability = {
 
 export class Relayer {
   public readonly relayerAddress: string;
+  private pendingTxnReceipts: { [chainId: number]: Promise<TransactionResponse[]> } = {};
 
   constructor(
     relayerAddress: string,
@@ -441,15 +443,36 @@ export class Relayer {
     return lpFees;
   }
 
+  protected async executeFills(chainId: number, simulate = false): Promise<string[]> {
+    const {
+      pendingTxnReceipts,
+      clients: { multiCallerClient },
+    } = this;
+
+    if (isDefined(pendingTxnReceipts[chainId])) {
+      this.logger.info({
+        at: "Relayer::executeFills",
+        message: `${getNetworkName(chainId)} transaction queue has pending fills; skipping...`,
+      });
+      multiCallerClient.clearTransactionQueue(chainId);
+      return [];
+    }
+    pendingTxnReceipts[chainId] = multiCallerClient.executeTxnQueue(chainId, simulate);
+    const txnReceipts = await pendingTxnReceipts[chainId];
+    delete pendingTxnReceipts[chainId];
+
+    return txnReceipts.map(({ hash }) => hash);
+  }
+
   async checkForUnfilledDepositsAndFill(
     sendSlowRelays = true,
     simulate = false
-  ): Promise<{ [chainId: number]: string[] }> {
+  ): Promise<{ [chainId: number]: Promise<string[]> }> {
     const { profitClient, spokePoolClients, tokenClient, multiCallerClient } = this.clients;
 
     // Flush any pre-existing enqueued transactions that might not have been executed.
     multiCallerClient.clearTransactionQueue();
-    const txnReceipts: { [chainId: number]: string[] } = Object.fromEntries(
+    const txnReceipts: { [chainId: number]: Promise<string[]> } = Object.fromEntries(
       Object.values(spokePoolClients).map(({ chainId }) => [chainId, []])
     );
 
@@ -492,8 +515,7 @@ export class Relayer {
       await this.evaluateFills(unfilledDeposits, lpFees, maxBlockNumbers, sendSlowRelays);
 
       if (multiCallerClient.getQueuedTransactions(destinationChainId).length > 0) {
-        const receipts = await multiCallerClient.executeTxnQueues(simulate, [destinationChainId]);
-        txnReceipts[destinationChainId] = receipts[destinationChainId];
+        txnReceipts[destinationChainId] = this.executeFills(destinationChainId, simulate);
       }
     });
 
