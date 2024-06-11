39ec0138cd7ef2caa88607503deeef31613d10eb
bennett <bennett@umaproject.org>
2024-06-11 12:39:17 -0600

    lint
    Signed-off-by: bennett <bennett@umaproject.org>

--- src/utils/NetworkUtils.ts
+++ src/utils/NetworkUtils.ts
@@ -1,11 +1,9 @@
 import { utils as sdkUtils } from "@across-protocol/sdk";
-import { PUBLIC_NETWORKS } from "@across-protocol/constants";
 
 export const { getNetworkName } = sdkUtils;
 
 export function getNativeTokenSymbol(chainId: number | string): string {
   if (chainId.toString() === "137" || chainId.toString() === "80001") {
-  return PUBLIC_NETWORKS[chainId].nativeToken;
     return "MATIC";
   }
   return "ETH";
