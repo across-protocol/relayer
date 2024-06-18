f00474033284b96f02f43c96c9ec05711e781245
bennett <bennett@umaproject.org>
2024-06-18 16:57:24 -0600

    lint
    Signed-off-by: bennett <bennett@umaproject.org>

--- src/common/ContractAddresses.ts
+++ src/common/ContractAddresses.ts
@@ -57,7 +57,7 @@ export const CONTRACT_ADDRESSES: {
     },
     zkSyncMailbox: {
       address: "0x32400084C286CF3E17e7B677ea9583e60a000324",
-      abi: ZK_SYNC_MAILBOX_ABI, 
+      abi: ZK_SYNC_MAILBOX_ABI,
     },
     zkSyncDefaultErc20Bridge: {
       address: "0x57891966931Eb4Bb6FB81430E6cE0A03AAbDe063",
