b232f2056a8df0e403a48715ad27658976b61868
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2024-07-01 16:03:08 -0400

    feat(lite chain): force origin chain repayment (#1629)
    * feat(lite chain): force origin chain repayment
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: remove self relay for lite chain
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * chore: use deposit flag
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: lessen diff
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * chore: remove unneded checks
    
    * improve: move logs to requestSlowFill
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * chore: improve superstruct
    
    * chore: enforce dataworker repayment
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * fix: slow relay
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * hotfix: historical lookup
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: consolidate logic for bundle updates
    
    * chore: update code for readability
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * fix: build error
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: add throw clauses
    
    * fix: tests
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: add addtl tests
    
    * Update src/relayer/Relayer.ts
    
    * chore: update naming
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * fix: make tests more comprehensive
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: switch statement to debug
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: revert self relay logic
    
    * chore: docs
    
    * fix: tests
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * Update src/clients/BundleDataClient.ts
    
    * refuse to fill deposit from lite chain if inventory on origin chain is over-allocated
    
    - Avoids relayer getting over allocated on lite chain
    
    * Update InventoryClient.ts
    
    * feat: disable slow fills for deposits involving lite chains (#1635)
    
    * feat: disable slow fills for deposits involving lite chains
    
    * Add debug log
    
    * Replace condition with precomputed field
    
    * Fix lint
    
    * Update sdk
    
    * Add tests
    
    * Remove only
    
    * Delete master changes
    
    * Delete master changes
    
    * Lint
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Lint
    
    ---------
    
    Co-authored-by: Alexandru Matei <alexandrumatei3693@gmail.com>
    
    * chore: remove comment
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    
    * chore: reduce slow fill checks
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * nit: docs
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    
    * chore: reduce assert
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: push lite chain check to bottom in inventory client
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: include origin chain id
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * improve: docs
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>
    Co-authored-by: nicholaspai <npai.nyc@gmail.com>
    Co-authored-by: amateima <89395931+amateima@users.noreply.github.com>
    Co-authored-by: Alexandru Matei <alexandrumatei3693@gmail.com>
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>

--- src/utils/FillUtils.ts
+++ src/utils/FillUtils.ts
@@ -8,14 +8,20 @@ export function getRefundInformationFromFill(
   fill: Fill,
   hubPoolClient: HubPoolClient,
   blockRangesForChains: number[][],
-  chainIdListForBundleEvaluationBlockNumbers: number[]
+  chainIdListForBundleEvaluationBlockNumbers: number[],
+  fromLiteChain: boolean
 ): {
   chainToSendRefundTo: number;
   repaymentToken: string;
 } {
   // Handle slow relay where repaymentChainId = 0. Slow relays always pay recipient on destination chain.
   // So, save the slow fill under the destination chain, and save the fast fill under its repayment chain.
-  const chainToSendRefundTo = sdkUtils.isSlowFill(fill) ? fill.destinationChainId : fill.repaymentChainId;
+  let chainToSendRefundTo = sdkUtils.isSlowFill(fill) ? fill.destinationChainId : fill.repaymentChainId;
+  // If the fill is for a deposit originating from the lite chain, the repayment chain is the origin chain
+  // regardless of whether it is a slow or fast fill (we ignore slow fills but this is for posterity).
+  if (fromLiteChain) {
+    chainToSendRefundTo = fill.originChainId;
+  }
 
   // Save fill data and associate with repayment chain and L2 token refund should be denominated in.
   const endBlockForMainnet = getBlockRangeForChain(
