c50811ccaf67d570ed1d4311f6f4c7a9014309bf
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2024-08-11 16:58:53 -0400

    improve(BlastFinalizer): Handle two step USDB withdrawal (#1703)
    * [WIP] improve(BlastFinalizer): Handle two step USDB withdrawal
    
    USDB withdrawals require a follow-up claim step from the USDYieldManager after they are finalized via the OptimismPortal like they are in all normal OPStack withdrawals.
    
    This PR is in draft mode because the BlastDaiRetriever is not deployed yet so there is no address available
    
    * Add dai retriever abi
    
    * fix
    
    * Use chainIsBlast
    
    * Update opStack.ts
    
    * Update opStack.ts
    
    * Handle updated ABI
    
    * Check withdrawalClaimed to find already claimed withdrawals
    
    * Fix logs
    
    * Refactor
    
    * Change WithdrawalRequested filtered address from zero address to daiRetriever
    
    * Update opStack.ts
    
    * Update Monitor.ts
    
    * Update Monitor.ts
    
    * Update opStack.ts
    
    * Update opStack.ts

--- src/common/ContractAddresses.ts
+++ src/common/ContractAddresses.ts
@@ -24,7 +24,8 @@ import LINEA_TOKEN_BRIDGE_ABI from "./abi/LineaTokenBridge.json";
 import LINEA_USDC_BRIDGE_ABI from "./abi/LineaUsdcBridge.json";
 import SCROLL_RELAY_MESSENGER_ABI from "./abi/ScrollRelayMessenger.json";
 import BLAST_BRIDGE_ABI from "./abi/BlastBridge.json";
-import BLAST_ETH_YIELD_ABI from "./abi/BlastYield.json";
+import BLAST_YIELD_MANAGER_ABI from "./abi/BlastYieldManager.json";
+import BLAST_DAI_RETRIEVER_ABI from "./abi/BlastDaiRetriever.json";
 import BLAST_OPTIMISM_PORTAL_ABI from "./abi/BlastOptimismPortal.json";
 import SCROLL_GATEWAY_ROUTER_L1_ABI from "./abi/ScrollGatewayRouterL1.json";
 import SCROLL_GATEWAY_ROUTER_L2_ABI from "./abi/ScrollGatewayRouterL2.json";
@@ -146,7 +147,15 @@ export const CONTRACT_ADDRESSES: {
     },
     blastEthYieldManager: {
       address: "0x98078db053902644191f93988341E31289E1C8FE",
-      abi: BLAST_ETH_YIELD_ABI,
+      abi: BLAST_YIELD_MANAGER_ABI,
+    },
+    blastUsdYieldManager: {
+      address: "0xa230285d5683C74935aD14c446e137c8c8828438",
+      abi: BLAST_YIELD_MANAGER_ABI,
+    },
+    blastDaiRetriever: {
+      address: "0x98Dd57048d7d5337e92D9102743528ea4Fea64aB",
+      abi: BLAST_DAI_RETRIEVER_ABI,
     },
     blastOptimismPortal: {
       address: "0x0Ec68c5B10F21EFFb74f2A5C61DFe6b08C0Db6Cb",
