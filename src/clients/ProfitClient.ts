6cb322f1d49aaf520f4a0e6f4cd0e7cbdd38924d
Paul <108695806+pxrl@users.noreply.github.com>
2024-08-30 10:31:13 +0200

    nuke

--- src/clients/ProfitClient.ts
+++ src/clients/ProfitClient.ts
@@ -436,6 +436,7 @@ export class ProfitClient {
     if (!fill.profitable || this.debugProfitability) {
       const { depositId } = deposit;
       const profitable = fill.profitable ? "profitable" : "unprofitable";
+
       this.logger.debug({
         at: "ProfitClient#getFillProfitability",
         message: `${l1Token.symbol} v3 deposit ${depositId} with repayment on ${repaymentChainId} is ${profitable}`,
