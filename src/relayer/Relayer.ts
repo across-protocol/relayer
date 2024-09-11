aa6a7e8bba674cb6a75b081476b308adf8c7bc1a
Paul <108695806+pxrl@users.noreply.github.com>
2024-09-11 10:20:00 +0200

    Clear token data on update

--- src/relayer/Relayer.ts
+++ src/relayer/Relayer.ts
@@ -114,6 +114,7 @@ export class Relayer {
       // Clear state from profit and token clients. These should start fresh on each iteration.
       profitClient.clearUnprofitableFills();
       tokenClient.clearTokenShortfall();
+      tokenClient.clearTokenData();
 
       await configStoreClient.update();
       await hubPoolClient.update();
