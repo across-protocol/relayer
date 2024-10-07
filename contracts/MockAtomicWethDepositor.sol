b05d76d946d54f04e420c3350046aa90db17b78c
bennett <bennett@umaproject.org>
2024-10-07 12:24:46 -0500

    improve: use new atomic depositor
    Signed-off-by: bennett <bennett@umaproject.org>

--- contracts/MockAtomicWethDepositor.sol
+++ contracts/MockAtomicWethDepositor.sol
@@ -3,9 +3,9 @@
 pragma solidity ^0.8.0;
 
 contract MockAtomicWethDepositor {
-    event ZkSyncEthDepositInitiated(address indexed from, address indexed to, uint256 amount);
+    event AtomicWethDepositInitiated(address indexed from, uint256 indexed chainId, uint256 amount);
 
-    function bridgeWethToZkSync(address to, uint256 amount, uint256, uint256, address) public {
-        emit ZkSyncEthDepositInitiated(msg.sender, to, amount);
+    function bridgeWeth(uint256 chainId, uint256 amount, bytes calldata) public {
+        emit AtomicWethDepositInitiated(msg.sender, chainId, amount);
     }
 }
