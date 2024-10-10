f7d189ee4f73e34f34ed8b037ff33b49002ce7af
Paul <108695806+pxrl@users.noreply.github.com>
2024-10-10 15:02:27 +0200

    feat: Support finalizing on World Chain
    This should probably have been part of the larger commit to support
    World Chain. It has been tested locally and successfully submitted a
    proof for a WBTC withdrawal.

--- src/finalizer/index.ts
+++ src/finalizer/index.ts
@@ -102,6 +102,10 @@ const chainFinalizers: { [chainId: number]: { finalizeOnL2: ChainFinalizer[]; fi
     finalizeOnL1: [opStackFinalizer],
     finalizeOnL2: [],
   },
+  [CHAIN_IDs.WORLD_CHAIN]: {
+    finalizeOnL1: [opStackFinalizer],
+    finalizeOnL2: [],
+  },
   // Testnets
   [CHAIN_IDs.BASE_SEPOLIA]: {
     finalizeOnL1: [cctpL2toL1Finalizer],
