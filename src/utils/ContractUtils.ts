b7beea983d2649e8cd122bc3031c56620b5ec84f
Paul <108695806+pxrl@users.noreply.github.com>
2024-11-30 09:03:18 +0100

    improve(relayer): Pass SpokePool addresses to listener processes (#1902)
    This skips an RPC call per SpokePool listener in the fast relayer
    configuration, and permits each SpokePool listener instance to
    communicate with only a single chain, instead of having to always query
    mainnet to resolve the current SpokePool address.

--- src/utils/ContractUtils.ts
+++ src/utils/ContractUtils.ts
@@ -1,32 +1,43 @@
-import { getNetworkName, Contract, Signer, getDeployedAddress, getDeployedBlockNumber } from ".";
-
 import * as typechain from "@across-protocol/contracts"; // TODO: refactor once we've fixed export from contract repo
+import { CHAIN_IDs, getNetworkName, Contract, Signer, getDeployedAddress, getDeployedBlockNumber } from ".";
 
 // Return an ethers contract instance for a deployed contract, imported from the Across-protocol contracts repo.
 export function getDeployedContract(contractName: string, networkId: number, signer?: Signer): Contract {
   try {
     const address = getDeployedAddress(contractName, networkId);
     // If the contractName is SpokePool then we need to modify it to find the correct contract factory artifact.
-    const factoryName = contractName === "SpokePool" ? castSpokePoolName(networkId) : contractName;
-    const artifact = typechain[`${[factoryName.replace("_", "")]}__factory`];
+    const factoryName = `${contractName === "SpokePool" ? castSpokePoolName(networkId) : contractName}__factory`;
+    const artifact = typechain[factoryName];
     return new Contract(address, artifact.abi, signer);
   } catch (error) {
-    throw new Error(`Could not find address for contract ${contractName} on ${networkId}`);
+    throw new Error(`Could not find address for contract ${contractName} on ${networkId} (${error})`);
   }
 }
 
 // If the name of the contract is SpokePool then we need to apply a transformation on the name to get the correct
 // contract factory name. For example, if the network is "mainnet" then the contract is called Ethereum_SpokePool.
 export function castSpokePoolName(networkId: number): string {
-  let networkName = getNetworkName(networkId);
-  if (networkName == "Mainnet" || networkName == "Rinkeby" || networkName == "Kovan" || networkName == "Goerli") {
-    return "Ethereum_SpokePool";
+  let networkName: string;
+  switch (networkId) {
+    case CHAIN_IDs.MAINNET:
+    case CHAIN_IDs.SEPOLIA:
+      return "Ethereum_SpokePool";
+    case CHAIN_IDs.ARBITRUM:
+      return "Arbitrum_SpokePool";
+    case CHAIN_IDs.ZK_SYNC:
+      return "ZkSync_SpokePool";
+    default:
+      networkName = getNetworkName(networkId);
   }
 
-  if (networkName.includes("-")) {
-    networkName = networkName.substring(0, networkName.indexOf("-"));
-  }
-  return `${networkName}_SpokePool`;
+  return `${networkName.replace(" ", "")}_SpokePool`;
+}
+
+// For a chain ID and optional SpokePool address, return a Contract instance with the corresponding ABI.
+export function getSpokePool(chainId: number, address?: string): Contract {
+  const factoryName = castSpokePoolName(chainId);
+  const artifact = typechain[`${factoryName}__factory`];
+  return new Contract(address ?? getDeployedAddress("SpokePool", chainId), artifact.abi);
 }
 
 export function getParamType(contractName: string, functionName: string, paramName: string): string {
