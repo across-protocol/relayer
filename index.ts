b3b77030212f7f85c3eb5bba669ff8ce6317617e
nicholaspai <npai.nyc@gmail.com>
2025-01-27 13:03:51 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -79,7 +79,10 @@ if (require.main === module) {
         args,
         notificationPath: "across-error",
       });
-      logger.debug({ at: cmd ?? "unknown process", message: "Logging full error", error });
+      if (process.env.CONSOLE_LOG_ERRORS) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
     })
     .finally(async () => {
       await waitForLogger(logger);
