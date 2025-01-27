c117559ab2ea2cc1f0ece9e707060882a2c85ffc
nicholaspai <npai.nyc@gmail.com>
2025-01-27 14:42:35 -0500

    improve(dataworker): Fund more fee tokens into fee payer address
    Reduces amortized gas cost at expense of locking up some capital. I think this is worth it given how often we need to top up the fee payer for executing L1 leaves

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -688,11 +688,11 @@ export const ARBITRUM_ORBIT_L1L2_MESSAGE_FEE_DATA: {
   // Leave feeToken undefined if feeToken is ETH.
   [CHAIN_IDs.ARBITRUM]: {
     amountWei: 0.02,
-    amountMultipleToFund: 1,
+    amountMultipleToFund: 50,
   },
   [CHAIN_IDs.ALEPH_ZERO]: {
     amountWei: 0.49,
-    amountMultipleToFund: 10,
+    amountMultipleToFund: 50,
     feePayer: "0x0d57392895Db5aF3280e9223323e20F3951E81B1", // DonationBox
     feeToken: TOKEN_SYMBOLS_MAP.AZERO.addresses[CHAIN_IDs.MAINNET],
   },
