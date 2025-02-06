4565206ff9e1196f50fb590afcbb0297f3cdf539
Paul <108695806+pxrl@users.noreply.github.com>
2025-02-06 15:10:23 +0000

    lint

--- src/dataworker/Dataworker.ts
+++ src/dataworker/Dataworker.ts
@@ -1196,10 +1196,12 @@ export class Dataworker {
       const messageHash = getMessageHash(relayData.message);
 
       // Start with the most recent fills and search backwards.
-      const fill = _.findLast(sortedFills, (fill) =>
-        fill.depositId.eq(relayData.depositId) &&
-        fill.originChainId === relayData.originChainId &&
-        getRelayEventKey(fill) === getRelayEventKey({ ...relayData, messageHash, destinationChainId })
+      const fill = _.findLast(
+        sortedFills,
+        (fill) =>
+          fill.depositId.eq(relayData.depositId) &&
+          fill.originChainId === relayData.originChainId &&
+          getRelayEventKey(fill) === getRelayEventKey({ ...relayData, messageHash, destinationChainId })
       );
 
       return fill;
