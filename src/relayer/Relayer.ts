9437718f539f5ea5f637b0f2ab7407006e27876e
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2025-02-06 21:23:09 -0500

    fix(Relayer): Simplify computeOriginChainCommitment check (#2057)

--- src/relayer/Relayer.ts
+++ src/relayer/Relayer.ts
@@ -470,16 +470,10 @@ export class Relayer {
 
     const deposits = originSpoke.getDeposits({ fromBlock, toBlock });
     const commitment = deposits.reduce((acc, deposit) => {
-      const fills = spokePoolClients[deposit.destinationChainId]
+      const fill = spokePoolClients[deposit.destinationChainId]
         ?.getFillsForDeposit(deposit)
-        ?.filter((fill) => validateFillForDeposit(fill, deposit).valid);
-      let fill;
-      if (isDefined(fills) && fills.length > 0) {
-        // There should only ever be one valid fill per deposit.
-        assert(fills.length === 1);
-        fill = fills[0];
-      }
-      if (!isDefined(fill) || fill.relayer !== this.relayerAddress) {
+        ?.find((f) => f.relayer === this.relayerAddress);
+      if (!isDefined(fill) || !validateFillForDeposit(fill, deposit).valid) {
         return acc;
       }
 
