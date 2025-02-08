d9c1f9109838e10d933dcb7449090cb30845c783
nicholaspai <npai.nyc@gmail.com>
2025-02-08 10:44:27 -0500

    Update utils.ts

--- src/finalizer/utils/cctp/utils.ts
+++ src/finalizer/utils/cctp/utils.ts
@@ -10,37 +10,37 @@ import { DecodedCCTPMessage } from "../../../utils/CCTPUtils";
  * fails to simulate, it will be undefined.
  */
 export async function generateMultiCallData(
-    messageTransmitter: Contract,
-    messages: DecodedCCTPMessage[],
-    logger: winston.Logger,
-    l2ChainId
-  ): Promise<(Multicall2Call | undefined)[]> {
-    assert(messages.every((message) => isDefined(message.attestation)));
-    return Promise.all(
-      messages.map(async (message) => {
-        try {
-          await messageTransmitter.estimateGas.receiveMessage(message.messageBytes, message.attestation);
-          // Try to simulate transaction to give more detailed error log if it would fail in the multicaller.
-          const txn = (await messageTransmitter.populateTransaction.receiveMessage(
-            message.messageBytes,
-            message.attestation
-          )) as TransactionRequest;
-          return {
-            target: txn.to,
-            callData: txn.data,
-          };
-        } catch (error) {
-          logger.warn({
-            at: `Finalizer#CCTPFinalizer:${l2ChainId}:generateMultiCallData`,
-            message: "Finalizing CCTP transfer failed simulation",
-            l2ChainId,
-            target: messageTransmitter.address,
-            method: "receiveMessage",
-            args: [message.messageBytes, message.attestation],
-            error,
-          });
-          return undefined;
-        }
-      })
-    );
-  }
+  messageTransmitter: Contract,
+  messages: DecodedCCTPMessage[],
+  logger: winston.Logger,
+  l2ChainId
+): Promise<(Multicall2Call | undefined)[]> {
+  assert(messages.every((message) => isDefined(message.attestation)));
+  return Promise.all(
+    messages.map(async (message) => {
+      try {
+        await messageTransmitter.estimateGas.receiveMessage(message.messageBytes, message.attestation);
+        // Try to simulate transaction to give more detailed error log if it would fail in the multicaller.
+        const txn = (await messageTransmitter.populateTransaction.receiveMessage(
+          message.messageBytes,
+          message.attestation
+        )) as TransactionRequest;
+        return {
+          target: txn.to,
+          callData: txn.data,
+        };
+      } catch (error) {
+        logger.warn({
+          at: `Finalizer#CCTPFinalizer:${l2ChainId}:generateMultiCallData`,
+          message: "Finalizing CCTP transfer failed simulation",
+          l2ChainId,
+          target: messageTransmitter.address,
+          method: "receiveMessage",
+          args: [message.messageBytes, message.attestation],
+          error,
+        });
+        return undefined;
+      }
+    })
+  );
+}
