50b46f1d7e2968e65bb6e97bb503f73d0fac46c9
Paul <108695806+pxrl@users.noreply.github.com>
2025-02-23 13:23:17 +0000

    Additional test

--- test/Dataworker.loadData.deposit.ts
+++ test/Dataworker.loadData.deposit.ts
@@ -1,6 +1,6 @@
 import { BundleDataClient, ConfigStoreClient, HubPoolClient, SpokePoolClient } from "../src/clients";
 import { amountToDeposit, destinationChainId, originChainId, repaymentChainId } from "./constants";
-import { setupDataworker } from "./fixtures/Dataworker.Fixture";
+import { DataworkerConfig, setupDataworker } from "./fixtures/Dataworker.Fixture";
 import {
   Contract,
   FakeContract,
@@ -109,6 +109,7 @@ describe("Dataworker: Load bundle data", async function () {
       );
       dataworkerInstance = new Dataworker(
         dataworkerInstance.logger,
+        {} as DataworkerConfig,
         { ...dataworkerInstance.clients, bundleDataClient },
         dataworkerInstance.chainIdListForBundleEvaluationBlockNumbers,
         dataworkerInstance.maxRefundCountOverride,
