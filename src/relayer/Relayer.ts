29ef48bed78a7be7866be8ade4c12d72bfd476cb
nicholaspai <npai.nyc@gmail.com>
2025-04-23 21:03:04 -0400

    Update Relayer.ts

--- src/relayer/Relayer.ts
+++ src/relayer/Relayer.ts
@@ -1481,7 +1481,7 @@ export class Relayer {
   }
 
   private constructBaseFillMarkdown(deposit: Deposit, _realizedLpFeePct: BigNumber, _gasPriceGwei: BigNumber): string {
-    const { symbol, decimals } = this.clients.hubPoolClient.getTokenInfoForDeposit(deposit);
+    const { symbol, decimals } = this.clients.hubPoolClient.getTokenInfoForAddress(deposit.inputToken, deposit.originChainId);
     const srcChain = getNetworkName(deposit.originChainId);
     const dstChain = getNetworkName(deposit.destinationChainId);
     const depositor = blockExplorerLink(deposit.depositor, deposit.originChainId);
