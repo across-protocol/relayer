7e87a362ae9f60544515468a8c240e945c4d2252
bennett <bennett@umaproject.org>
2025-05-05 14:16:11 -0500

    chore: reduce bsc finalizer log level
    Signed-off-by: bennett <bennett@umaproject.org>

--- src/finalizer/utils/binance/l2ToL1.ts
+++ src/finalizer/utils/binance/l2ToL1.ts
@@ -108,7 +108,7 @@ export async function binanceL2ToL1Finalizer(
       // Binance also takes fees from withdrawals. Since we are bundling together multiple deposits, it is possible that the amount we are trying to withdraw is slightly greater than our free balance
       // (since a prior withdrawal's fees were paid for in part from the current withdrawal's balance). In this case, set `amountToFinalize` as `min(amountToFinalize, accountBalance)`.
       if (amountToFinalize > Number(coin.balance)) {
-        logger.warn({
+        logger.debug({
           at: "BinanceL2ToL1Finalizer",
           message:
             "Need to reduce the amount to finalize since hot wallet balance is less than desired withdrawal amount",
