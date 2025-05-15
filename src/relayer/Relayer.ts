5edc2c9a1d4779a2b3980840091d3e0234485eca
Ihor Farion <ihor@umaproject.org>
2025-05-14 18:53:44 -0700

    remove some L2 approval comments

--- src/relayer/Relayer.ts
+++ src/relayer/Relayer.ts
@@ -96,13 +96,11 @@ export class Relayer {
     ]);
 
     if (this.config.sendingRelaysEnabled && this.config.sendingTransactionsEnabled) {
-      // TODO: does this cause L2 token approvals? Investigate.
       await tokenClient.setOriginTokenApprovals();
     }
 
     if (this.config.sendingRebalancesEnabled && this.config.sendingTransactionsEnabled) {
       await inventoryClient.setL1TokenApprovals();
-      // TODO: add inventoryClient.setL2TokenApprovals()
     }
 
     this.logger.debug({
