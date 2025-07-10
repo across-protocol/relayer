510a840126fe76d2b8c465d710507798f4902af1
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-07-09 17:54:15 -0700

    Added max startup delay check

--- src/utils/FunctionUtils.ts
+++ src/utils/FunctionUtils.ts
@@ -1,7 +1,19 @@
-export async function waitUntilTrue(checkCondition: () => Promise<boolean>, intervalMs: number): Promise<void> {
+export async function waitUntilTrue(
+  checkCondition: () => Promise<boolean>,
+  intervalMs: number,
+  maxWaitMs = 60000
+): Promise<boolean> {
+  const startTime = Date.now();
+
+  // eslint-disable-next-line no-constant-condition
   while (true) {
     const result = await checkCondition();
-    if (result) break;
+    if (result) {
+      return true;
+    }
+    if (Date.now() - startTime > maxWaitMs) {
+      return false;
+    }
     await new Promise((res) => setTimeout(res, intervalMs));
   }
 }
