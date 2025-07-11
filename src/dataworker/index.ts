c230ffecd61665e714db1d4a9e2fc4f1a978c048
Paul <108695806+pxrl@users.noreply.github.com>
2025-07-11 14:05:56 +0000

    revert: "combine the proposer and l1 executor (#2358)"
    This reverts commit c263cb90dfccdae99aec64c1a6b0e81fd805903e.

--- src/dataworker/index.ts
+++ src/dataworker/index.ts
@@ -9,8 +9,8 @@ import {
   startupLogLevel,
   Signer,
   disconnectRedisClients,
-  Profiler,
   isDefined,
+  Profiler,
   getSvmSignerFromEvmSigner,
 } from "../utils";
 import { spokePoolClientsToProviders } from "../common";
@@ -23,7 +23,7 @@ import {
   DataworkerClients,
 } from "./DataworkerClientHelper";
 import { BalanceAllocator } from "../clients/BalanceAllocator";
-import { PendingRootBundle } from "../interfaces";
+import { PendingRootBundle, BundleData } from "../interfaces";
 
 config();
 let logger: winston.Logger;
@@ -49,6 +49,7 @@ export async function createDataworker(
     config.maxPoolRebalanceLeafSizeOverride,
     config.spokeRootsLookbackCount,
     config.bufferToPropose,
+    config.forcePropose,
     config.forceProposalBundleRange
   );
 
@@ -114,6 +115,7 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
   );
 
   await config.update(logger); // Update address filter.
+  let proposedBundleData: BundleData | undefined = undefined;
   let poolRebalanceLeafExecutionCount = 0;
   try {
     // Explicitly don't log addressFilter because it can be huge and can overwhelm log transports.
@@ -177,7 +179,7 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
       logger[startupLogLevel(config)]({ at: "Dataworker#index", message: "Disputer disabled" });
     }
 
-    if (config.proposerEnabled || config.l1ExecutorEnabled) {
+    if (config.proposerEnabled) {
       if (config.sendingTransactionsEnabled) {
         const tokenClient = new TokenClient(
           logger,
@@ -197,10 +199,22 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
         await tokenClient.setBondTokenAllowance();
       }
 
-      const executionQueue = new Array(2).fill(Promise.resolve(undefined));
+      // Bundle data is defined if and only if there is a new bundle proposal transaction enqueued.
+      proposedBundleData = await dataworker.proposeRootBundle(
+        spokePoolClients,
+        config.rootBundleExecutionThreshold,
+        config.sendingTransactionsEnabled,
+        fromBlocks
+      );
+    } else {
+      logger[startupLogLevel(config)]({ at: "Dataworker#index", message: "Proposer disabled" });
+    }
+
+    if (config.l2ExecutorEnabled || config.l1ExecutorEnabled) {
+      const balanceAllocator = new BalanceAllocator(spokePoolClientsToProviders(spokePoolClients));
+
       if (config.l1ExecutorEnabled) {
-        const balanceAllocator = new BalanceAllocator(spokePoolClientsToProviders(spokePoolClients));
-        executionQueue[0] = dataworker.executePoolRebalanceLeaves(
+        poolRebalanceLeafExecutionCount = await dataworker.executePoolRebalanceLeaves(
           spokePoolClients,
           balanceAllocator,
           config.sendingTransactionsEnabled,
@@ -208,45 +222,21 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
         );
       }
 
-      if (config.proposerEnabled) {
-        executionQueue[1] = dataworker.proposeRootBundle(
+      if (config.l2ExecutorEnabled) {
+        // Execute slow relays before relayer refunds to give them priority for any L2 funds.
+        await dataworker.executeSlowRelayLeaves(
           spokePoolClients,
-          config.rootBundleExecutionThreshold,
+          balanceAllocator,
+          config.sendingTransactionsEnabled,
+          fromBlocks
+        );
+        await dataworker.executeRelayerRefundLeaves(
+          spokePoolClients,
+          balanceAllocator,
           config.sendingTransactionsEnabled,
           fromBlocks
         );
       }
-
-      const [_poolRebalanceLeafExecutionCount, proposeRootBundleTxn] = await Promise.all(executionQueue);
-      poolRebalanceLeafExecutionCount = _poolRebalanceLeafExecutionCount ?? 0;
-      const updatedChallengeRemaining = await getChallengeRemaining(config.hubPoolChainId);
-      if (updatedChallengeRemaining === 0) {
-        if (poolRebalanceLeafExecutionCount > 0) {
-          await clients.multiCallerClient.executeTxnQueues();
-        }
-        if (isDefined(proposeRootBundleTxn)) {
-          clients.multiCallerClient.enqueueTransaction(proposeRootBundleTxn);
-        }
-      }
-    } else {
-      logger[startupLogLevel(config)]({ at: "Dataworker#index", message: "Proposer disabled" });
-    }
-
-    if (config.l2ExecutorEnabled) {
-      const balanceAllocator = new BalanceAllocator(spokePoolClientsToProviders(spokePoolClients));
-      // Execute slow relays before relayer refunds to give them priority for any L2 funds.
-      await dataworker.executeSlowRelayLeaves(
-        spokePoolClients,
-        balanceAllocator,
-        config.sendingTransactionsEnabled,
-        fromBlocks
-      );
-      await dataworker.executeRelayerRefundLeaves(
-        spokePoolClients,
-        balanceAllocator,
-        config.sendingTransactionsEnabled,
-        fromBlocks
-      );
     } else {
       logger[startupLogLevel(config)]({ at: "Dataworker#index", message: "Executor disabled" });
     }
@@ -256,6 +246,7 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
     // leaves to be executed but the proposed bundle was already executed, then exit early.
     const pendingProposal: PendingRootBundle = await clients.hubPoolClient.hubPool.rootBundleProposal();
 
+    const proposalCollision = isDefined(proposedBundleData) && pendingProposal.unclaimedPoolRebalanceLeafCount > 0;
     // The pending root bundle that we want to execute has already been executed if its unclaimed leaf count
     // does not match the number of leaves the executor wants to execute, or the pending root bundle's
     // challenge period timestamp is in the future. This latter case is rarer but it can
@@ -264,10 +255,12 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
       poolRebalanceLeafExecutionCount > 0 &&
       (pendingProposal.unclaimedPoolRebalanceLeafCount !== poolRebalanceLeafExecutionCount ||
         pendingProposal.challengePeriodEndTimestamp > clients.hubPoolClient.currentTime);
-    if (executorCollision) {
+    if (proposalCollision || executorCollision) {
       logger[startupLogLevel(config)]({
         at: "Dataworker#index",
         message: "Exiting early due to dataworker function collision",
+        proposalCollision,
+        proposedBundleDataDefined: isDefined(proposedBundleData),
         executorCollision,
         poolRebalanceLeafExecutionCount,
         unclaimedPoolRebalanceLeafCount: pendingProposal.unclaimedPoolRebalanceLeafCount,
