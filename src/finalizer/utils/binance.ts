ba386612ec72bf89e5da211b5aa3323c9d318a73
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-07-15 12:12:07 -0400

    fix: Converted binance withdrawal to run in series

--- src/finalizer/utils/binance.ts
+++ src/finalizer/utils/binance.ts
@@ -146,12 +146,14 @@ export async function binanceFinalizer(
       symbol = symbol === "WBNB" ? "BNB" : symbol;
 
       const coin = accountCoins.find((coin) => coin.symbol === symbol);
+      let coinBalance = coin.balance;
       const l1Token = TOKEN_SYMBOLS_MAP[symbol].addresses[hubChainId];
       const { decimals: l1Decimals } = getTokenInfo(EvmAddress.from(l1Token), hubChainId);
       const withdrawals = await getBinanceWithdrawals(binanceApi, symbol, fromTimestamp);
 
       // Start by finalizing L1 -> L2, then go to L2 -> L1.
-      await mapAsync([DepositNetwork.Ethereum, DepositNetwork.BSC], async (depositNetwork) => {
+      for (const depositNetwork of [DepositNetwork.Ethereum, DepositNetwork.BSC]) {
+        // await mapAsync([DepositNetwork.Ethereum, DepositNetwork.BSC], async (depositNetwork) => {
         const withdrawNetwork =
           depositNetwork === DepositNetwork.Ethereum ? DepositNetwork.BSC : DepositNetwork.Ethereum;
         const networkLimits = coin.networkList.find((network) => network.name === withdrawNetwork);
@@ -192,16 +194,17 @@ export async function binanceFinalizer(
         }
         // Binance also takes fees from withdrawals. Since we are bundling together multiple deposits, it is possible that the amount we are trying to withdraw is slightly greater than our free balance
         // (since a prior withdrawal's fees were paid for in part from the current withdrawal's balance). In this case, set `amountToFinalize` as `min(amountToFinalize, accountBalance)`.
-        if (amountToFinalize > Number(coin.balance)) {
+        if (amountToFinalize > Number(coinBalance)) {
           logger.debug({
             at: "BinanceFinalizer",
             message: `(${depositNetwork} -> ${withdrawNetwork}) Need to reduce the amount to finalize since hot wallet balance is less than desired withdrawal amount.`,
             amountToFinalize,
-            balance: coin.balance,
+            balance: coinBalance,
           });
-          amountToFinalize = Number(coin.balance);
+          amountToFinalize = Number(coinBalance);
         }
         if (amountToFinalize >= Number(networkLimits.withdrawMin)) {
+          coinBalance = (Number(coinBalance) - amountToFinalize).toFixed(6);
           // Lastly, we need to truncate the amount to withdraw to six decimal places.
           amountToFinalize = Math.floor(amountToFinalize * DECIMAL_PRECISION) / DECIMAL_PRECISION;
           const withdrawalId = await binanceApi.withdraw({
@@ -223,7 +226,8 @@ export async function binanceFinalizer(
             message: `(${depositNetwork} -> ${withdrawNetwork}) ${amountToFinalize} is less than minimum withdrawable amount ${networkLimits.withdrawMin} for token ${symbol}.`,
           });
         }
-      });
+        // });
+      }
     });
   });
   return {
