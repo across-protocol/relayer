2ab84603acc5770ede93bf42629fac1fd16f19e8
nicholaspai <npai.nyc@gmail.com>
2025-07-21 19:40:09 -0400

    make sendRawTransaction work for unit tests

--- src/monitor/Monitor.ts
+++ src/monitor/Monitor.ts
@@ -670,8 +670,8 @@ export class Monitor {
               });
             } else {
               const nativeSymbolForChain = getNativeTokenSymbol(chainId);
-              // To send a raw transaction, we need to create a fake Contract instance with an empty ABI and
-              // an address set to the recipient of the refill transfer.
+              // To send a raw transaction, we need to create a fake Contract instance with an empty ABI,
+              // an empty method param, and an address set to the recipient of the refill transfer.
               const sendRawTransactionContract = new Contract(
                 account.toEvmAddress(),
                 [], // ABI should be left empty when sending raw transactions
