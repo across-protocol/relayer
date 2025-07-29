ceb58a966d1e36cd62021403b32952ea0b051c8f
nicholaspai <npai.nyc@gmail.com>
2025-07-29 10:05:53 -0400

    Update index.ts

--- src/dataworker/index.ts
+++ src/dataworker/index.ts
@@ -124,12 +124,12 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
 
   const { clients, dataworker } = await createDataworker(logger, baseSigner, config);
 
-  const redis = await getRedisCache(logger);
   // If L1 Executor is enabled, block other instances from running for ~5 mins to prevent multiple executors from
   // executing the same bundle.
   if (config.l1ExecutorEnabled && process.env.L1_EXECUTOR_MUTEX_DISABLED !== "true") {
+    const _redis = await getRedisCache(logger);
     const l1ExecutorMutexKey = "L1_EXECUTOR_RUNNING";
-    const redisMessage = await redis.get(l1ExecutorMutexKey);
+    const redisMessage = await _redis.get(l1ExecutorMutexKey);
     if (redisMessage) {
       logger.debug({
         at: "Dataworker#index",
@@ -138,7 +138,7 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
       return;
     } else {
       const mutexExpirySeconds = Number(process.env.L1_EXECUTOR_MUTEX_EXPIRY_SECONDS ?? 300); /* 5 minute lock */
-      await redis.set(l1ExecutorMutexKey, "locked", mutexExpirySeconds);
+      await _redis.set(l1ExecutorMutexKey, "locked", mutexExpirySeconds);
       logger.debug({
         at: "Dataworker#index",
         message: `L1 Executor mutex set for ${mutexExpirySeconds} seconds.`,
@@ -151,6 +151,8 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
   const adjustedL1BlockTime = l1BlockTime + Number(process.env["L1_BLOCK_TIME_BUFFER"] ?? l1BlockTime); // Default adjustment is double l1BlockTime.
   let proposedBundleData: BundleData | undefined = undefined;
   let poolRebalanceLeafExecutionCount = 0;
+
+  const redis = await getRedisCache(logger);
 
   try {
     // Explicitly don't log addressFilter because it can be huge and can overwhelm log transports.
