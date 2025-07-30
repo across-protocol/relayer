7fa0513848f5fcd8d9b297dce46a7da906d17963
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-07-29 23:33:44 -0400

    Added address formatter

--- src/utils/LogUtils.ts
+++ src/utils/LogUtils.ts
@@ -1,5 +1,9 @@
 export type DefaultLogLevels = "debug" | "info" | "warn" | "error";
 
+import { LogEntry } from "winston";
+
+import { Address } from "./SDKUtils";
+
 export function stringifyThrownValue(value: unknown): string {
   if (value instanceof Error) {
     const errToString = value.toString();
@@ -15,3 +19,41 @@ export function stringifyThrownValue(value: unknown): string {
     return `ThrownValue: ${value.toString()}`;
   }
 }
+
+// Iterate over each element in the log and see if it is a address. if it is, then try casting it to a string to
+// make it more readable. If something goes wrong in parsing the object (it's too large or something else) then simply
+// return the original log entry without modifying it.
+export function addressFormatter(logEntry: LogEntry) {
+  type SymbolRecord = Record<string | symbol, any>;
+  try {
+    // Out is the original object if and only if one or more BigNumbers were replaced.
+    const out: SymbolRecord = iterativelyReplaceAddresses(logEntry);
+
+    // Because winston depends on some non-enumerable symbol properties, we explicitly copy those over, as they are not
+    // handled in iterativelyReplaceAddresses. This only needs to happen if logEntry is being replaced.
+    if (out !== logEntry)
+      Object.getOwnPropertySymbols(logEntry).map((symbol) => (out[symbol] = (logEntry as SymbolRecord)[symbol]));
+    return out as LogEntry;
+  } catch (_) {
+    return logEntry;
+  }
+}
+
+// Traverse a potentially nested object and replace any element that is an Address
+// with the string version of it for easy logging.
+const iterativelyReplaceAddresses = (obj: Record<string | symbol, any>) => {
+  // This does a DFS, recursively calling this function to find the desired value for each key.
+  // It doesn't modify the original object. Instead, it creates an array of keys and updated values.
+  const replacements = Object.entries(obj).map(([key, value]): [string, any] => {
+    if (Address.isAddress(value)) return [key, value.toString()];
+    else if (typeof value === "object" && value !== null) return [key, iterativelyReplaceAddresses(value)];
+    else return [key, value];
+  });
+
+  // This will catch any values that were changed by value _or_ by reference.
+  // If no changes were detected, no copy is needed and it is fine to discard the copy and return the original object.
+  const copyNeeded = replacements.some(([key, value]) => obj[key] !== value);
+
+  // Only copy if something changed. Otherwise, return the original object.
+  return copyNeeded ? Object.fromEntries(replacements) : obj;
+};
