373c2aa7ae9020790c3c0f212abfb7b7317997dc
Aleksa Colovic <aleksa.colovic98@gmail.com>
2025-08-08 11:54:52 +0200

    Style fix

--- src/clients/SvmFillerClient.ts
+++ src/clients/SvmFillerClient.ts
@@ -112,17 +112,17 @@ export class SvmFillerClient {
           signature: signatureString,
           explorer: blockExplorerLink(signatureString, this.chainId),
         });
-      } catch (e) {
+      } catch (e: any) {
         let message = "";
         if (!isSolanaError(e)) {
-          message = "Unknown error";
+          message = e?.message ?? "Unknown error";
         } else {
           message = `Solana error code: ${e.context.__code}`;
         }
 
         this.logger.error({
           at: "SvmFillerClient#executeTxnQueue",
-          message: `Failed to send fill transaction: ${message}`,
+          message: `Failed to send fill transaction (${message})`,
           mrkdwn,
           error: e,
         });
