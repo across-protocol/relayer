f5a393321c1f5562c5d7d3b24c9ecff4c1d7b151
nicholaspai <npai.nyc@gmail.com>
2025-08-13 22:13:22 -0400

    improve(LogUtils): Log full error if unable to parse it

--- src/utils/LogUtils.ts
+++ src/utils/LogUtils.ts
@@ -7,10 +7,12 @@ export function stringifyThrownValue(value: unknown): string {
       ? value.stack
       : value.message || errToString !== "[object Object]"
       ? errToString
-      : "could not extract error from 'Error' instance";
+      : `could not extract error from 'Error' instance ${errToString}`;
   } else if (value instanceof Object) {
     const objStringified = JSON.stringify(value);
-    return objStringified !== "{}" ? objStringified : "could not extract error from 'Object' instance";
+    return objStringified !== "{}"
+      ? objStringified
+      : `could not extract error from 'Object' instance ${objStringified}`;
   } else {
     return `ThrownValue: ${value.toString()}`;
   }
