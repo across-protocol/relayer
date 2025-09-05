939a49a74bab6f6a8e9160033054e643b801aeae
bennett <bennett@umaproject.org>
2025-09-05 09:10:14 -0500

    combine sdk bump
    Signed-off-by: bennett <bennett@umaproject.org>

--- src/utils/SDKUtils.ts
+++ src/utils/SDKUtils.ts
@@ -105,7 +105,7 @@ export const {
 
 export const {
   getRefundsFromBundle,
-  isChainDisabled,
+  isChainDisabledAtBlock,
   getWidestPossibleExpectedBlockRange,
   getEndBlockBuffers,
   buildPoolRebalanceLeafTree,
