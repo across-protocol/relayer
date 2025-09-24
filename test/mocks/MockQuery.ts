3f0049d8afc67909d888fd1967ec42718217ecde
Ihor Farion <ihor@umaproject.org>
2025-09-23 19:44:23 -0700

    remove extras
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- test/mocks/MockQuery.ts
+++ test/mocks/MockQuery.ts
@@ -1,32 +1,11 @@
-import { utils as sdkUtils } from "@across-protocol/sdk";
 import { BigNumber } from "../utils";
 import { bnZero } from "../../src/utils";
 
-type TransactionCostEstimate = sdkUtils.TransactionCostEstimate;
-
 export class MockQuery {
-  private gasCostsByChain: { [chainId: number]: TransactionCostEstimate } = {};
   private auxNativeTokenCostByChain: { [chainId: number]: BigNumber } = {};
-
-  setGasCost(chainId: number, gasCost?: TransactionCostEstimate): void {
-    if (gasCost) {
-      this.gasCostsByChain[chainId] = gasCost;
-    } else {
-      delete this.gasCostsByChain[chainId];
-    }
-  }
 
   setAuxiliaryNativeTokenCost(chainId: number, cost: BigNumber): void {
     this.auxNativeTokenCostByChain[chainId] = cost;
-  }
-
-  getGasCosts(deposit: { destinationChainId: number }): TransactionCostEstimate {
-    const cost = this.gasCostsByChain[deposit.destinationChainId];
-    if (!cost) {
-      // Mirror real query behavior by throwing when unavailable so caller can fall back.
-      throw new Error("MockQuery: gas cost unavailable for chain");
-    }
-    return cost;
   }
 
   getAuxiliaryNativeTokenCost(deposit: { destinationChainId: number }): BigNumber {
