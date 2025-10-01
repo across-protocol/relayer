40186ae7be4583d258cd5d07bb3da46ebff246ae
nicholaspai <npai.nyc@gmail.com>
2025-10-01 17:16:28 -0400

    Update TokenClient.ts

--- src/clients/TokenClient.ts
+++ src/clients/TokenClient.ts
@@ -90,12 +90,6 @@ export class TokenClient {
   }
 
   getUnfilledDepositAmounts(chainId: number, token: Address): BigNumber[] {
-    if (chainId === 8453 && token.eq(toAddressType("0x833589fcd6edb6e08f4c7c32d4f71b54bda02913", chainId))) {
-      return [
-        toBNWei("0.1", 6),
-        toBNWei("0.2", 6),
-      ];
-    }
     return this.unfilledDepositAmounts?.[chainId]?.[token.toNative()] || [];
   }
 
