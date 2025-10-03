0c440144118128179a57cc9038771970e4abe1a5
nicholaspai <npai.nyc@gmail.com>
2025-10-03 10:14:58 -0400

    Update UsdcCCTPBridge.ts

--- src/adapter/bridges/UsdcCCTPBridge.ts
+++ src/adapter/bridges/UsdcCCTPBridge.ts
@@ -77,7 +77,7 @@ export class UsdcCCTPBridge extends BaseBridgeAdapter {
     if (optionalParams?.fastMode) {
       ({ maxFee, finalityThreshold } = await this._getCctpV2DepositForBurnMaxFee(amount));
     }
-      const adjustedAmount = amount.add(maxFee);
+    const adjustedAmount = amount.add(maxFee);
     return Promise.resolve({
       contract: this.getL1Bridge(),
       method: "depositForBurn",
