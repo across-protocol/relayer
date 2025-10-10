140aecb048af3e9354566a701c60d76f18514b27
bennett <bennett@umaproject.org>
2025-10-10 16:46:26 -0500

    feat: rebalance USDC off of Solana L2
    Signed-off-by: bennett <bennett@umaproject.org>

---
+++ src/adapter/l2Bridges/SolanaUsdcCCTPBridge.ts
@@ -0,0 +1,144 @@
+import {
+  BigNumber,
+  bnZero,
+  Contract,
+  EventSearchConfig,
+  paginatedEventQuery,
+  Signer,
+  EvmAddress,
+  getCctpDomainForChainId,
+  TOKEN_SYMBOLS_MAP,
+  assert,
+  getCctpV1TokenMessenger,
+  getCctpV1MessageTransmitter,
+  SvmAddress,
+  getAssociatedTokenAddress,
+  getKitKeypairFromEvmSigner,
+  getCCTPDepositAccounts,
+  SVMProvider,
+} from "../../utils";
+import { BaseL2BridgeAdapter } from "./BaseL2BridgeAdapter";
+import { AugmentedTransaction } from "../../clients/TransactionClient";
+import { CCTP_MAX_SEND_AMOUNT } from "../../common";
+import { arch } from "@across-protocol/sdk";
+import { TokenMessengerMinterIdl, TokenMessengerMinterClient } from "@across-protocol/contracts";
+import { Address, generateKeyPairSigner, address, type KeyPairSigner } from "@solana/kit";
+
+export class SolanaUsdcCCTPBridge extends BaseL2BridgeAdapter {
+  private IS_CCTP_V2 = false;
+  private readonly l1UsdcTokenAddress: EvmAddress;
+  private readonly l2UsdcTokenAddress: SvmAddress;
+  private readonly solanaEventsClientPromise: Promise<arch.svm.SvmCpiEventsClient>;
+  private readonly tokenMessengerMinter: Address;
+  private readonly messageTransmitter: Address;
+  private readonly svmSignerPromise: Promise<KeyPairSigner>;
+  private svmSigner: KeyPairSigner;
+  private solanaEventsClient: arch.svm.SvmCpiEventsClient;
+
+  constructor(l2chainId: number, hubChainId: number, l2Provider: SVMProvider, l1Signer: Signer, l1Token: EvmAddress) {
+    super(l2chainId, hubChainId, l2Provider, l1Signer, l1Token);
+
+    const { address: l1TokenMessengerAddress, abi: l1Abi } = getCctpV1TokenMessenger(hubChainId);
+    this.l1Bridge = new Contract(l1TokenMessengerAddress, l1Abi, l1Signer);
+
+    this.l1UsdcTokenAddress = EvmAddress.from(TOKEN_SYMBOLS_MAP.USDC.addresses[this.hubChainId]);
+    this.l2UsdcTokenAddress = SvmAddress.from(TOKEN_SYMBOLS_MAP.USDC.addresses[this.l2chainId]);
+
+    const { address: l2TokenMessengerAddress } = getCctpV1TokenMessenger(this.l2chainId);
+    const { address: l2MessageTransmitterAddress } = getCctpV1MessageTransmitter(this.l2chainId);
+
+    this.tokenMessengerMinter = address(l2TokenMessengerAddress);
+    this.messageTransmitter = address(l2MessageTransmitterAddress);
+
+    this.solanaEventsClientPromise = arch.svm.SvmCpiEventsClient.createFor(
+      l2Provider,
+      this.tokenMessengerMinter,
+      TokenMessengerMinterIdl
+    );
+
+    this.svmSignerPromise = getKitKeypairFromEvmSigner(this.l1Signer);
+  }
+
+  private get l1DestinationDomain(): number {
+    return getCctpDomainForChainId(this.hubChainId);
+  }
+
+  async constructWithdrawToL1Txns(
+    toAddress: EvmAddress,
+    l2Token: SvmAddress,
+    l1Token: EvmAddress,
+    amount: BigNumber
+  ): Promise<AugmentedTransaction[]> {
+    assert(l1Token.eq(this.l1UsdcTokenAddress));
+    assert(l2Token.eq(this.l2UsdcTokenAddress));
+    this.svmSigner ??= await this.svmSignerPromise;
+
+    amount = amount.gt(CCTP_MAX_SEND_AMOUNT) ? CCTP_MAX_SEND_AMOUNT : amount;
+    const [cctpDepositAccounts, burnTokenAccount, messageSentEventData] = await Promise.all([
+      getCCTPDepositAccounts(
+        this.hubChainId,
+        this.l1DestinationDomain,
+        this.tokenMessengerMinter,
+        this.messageTransmitter
+      ),
+      this._getAssociatedTokenAddress(this.svmSigner.address),
+      generateKeyPairSigner(),
+    ]);
+    const {
+      tokenMessenger,
+      messageTransmitter,
+      localToken,
+      remoteTokenMessenger,
+      tokenMinter,
+      cctpEventAuthority: eventAuthority,
+      tokenMessengerMinterSenderAuthority: senderAuthorityPda,
+    } = cctpDepositAccounts;
+    const depositForBurnIx = TokenMessengerMinterClient.getDepositForBurnInstruction({
+      owner: this.svmSigner,
+      eventRentPayer: this.svmSigner,
+      senderAuthorityPda,
+      burnTokenAccount,
+      messageTransmitter,
+      tokenMessenger,
+      remoteTokenMessenger,
+      tokenMinter,
+      localToken,
+      burnTokenMint: arch.svm.toAddress(this.l2UsdcTokenAddress),
+      messageSentEventData,
+      messageTransmitterProgram: this.messageTransmitter,
+      tokenMessengerMinterProgram: this.tokenMessengerMinter,
+      eventAuthority,
+      program: this.tokenMessengerMinter,
+      amount: amount.toBigInt(),
+      destinationDomain: this.l1DestinationDomain,
+      mintRecipient: address(toAddress.toBase58()),
+    });
+    return [];
+  }
+
+  async getL2PendingWithdrawalAmount(
+    l2EventConfig: EventSearchConfig,
+    l1EventConfig: EventSearchConfig,
+    fromAddress: EvmAddress,
+    l2Token: EvmAddress
+  ): Promise<BigNumber> {
+    if (!l2Token.eq(this.l2UsdcTokenAddress)) {
+      return bnZero;
+    }
+    this.svmSigner ??= await this.svmSignerPromise;
+
+    // @dev: First parameter in MintAndWithdraw is mintRecipient, this should be the same as the fromAddress
+    // for all use cases of this adapter.
+    const l1EventFilterArgs = [fromAddress.toNative(), undefined, this.l1UsdcTokenAddress.toNative()];
+    const [withdrawalFinalizedEvents] = await Promise.all([
+      paginatedEventQuery(this.l1Bridge, this.l1Bridge.filters.MintAndWithdraw(...l1EventFilterArgs), l1EventConfig),
+    ]);
+    // const counted = new Set<number>();
+    const withdrawalAmount = bnZero;
+    return withdrawalAmount;
+  }
+
+  async _getAssociatedTokenAddress(address: Address): Promise<Address> {
+    return await getAssociatedTokenAddress(SvmAddress.from(address as string), this.l2UsdcTokenAddress);
+  }
+}
