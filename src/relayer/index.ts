4dd4eb25664e77a4cb47d7e3668f822ed76b2ffd
Aleksa Colovic <aleksa.colovic98@gmail.com>
2025-10-17 12:19:12 +0200

    Test relayer instead of Monitor

--- src/relayer/index.ts
+++ src/relayer/index.ts
@@ -63,6 +63,10 @@ export async function runRelayer(_logger: winston.Logger, baseSigner: Signer): P
 
   try {
     for (let run = 1; !stop; ++run) {
+      logger.error({
+        at: "Relayer#testPage",
+        message: "Testing @risk-labs/logger PagerDuty support",
+      });
       if (loop) {
         logger.debug({ at: "relayer#run", message: `Starting relayer execution loop ${run}.` });
       }
