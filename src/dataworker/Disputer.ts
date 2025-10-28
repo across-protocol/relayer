01810dc48737afd3745dd7729ad9632e60a51990
Paul <108695806+pxrl@users.noreply.github.com>
2025-10-28 16:12:06 +0000

    feat: Add disputer watchdog

---
+++ src/dataworker/Disputer.ts
@@ -0,0 +1,156 @@
+import { Contract, Signer } from "ethers";
+import { WETH9__factory as WETH9 } from "@across-protocol/contracts";
+import { AugmentedTransaction, TransactionClient } from "../clients";
+import {
+  BigNumber,
+  bnUint256Max,
+  bnZero,
+  formatEther,
+  getNetworkName,
+  isDefined,
+  Provider,
+  TransactionReceipt,
+  winston,
+  ZERO_BYTES,
+} from "../utils";
+
+export class Disputer {
+  private bondToken: Contract;
+  private provider: Provider;
+  private txnClient: TransactionClient;
+  private bondMultiplier: { min: number; target: number };
+  private chain: string;
+
+  constructor(
+    private readonly chainId: number,
+    private readonly logger: winston.Logger,
+    private readonly hubPool: Contract,
+    private readonly signer: Signer,
+    private readonly simulate = true
+  ) {
+    this.chain = getNetworkName(chainId);
+    this.provider = hubPool.provider;
+    this.bondMultiplier = {
+      min: 4,
+      target: 8,
+    };
+    this.txnClient = new TransactionClient(this.logger);
+  }
+
+  async init(): Promise<void> {
+    // @todo: Optimise all calls here by using Multicall3 to query:
+    // - bondToken
+    // - bondAmount
+    // - native balance
+    const [bondToken, bondAmount] = await Promise.all([this.hubPool.bondToken(), this.hubPool.bondAmount()]);
+    this.bondToken = WETH9.connect(bondToken, this.provider);
+
+    const minBondAmount = bondAmount.mul(this.bondMultiplier.min);
+
+    // Balance checks.
+    const balance = await this.balance();
+    const mintAmount = minBondAmount.sub(balance);
+    if (mintAmount.gt(bnZero)) {
+      const nativeBalance = await this.provider.getBalance(await this.signer.getAddress());
+      if (nativeBalance.lte(mintAmount)) {
+        const nativeAmount = formatEther(mintAmount);
+        if (!this.simulate) {
+          throw new Error(`Insufficient native token balance to mint ${nativeAmount} bond tokens`);
+        }
+      }
+      await this.mintBond(mintAmount);
+    }
+
+    // Ensure allowances are in place.
+    const allowance = await this.allowance();
+    const minAllowance = bondAmount.mul(this.bondMultiplier.target);
+    if (allowance.lt(minAllowance) && !this.simulate) {
+      await this.approve();
+    }
+  }
+
+  private async balance(): Promise<BigNumber> {
+    const disputer = await this.signer.getAddress();
+    return this.bondToken.balanceOf(disputer);
+  }
+
+  private async allowance(): Promise<BigNumber> {
+    const signer = await this.signer.getAddress();
+    return this.bondToken.allowance(signer, this.hubPool.address);
+  }
+
+  private async approve(amount = bnUint256Max): Promise<void> {
+    const { chainId, bondToken, hubPool } = this;
+    const txn = {
+      chainId,
+      contract: bondToken,
+      method: "approve",
+      args: [hubPool.toNative(), amount],
+      message: "Approved HubPool to spend bondToken.",
+      amount,
+      unpermissioned: false,
+      canFailInSimulation: false,
+      nonMulticall: true,
+    };
+
+    await this.submit(txn);
+  }
+
+  private async mintBond(amount: BigNumber): Promise<void> {
+    const { chainId, bondToken } = this;
+    const txn = {
+      chainId,
+      contract: bondToken,
+      method: "deposit",
+      args: [],
+      message: `Minted ${amount} HubPool bondToken.`,
+      value: amount,
+      unpermissioned: false,
+      canFailInSimulation: false,
+      nonMulticall: true,
+    };
+
+    await this.submit(txn);
+  }
+
+  dispute(): Promise<TransactionReceipt> {
+    const { chainId, hubPool } = this;
+    const txn = {
+      chainId,
+      contract: hubPool,
+      method: "dispute1",
+      args: [],
+      message: "Disputed HubPool root bundle proposal.",
+      unpermissioned: false,
+      canFailInSimulation: false,
+      nonMulticall: true,
+    };
+
+    return this.submit(txn);
+  }
+
+  private async submit(txn: AugmentedTransaction, maxTries = 3): Promise<TransactionReceipt | undefined> {
+    const { txnClient, chainId } = this;
+
+    if (this.simulate) {
+      this.logger.warn({ at: "Disputer::submit", message: `Suppressing ${txn.method} transaction.` });
+      return Promise.resolve(undefined);
+    }
+
+    let txnReceipt: TransactionReceipt;
+    let error: unknown;
+    let tries = 0;
+
+    do {
+      try {
+        const [txnResponse] = await txnClient.submit(chainId, [txn]);
+        txnReceipt = await txnResponse.wait();
+        return txnReceipt;
+      } catch (err: unknown) {
+        error = err;
+      }
+    } while (!isDefined(txnReceipt) && ++tries < maxTries);
+
+    throw new Error(`Unable to submit transaction on ${this.chain}`, { cause: error });
+  }
+}
--- src/dataworker/index.ts
+++ src/dataworker/index.ts
@@ -1,5 +1,6 @@
 import { TokenClient } from "../clients";
 import {
+  CHAIN_IDs,
   EvmAddress,
   SvmAddress,
   winston,
@@ -13,11 +14,14 @@ import {
   getSvmSignerFromEvmSigner,
   waitForPubSub,
   averageBlockTime,
+  getRedisCache,
   getRedisPubSub,
+  Provider,
 } from "../utils";
 import { spokePoolClientsToProviders } from "../common";
 import { Dataworker } from "./Dataworker";
 import { DataworkerConfig } from "./DataworkerConfig";
+import { generateValidationKey } from "./DataworkerUtils";
 import {
   constructDataworkerClients,
   constructSpokePoolClientsForFastDataworker,
@@ -25,7 +29,8 @@ import {
   DataworkerClients,
 } from "./DataworkerClientHelper";
 import { BalanceAllocator } from "../clients/BalanceAllocator";
-import { PendingRootBundle, BundleData } from "../interfaces";
+import { CachingMechanismInterface, PendingRootBundle, ProposedRootBundle, BundleData } from "../interfaces";
+import { Disputer } from "./Disputer";
 
 config();
 let logger: winston.Logger;
@@ -80,23 +85,47 @@ function resolvePersonality(config: DataworkerConfig): string {
   return "Dataworker"; // unknown
 }
 
+/**
+ * Query the details of a proposal at a given block/tag.
+ * @param provider Ethers provider instance.
+ * @param chainId HubPool chain ID.
+ * @param blockTag Block/tag to query at.
+ */
+async function getProposal(
+  provider: Provider,
+  chainId = CHAIN_IDs.MAINNET,
+  blockTag: number | "latest" = "latest"
+): Promise<
+  Pick<
+    ProposedRootBundle,
+    "poolRebalanceRoot" | "relayerRefundRoot" | "slowRelayRoot" | "challengePeriodEndTimestamp" | "proposer"
+  > & { currentTime: number; currentBlock: number }
+> {
+  const { number: currentBlock, timestamp: currentTime } = await provider.getBlock(blockTag);
+  const hubPool = getDeployedContract("HubPool", chainId).connect(provider);
+
+  const { poolRebalanceRoot, relayerRefundRoot, slowRelayRoot, proposer, challengePeriodEndTimestamp } =
+    await hubPool.rootBundleProposal({ blockTag: currentBlock });
+
+  return {
+    currentTime,
+    currentBlock,
+    poolRebalanceRoot,
+    relayerRefundRoot,
+    slowRelayRoot,
+    challengePeriodEndTimestamp,
+    proposer: EvmAddress.from(proposer),
+  };
+}
+
 async function getChallengeRemaining(
   chainId: number,
   challengeBuffer: number,
   logger: winston.Logger
 ): Promise<number> {
   const provider = await getProvider(chainId);
-  const latestBlock = await provider.getBlockNumber();
-  const hubPool = getDeployedContract("HubPool", chainId).connect(provider);
+  const { currentBlock, currentTime, ...proposal } = await getProposal(provider, chainId);
 
-  const [proposal, currentTime] = await Promise.all([
-    hubPool.rootBundleProposal({
-      blockTag: latestBlock,
-    }),
-    hubPool.getCurrentTime({
-      blockTag: latestBlock,
-    }),
-  ]);
   const { challengePeriodEndTimestamp } = proposal;
   const challengeRemaining = Math.max(challengePeriodEndTimestamp + challengeBuffer - currentTime, 0);
   logger.debug({
@@ -105,8 +134,8 @@ async function getChallengeRemaining(
     challengeRemaining,
     challengeBuffer,
     challengePeriodEndTimestamp,
+    currentBlock,
     currentTime,
-    blockTag: latestBlock,
   });
 
   return challengeRemaining;
@@ -373,5 +402,49 @@ export async function runDataworker(_logger: winston.Logger, baseSigner: Signer)
     }
   } finally {
     await disconnectRedisClients(logger);
+  }
+}
+
+export async function runDisputerWatchdog(logger: winston.Logger, signer: Signer): Promise<void> {
+  const personality = "Disputer Watchdog";
+  const at = "runDisputerWatchDog";
+  const config = new DataworkerConfig(process.env);
+  const { hubPoolChainId: hubChainId, sendingTransactionsEnabled: enabled } = config;
+
+  const { DISPUTER_WATCHDOG_MIN_ATTESTATIONS = "3", DISPUTER_WATCHDOG_CHALLENGE_LIMIT = "600" } = process.env; // @todo Watchdog config.
+  const minValidations = Number(DISPUTER_WATCHDOG_MIN_ATTESTATIONS);
+  const challengeLimit = Number(DISPUTER_WATCHDOG_CHALLENGE_LIMIT);
+
+  const provider = await getProvider(hubChainId, logger);
+  const hubPool = getDeployedContract("HubPool", hubChainId).connect(provider);
+  const disputer = new Disputer(hubChainId, logger, hubPool, signer, !enabled);
+
+  logger.debug({ at, message: "Starting Disputer Watchdog." });
+
+  try {
+    await disputer.init();
+    const redis = await getRedisCache(logger);
+
+    const { currentTime, currentBlock, ...proposal } = await getProposal(provider, hubChainId);
+
+    const getValidations = async () => {
+      const key = generateValidationKey(proposal);
+      const result = await redis.get<string>(key);
+      return Number(result) || 0; // Revert to 0 on isNaN(result)
+    };
+
+    // @todo Validate that currentTime is not too different from host time.
+    const challengeRemaining = proposal.challengePeriodEndTimestamp - currentTime;
+    if (challengeRemaining <= 0) {
+      return;
+    }
+
+    const validations = await getValidations();
+    if (challengeRemaining < challengeLimit && validations < minValidations) {
+      await disputer.dispute();
+    }
+  } finally {
+    await disconnectRedisClients(logger);
+    logger.debug({ at, message: `Completed ${personality} run.` });
   }
 }
