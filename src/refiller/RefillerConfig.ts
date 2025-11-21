b567c46889c0634809cdbe7227e10be5ea6433fa
bennett <bennett@umaproject.org>
2025-11-21 15:43:06 -0600

    improve: add a minimum USDH rebalance amount
    Signed-off-by: bennett <bennett@umaproject.org>

--- src/refiller/RefillerConfig.ts
+++ src/refiller/RefillerConfig.ts
@@ -1,5 +1,5 @@
 import { CommonConfig, ProcessEnv } from "../common";
-import { getNativeTokenAddressForChain, Address, toAddressType, isDefined } from "../utils";
+import { getNativeTokenAddressForChain, Address, toAddressType, isDefined, toBN, BigNumber } from "../utils";
 
 export type RefillBalanceData = {
   chainId: number;
@@ -14,11 +14,12 @@ export type RefillBalanceData = {
 export class RefillerConfig extends CommonConfig {
   readonly refillEnabledBalances: RefillBalanceData[] = [];
   readonly nativeMarketsApiConfig: { apiKey: string; apiUrl: string };
+  readonly minUsdhRebalanceAmount: BigNumber;
 
   constructor(env: ProcessEnv) {
     super(env);
 
-    const { REFILL_BALANCES, NATIVE_MARKETS_API_KEY, NATIVE_MARKETS_API_BASE } = env;
+    const { REFILL_BALANCES, NATIVE_MARKETS_API_KEY, NATIVE_MARKETS_API_BASE, MIN_USDH_REBALANCE_AMOUNT } = env;
 
     // Used to send tokens if available in wallet to balances under target balances.
     if (REFILL_BALANCES) {
@@ -50,6 +51,9 @@ export class RefillerConfig extends CommonConfig {
     if (isDefined(NATIVE_MARKETS_API_KEY) && isDefined(NATIVE_MARKETS_API_BASE)) {
       this.nativeMarketsApiConfig = { apiKey: NATIVE_MARKETS_API_KEY, apiUrl: NATIVE_MARKETS_API_BASE };
     }
+
+    // Default minimum is 10 USDH
+    this.minUsdhRebalanceAmount = toBN(Number(MIN_USDH_REBALANCE_AMOUNT ?? 10_000_000));
 
     // Should only have 1 HubPool.
     if (Object.values(this.refillEnabledBalances).filter((x) => x.isHubPool).length > 1) {
