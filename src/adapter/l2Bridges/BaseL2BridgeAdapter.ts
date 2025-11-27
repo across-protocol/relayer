8300bc00978e31f8e041015b53737d2f2afbc705
Aleksa Colovic <aleksa.colovic98@gmail.com>
2025-11-27 15:21:26 +0100

    prepare for initial review

--- src/adapter/l2Bridges/BaseL2BridgeAdapter.ts
+++ src/adapter/l2Bridges/BaseL2BridgeAdapter.ts
@@ -10,19 +10,14 @@ import {
   getSpokePoolAddress,
   SVMProvider,
   SolanaTransaction,
-  isDefined,
 } from "../../utils";
 import { TransferTokenParams } from "../utils";
 
 export abstract class BaseL2BridgeAdapter {
   protected l2Bridge: Contract;
   protected l1Bridge: Contract;
-  protected l2DstBridge: Contract | undefined;
   protected readonly hubPoolAddress: EvmAddress;
   protected readonly spokePoolAddress: Address;
-  protected readonly dstSpokePoolAddress: Address | undefined;
-  protected readonly l2DstSigner: Signer | undefined;
-  protected readonly dstSvmProvider: SVMProvider | undefined;
   // Whether either of these two are defined is determined at construction.
   // The solana bridge defines `svmProvider` while the EVM bridges define `l2Signer`.
   protected readonly l2Signer: Signer | undefined;
@@ -33,9 +28,7 @@ export abstract class BaseL2BridgeAdapter {
     protected hubChainId: number,
     protected l2SignerOrSvmProvider: Signer | SVMProvider,
     protected l1Signer: Signer,
-    protected l1Token: EvmAddress,
-    protected l2dstChainId?: number,
-    protected l2DstSignerOrSvmProvider?: Signer | SVMProvider
+    protected l1Token: EvmAddress
   ) {
     this.hubPoolAddress = getHubPoolAddress(hubChainId);
     this.spokePoolAddress = getSpokePoolAddress(l2chainId);
@@ -43,15 +36,6 @@ export abstract class BaseL2BridgeAdapter {
       this.l2Signer = l2SignerOrSvmProvider satisfies Signer;
     } else {
       this.svmProvider = l2SignerOrSvmProvider satisfies SVMProvider;
-    }
-
-    if (isDefined(l2dstChainId) && isDefined(l2DstSignerOrSvmProvider)) {
-      this.dstSpokePoolAddress = getSpokePoolAddress(l2dstChainId);
-      if (l2DstSignerOrSvmProvider instanceof Signer) {
-        this.l2DstSigner = l2DstSignerOrSvmProvider satisfies Signer;
-      } else {
-        this.dstSvmProvider = l2DstSignerOrSvmProvider satisfies SVMProvider;
-      }
     }
   }
 
--- src/adapter/l2Bridges/UsdcCCTPBridge.ts
+++ src/adapter/l2Bridges/UsdcCCTPBridge.ts
@@ -30,18 +30,9 @@ import { TransferTokenParams } from "../utils";
 export class UsdcCCTPBridge extends BaseL2BridgeAdapter {
   private readonly l1UsdcTokenAddress: EvmAddress;
   private readonly l2UsdcTokenAddress: EvmAddress;
-  private readonly l2DstUsdcTokenAddress: EvmAddress;
 
-  constructor(
-    l2chainId: number,
-    hubChainId: number,
-    l2Signer: Signer,
-    l1Signer: Signer,
-    l1Token: EvmAddress,
-    l2dstChainId?: number,
-    l2dstSigner?: Signer
-  ) {
-    super(l2chainId, hubChainId, l2Signer, l1Signer, l1Token, l2dstChainId, l2dstSigner);
+  constructor(l2chainId: number, hubChainId: number, l2Signer: Signer, l1Signer: Signer, l1Token: EvmAddress) {
+    super(l2chainId, hubChainId, l2Signer, l1Signer, l1Token);
 
     const { address: l2TokenMessengerAddress, abi: l2TokenMessengerAbi } = getCctpV2TokenMessenger(l2chainId);
     this.l2Bridge = new Contract(l2TokenMessengerAddress, l2TokenMessengerAbi, l2Signer);
@@ -51,23 +42,10 @@ export class UsdcCCTPBridge extends BaseL2BridgeAdapter {
 
     this.l1UsdcTokenAddress = EvmAddress.from(TOKEN_SYMBOLS_MAP.USDC.addresses[this.hubChainId]);
     this.l2UsdcTokenAddress = EvmAddress.from(TOKEN_SYMBOLS_MAP.USDC.addresses[this.l2chainId]);
-
-    if (isDefined(this.l2dstChainId) && isDefined(this.l2DstSigner)) {
-      const { address: l2DstTokenMessengerAddress, abi: l2DstTokenMessengerAbi } = getCctpV2TokenMessenger(
-        this.l2dstChainId
-      );
-      this.l2DstBridge = new Contract(l2DstTokenMessengerAddress, l2DstTokenMessengerAbi, this.l2DstSigner);
-      this.l2DstUsdcTokenAddress = EvmAddress.from(TOKEN_SYMBOLS_MAP.USDC.addresses[this.l2dstChainId]);
-    }
   }
 
   private get l1DestinationDomain(): number {
     return getCctpDomainForChainId(this.hubChainId);
-  }
-
-  private get l2DstDestinationDomain(): number {
-    assert(isDefined(this.l2dstChainId), "Destination L2 chain must be configured");
-    return getCctpDomainForChainId(this.l2dstChainId);
   }
 
   async constructWithdrawToL1Txns(
@@ -104,76 +82,6 @@ export class UsdcCCTPBridge extends BaseL2BridgeAdapter {
         args: [
           amountToSend,
           this.l1DestinationDomain,
-          toAddress.toBytes32(),
-          this.l2UsdcTokenAddress.toNative(),
-          ethers.constants.HashZero, // Anyone can finalize the message on domain when this is set to bytes32(0)
-          maxFee,
-          finalityThreshold,
-        ],
-      },
-    ]);
-  }
-
-  /**
-   * Constructs a transaction to send USDC from this L2 chain to another L2 chain via CCTP.
-   * This is similar to L2->L1 transfers, but uses the destination L2's CCTP domain instead of L1's.
-   * The destination L2 chain must be configured in the constructor via l2dstChainId and l2dstSigner.
-   *
-   * @param toAddress Recipient address on the destination L2 chain
-   * @param l2SrcToken Source L2 token address (should be USDC on this chain)
-   * @param l2DstToken Destination L2 token address (should be USDC on this chain)
-   * @param amount Amount to transfer
-   * @param optionalParams Optional parameters including fastMode
-   * @returns Array of augmented transactions
-   */
-  async constructL2ToL2Txn(
-    toAddress: EvmAddress,
-    l2SrcToken: EvmAddress,
-    l2DstToken: EvmAddress,
-    amount: BigNumber,
-    optionalParams?: TransferTokenParams
-  ): Promise<AugmentedTransaction[]> {
-    assert(
-      isDefined(this.l2dstChainId) && isDefined(this.l2DstSigner),
-      "Destination L2 chain must be configured in constructor (l2dstChainId and l2dstSigner)"
-    );
-    assert(l2SrcToken.eq(this.l2UsdcTokenAddress), "Source token must be USDC on source L2 chain");
-    assert(l2DstToken.eq(this.l2DstUsdcTokenAddress), "Destination token must be USDC on destination L2 chain");
-    assert(this.l2dstChainId !== this.l2chainId, "Source and destination chains must be different");
-    assert(this.l2dstChainId !== this.hubChainId, "Use constructWithdrawToL1Txns for L2->L1 transfers");
-
-    const { decimals } = getTokenInfo(l2SrcToken, this.l2chainId);
-    const formatter = createFormatFunction(2, 4, false, decimals);
-
-    amount = amount.gt(CCTP_MAX_SEND_AMOUNT) ? CCTP_MAX_SEND_AMOUNT : amount;
-    let maxFee = bnZero,
-      finalityThreshold = CCTPV2_FINALITY_THRESHOLD_STANDARD;
-    if (optionalParams?.fastMode) {
-      // For L2->L2, we need to calculate fees based on source and destination L2 chains
-      ({ maxFee, finalityThreshold } = await this._getCctpV2DepositForBurnMaxFeeL2ToL2(amount));
-    }
-    // Add maxFee so that we end up with desired amount of tokens on destination chain.
-    const amountWithFee = amount.add(maxFee);
-    const amountToSend = amountWithFee.gt(CCTP_MAX_SEND_AMOUNT) ? CCTP_MAX_SEND_AMOUNT : amountWithFee;
-
-    const sourceChainName = getNetworkName(this.l2chainId);
-    const destinationChainName = getNetworkName(this.l2dstChainId!);
-
-    return Promise.resolve([
-      {
-        contract: this.l2Bridge,
-        chainId: this.l2chainId,
-        method: "depositForBurn",
-        nonMulticall: true,
-        message: `🎰 Bridged CCTP USDC from ${sourceChainName} to ${destinationChainName}${
-          optionalParams?.fastMode ? " using fast mode" : ""
-        }`,
-        mrkdwn: `Bridged ${formatter(amountToSend)} USDC from ${sourceChainName} to ${destinationChainName} via CCTP${
-          optionalParams?.fastMode ? ` using fast mode with a max fee of ${formatter(maxFee)}` : ""
-        }`,
-        args: [
-          amountToSend,
-          this.l2DstDestinationDomain,
           toAddress.toBytes32(),
           this.l2UsdcTokenAddress.toNative(),
           ethers.constants.HashZero, // Anyone can finalize the message on domain when this is set to bytes32(0)
@@ -232,16 +140,5 @@ export class UsdcCCTPBridge extends BaseL2BridgeAdapter {
 
   async _getCctpV2DepositForBurnMaxFee(amount: BigNumber): Promise<{ maxFee: BigNumber; finalityThreshold: number }> {
     return getV2DepositForBurnMaxFee(this.l2UsdcTokenAddress, this.l2chainId, this.hubChainId, amount);
-  }
-
-  /**
-   * Get max fee for L2->L2 CCTP transfers.
-   * Uses the same fee calculation as L2->L1, but with destination L2 chain instead of L1.
-   */
-  async _getCctpV2DepositForBurnMaxFeeL2ToL2(
-    amount: BigNumber
-  ): Promise<{ maxFee: BigNumber; finalityThreshold: number }> {
-    assert(isDefined(this.l2dstChainId), "Destination L2 chain must be configured in constructor");
-    return getV2DepositForBurnMaxFee(this.l2UsdcTokenAddress, this.l2chainId, this.l2dstChainId, amount);
   }
 }
