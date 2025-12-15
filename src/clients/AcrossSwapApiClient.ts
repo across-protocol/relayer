31d71af84c3f94bb6264392a3efa15ec76718f04
Paul <108695806+pxrl@users.noreply.github.com>
2025-12-15 16:07:02 +0000

    lint

--- src/clients/AcrossSwapApiClient.ts
+++ src/clients/AcrossSwapApiClient.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from "axios";
-import { BigNumber, bnZero, EvmAddress, winston } from "../utils";
+import { BigNumber, EvmAddress, winston } from "../utils";
 import { SWAP_ROUTES, SwapRoute } from "../common";
 
 interface SwapApiResponse {
