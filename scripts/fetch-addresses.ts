6fc3e9a00f4f5ba30cd478e6b428c8728f81f97c
Paul <108695806+pxrl@users.noreply.github.com>
2025-12-16 17:12:38 +0000

    lint

--- scripts/fetch-addresses.ts
+++ scripts/fetch-addresses.ts
@@ -7,9 +7,7 @@ const OUTPUT_PATH = "addresses.json";
 let logger: typeof Logger;
 
 async function run(): Promise<number> {
-  const {
-    RISKLABS_ADDRESS_LIST_HOSTNAME: path = "https://blacklist.risklabs.foundation/api/blacklist",
-  } = process.env;
+  const { RISKLABS_ADDRESS_LIST_HOSTNAME: path = "https://blacklist.risklabs.foundation/api/blacklist" } = process.env;
   const addressList = new AddressAggregator(
     [new addressAdapters.risklabs.AddressList({ path, throwOnError: false })],
     logger
