12c825c521463d156e4c812384c6526b37c7af54
Aleksa Colovic <aleksa.colovic98@gmail.com>
2025-12-19 11:34:57 +0100

    feat: Add new tokens on Ink and Unichain

--- src/common/ContractAddresses.ts
+++ src/common/ContractAddresses.ts
@@ -476,6 +476,14 @@ export const CONTRACT_ADDRESSES: {
     nativeToken: {
       address: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
     },
+    cctpV2MessageTransmitter: {
+      address: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
+      abi: CCTP_MESSAGE_TRANSMITTER_ABI,
+    },
+    cctpV2TokenMessenger: {
+      address: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
+      abi: CCTP_V2_TOKEN_MESSENGER_ABI,
+    },
   },
   [CHAIN_IDs.BLAST]: {
     ovmStandardBridge: {
