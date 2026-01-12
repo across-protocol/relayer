4b084a7d0c2a3a5b5e8d6c05ab74e7b7dc0087f9
nicholaspai <npai.nyc@gmail.com>
2026-01-12 12:05:53 -0500

    Add rebalancer client getPendingRebalances() function

--- src/rebalancer/rebalancer.ts
+++ src/rebalancer/rebalancer.ts
@@ -3,6 +3,7 @@ import {
   assert,
   BigNumber,
   bnUint256Max,
+  bnZero,
   ConvertDecimals,
   forEachAsync,
   getNetworkName,
@@ -371,40 +372,26 @@ export class RebalancerClient {
 
       await this.adapters[rebalanceRouteToUse.adapter].initializeRebalance(rebalanceRouteToUse, amountToTransfer);
     }
+  }
 
-    // Setup:
-    // - We can only rebalance via rebalance routes from source chains + tokens to destination chains + tokens.
-    // - From the rebalance route list, load all chains and tokens we need to query:
-    //   - Set of all source chains + source tokens
-    //   - Set of all destination chains + destination tokens
-    // - For each chain + token:
-    //   - Load all current balances per chain including pending rebalances.
-    //   - We should define these balances in $ terms to normalize them, therefore we'll need a price oracle for each token.
-    // Run:
-    // - Identify list of source chains that have EXCESS balances.
-    // - Identify list of destination chains that have DEFICIT balances.
-    // - Our goal is to attempt to send funds from excess balances to refill deficit balances.
-    // - We do not attempt to reduce excess balances unless there is a deficit to refill. Absent anu deficits,
-    //   excesses are ok.
-    // - The user might find it helpful to define a chain as a "universal source" by setting its target balance to 0,
-    //   therefore that chain always has an excess balance.
-    // - Similarly, the user might find it helpful to define a "universal sink" by setting its target balance to positive
-    //   infinity, therefore that chain always has a deficit balance.
-    // - For each DEFICIT chain, query all rebalance routes TO that chain that have an "excess" balance
-    //   large enough to cover the deficit (excess = current balance - target balance).
-    //    - Note: This should include both swap and bridge routes, so you could theoretically fill deficits in USDC using
-    //      by drawing from excesses in USDT.
-    // - If resultant route list is empty, log a warning.
-    // - Otherwise, call initializeRebalance() for that route using the "cheapest" route whose cost is under the
-    //   max allowable cost. We'll need to query route.adapter.getCostEstimate to figure out the cost of the route.
-    // - To avoid duplicate rebalances, the adapters should correctly implement getPendingRebalances() so that this
-    //   client computes current balances correctly.
-
-    // for (const deficit of  deficits) {
-    //   const routes = this.getRebalanceRoutesToChain(deficit.chain, deficit.token, deficit.amount);
-    //   const cheapestRoute = routes.filter((route) => route.adapter.getCostEstimate(route) < maxCost).sort((a, b) => a.cost - b.cost)[0];
-    //   await this.adapters.hyperliquid.initializeRebalance(cheapestRoute);
-    // }
+  /**
+   * @notice Get all currently unfinalized rebalance amounts. Should be used to add virtual balance credits or
+   * debits for the token and chain combinations.
+   * @return Dictionary of chainId -> token -> amount where positive amounts present pending rebalance credits to that
+   * chain while negative amounts represent debits that should be subtracted from that chain's current balance.
+   */
+  async getPendingRebalances(): Promise<{ [chainId: number]: { [token: string]: BigNumber } }> {
+    const pendingRebalances: { [chainId: number]: { [token: string]: BigNumber } } = {};
+    await forEachAsync(Object.values(this.adapters), async (adapter) => {
+      const pending = await adapter.getPendingRebalances();
+      Object.entries(pending).forEach(([chainId, tokenBalance]) => {
+        Object.entries(tokenBalance).forEach(([token, amount]) => {
+          pending[chainId] ??= {};
+          pending[chainId][token] = (pending[chainId]?.[token] ?? bnZero).add(amount);
+        });
+      });
+    });
+    return pendingRebalances;
   }
 }
 
