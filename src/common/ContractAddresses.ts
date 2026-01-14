7b36dc490c05f24ca6bafd45eb0b3770a130d8cb
Aleksa Colovic <aleksa.colovic98@gmail.com>
2026-01-14 11:19:52 +0100

    feat: Add MegaETH

--- src/common/ContractAddresses.ts
+++ src/common/ContractAddresses.ts
@@ -112,6 +112,10 @@ export const CONTRACT_ADDRESSES: {
       address: "0x470458C91978D2d929704489Ad730DC3E3001113",
       abi: OVM_L1_STANDARD_BRIDGE_ABI,
     },
+    ovmStandardBridge_4326: {
+      address: "0x0CA3A2FBC3D770b578223FBB6b062fa875a2eE75",
+      abi: OVM_L1_STANDARD_BRIDGE_ABI,
+    },
     ovmStandardBridge_690: {
       address: "0xc473ca7E02af24c129c2eEf51F2aDf0411c1Df69",
       abi: OVM_L1_STANDARD_BRIDGE_ABI,
@@ -459,6 +463,15 @@ export const CONTRACT_ADDRESSES: {
     },
   },
   [CHAIN_IDs.MODE]: {
+    ovmStandardBridge: {
+      address: "0x4200000000000000000000000000000000000010",
+      abi: OVM_L2_STANDARD_BRIDGE_ABI,
+    },
+    nativeToken: {
+      address: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+    },
+  },
+  [CHAIN_IDs.MEGAETH]: {
     ovmStandardBridge: {
       address: "0x4200000000000000000000000000000000000010",
       abi: OVM_L2_STANDARD_BRIDGE_ABI,
