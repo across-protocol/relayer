dfba60cc0f24a67cc38b6c6cc1857a99314d22f1
dijanin-brat <106677800+dijanin-brat@users.noreply.github.com>
2026-01-18 14:19:22 +0100

    fix: Lower block range for MegaETH (#2889)
    Quicknode has a limit of 10k block range so we need to lower the range
    for MegaETH.

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -154,7 +154,6 @@ const resolveRpcConfig = () => {
     [CHAIN_IDs.ALEPH_ZERO]: 0,
     [CHAIN_IDs.BOBA]: 0,
     [CHAIN_IDs.HYPEREVM]: 1_000, // QuickNode constraint.
-    [CHAIN_IDs.MEGAETH]: 20_000,
     [CHAIN_IDs.MONAD]: 1_000, // Alchemy constraint
     [CHAIN_IDs.SOLANA]: 1_000,
     [CHAIN_IDs.SOLANA_DEVNET]: 1000,
