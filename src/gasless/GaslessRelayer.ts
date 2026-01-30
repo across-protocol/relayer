e9c2d767dc6fad7806de159fdc79422bd4054570
bmzig <57361391+bmzig@users.noreply.github.com>
2026-01-30 12:29:30 -0600

    feat: gasless relayer skeleton (#2907)
    * feat: gasless relayer skeleton
    
    Signed-off-by: bennett <bennett@umaproject.org>
    
    * remove defn
    
    Signed-off-by: bennett <bennett@umaproject.org>
    
    ---------
    
    Signed-off-by: bennett <bennett@umaproject.org>

---
+++ src/gasless/GaslessRelayer.ts
@@ -0,0 +1,76 @@
+import winston from "winston";
+import { GaslessRelayerConfig } from "./GaslessRelayerConfig";
+import { isDefined, getRedisCache, delay, Signer } from "../utils";
+
+const abortController = new AbortController();
+
+/**
+ * Independent relayer bot which processes EIP-3009 signatures into deposits and corresponding fills.
+ */
+export class GaslessRelayer {
+  private initialized = false;
+
+  public constructor(
+    readonly logger: winston.Logger,
+    readonly config: GaslessRelayerConfig,
+    readonly baseSigner: Signer
+  ) {}
+
+  /*
+   * @notice Initializes a GaslessRelayer instance.
+   */
+  public async initialize(): Promise<void> {
+    process.on("SIGHUP", () => {
+      this.logger.debug({
+        at: "GaslessRelayer#initialize",
+        message: "Received SIGHUP on gasless relayer. stopping...",
+      });
+      abortController.abort();
+    });
+
+    process.on("disconnect", () => {
+      this.logger.debug({
+        at: "GaslessRelayer#initialize",
+        message: "Gasless relayer disconnected, stopping...",
+      });
+      abortController.abort();
+    });
+    this.initialized = true;
+  }
+
+  /*
+   * @notice Utility function which tells the relayer when a handoff has occurred.
+   * Calls the abort controller and settles this function's promise once a handoff is observed.
+   */
+  public async waitForDisconnect(): Promise<void> {
+    const {
+      RUN_IDENTIFIER: runIdentifier,
+      BOT_IDENTIFIER: botIdentifier,
+      MAX_CYCLES: _maxCycles = 120,
+      DISCONNECT_POLLING_DELAY: _pollingDelay = 3,
+    } = process.env;
+    const maxCycles = Number(_maxCycles);
+    const pollingDelay = Number(_pollingDelay);
+    const redis = await getRedisCache(this.logger);
+    // Set the active instance immediately on arrival here. This function will poll until it reaches the max amount of
+    // runs or it is interrupted by another process.
+    if (isDefined(runIdentifier) && isDefined(botIdentifier)) {
+      await redis.set(botIdentifier, runIdentifier, maxCycles * pollingDelay);
+      for (let run = 0; run < maxCycles; run++) {
+        const currentBot = await redis.get(botIdentifier);
+        if (currentBot !== runIdentifier) {
+          this.logger.debug({
+            at: "GaslessRelayer#waitForDisconnect",
+            message: `Handing over ${runIdentifier} instance to ${currentBot} for ${botIdentifier}`,
+            run,
+          });
+          abortController.abort();
+          return;
+        }
+        await delay(pollingDelay);
+      }
+      // If we finish looping without receiving a handover signal, still exit so that we won't await the other promise forever.
+      abortController.abort();
+    }
+  }
+}
