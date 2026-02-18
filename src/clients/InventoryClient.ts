2646e2910a270d49bf21ac9f1f54fc46704c6fb4
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2026-02-18 18:00:49 -0500

    Revert "!feat: Relayer with `allowedSwapRoutes` can only fill in-protocol swaps (#2966)" (#2967)
    This reverts commit d47a630a6bc9da6194757d2fe0ea97f01dc7e2fb.
    
    The reason we should revert this is that this PR doesn't allow an inventoryConfig with an allowedSwapRoutes config to be used by a relayer bot instance that supports non-swaps and swaps.
    
    Ideally, we can have a single bot instance that supports both (and prices them differently in the ProfitClient)

--- src/clients/InventoryClient.ts
+++ src/clients/InventoryClient.ts
@@ -545,12 +545,6 @@ export class InventoryClient {
   validateOutputToken(deposit: Deposit): boolean {
     const { inputToken, outputToken, originChainId, destinationChainId } = deposit;
 
-    // If swaps are supported, then only return true if the deposit represents an allowed in-protocol swap:
-    if (this.supportsSwaps()) {
-      // Return true if the input and output tokens are defined as equivalent according to a user-defined swap config.
-      return this.isSwapSupported(inputToken, outputToken, originChainId, destinationChainId);
-    }
-
     // Return true if input and output tokens are mapped to the same L1 token via PoolRebalanceRoutes
     const equivalentTokens = this.hubPoolClient.areTokensEquivalent(
       inputToken,
@@ -559,6 +553,11 @@ export class InventoryClient {
       destinationChainId
     );
     if (equivalentTokens) {
+      return true;
+    }
+
+    // Return true if the input and output tokens are defined as equivalent according to a user-defined swap config.
+    if (this.isSwapSupported(inputToken, outputToken, originChainId, destinationChainId)) {
       return true;
     }
 
