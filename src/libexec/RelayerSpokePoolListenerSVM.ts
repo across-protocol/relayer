29f6daf4b382f24b602060c8be8eebccdde1502b
Paul <108695806+pxrl@users.noreply.github.com>
2026-02-18 00:11:49 +0000

    Simplify config

--- src/libexec/RelayerSpokePoolListenerSVM.ts
+++ src/libexec/RelayerSpokePoolListenerSVM.ts
@@ -122,7 +122,7 @@ async function listen(
 
   // Default keepalive interval is 5s but this can cause premature hangup.
   // See https://github.com/anza-xyz/agave/issues/7022
-  const intervalMs = Number(process.env.RELAYER_SVM_WS_KEEPALIVE ?? 10_000);
+  const intervalMs = Number(process.env.RELAYER_SVM_WS_KEEPALIVE ?? 10) * 1000;
   const providers = urls.map((url) => createSolanaRpcSubscriptions(url, { intervalMs }));
 
   const readSlot = async (provider: WSProvider, providerName: string) => {
