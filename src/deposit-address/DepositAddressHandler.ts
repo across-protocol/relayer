3edc497a10b6919ac4367905e04dcc89801c9f45
Aleksa Colovic <aleksa.colovic98@gmail.com>
2026-02-27 15:02:56 +0100

    add swapApiKey

--- src/deposit-address/DepositAddressHandler.ts
+++ src/deposit-address/DepositAddressHandler.ts
@@ -54,7 +54,7 @@ export class DepositAddressHandler {
     readonly baseSigner: Signer,
     readonly depositAddressSigners: Signer[]
   ) {
-    this.api = new AcrossSwapApiClient(this.logger, this.config.apiTimeoutOverride);
+    this.api = new AcrossSwapApiClient(this.logger, this.config.apiTimeoutOverride, this.config.swapApiKey);
     this.indexerApi = new AcrossIndexerApiClient(this.logger, this.config.apiTimeoutOverride);
     this.transactionClient = new TransactionClient(this.logger, depositAddressSigners);
   }
