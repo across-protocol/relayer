1ed7f064d5e07c4f5a7a85253e0a6fce47d68639
nicholaspai <npai.nyc@gmail.com>
2026-02-27 11:48:55 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -84,7 +84,6 @@ if (require.main === module) {
   let exitCode = 0;
   run(args)
     .catch(async (error) => {
-      console.error(error)
       exitCode = 1;
       const stringifiedError = stringifyThrownValue(error);
       logger.error({
