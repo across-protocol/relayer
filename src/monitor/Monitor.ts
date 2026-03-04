b1b278eab4a9b59b5f90cacee792ad0010740581
nicholaspai <npai.nyc@gmail.com>
2026-03-04 09:59:15 -0500

    Update Monitor.ts

--- src/monitor/Monitor.ts
+++ src/monitor/Monitor.ts
@@ -1282,6 +1282,7 @@ export class Monitor {
       // getTotalPendingWithdrawalAmount() async call is getting rate limited by the Binance API.
       // We should add more rate limiting or retry logic to this call.
     );
+    const allPendingWithdrawalBalances: { [l1Token: string]: { [chainId: number]: BigNumber } } = {};
     await Promise.all(
       allL1Tokens.map(async (l1Token) => {
         const pendingWithdrawalBalances =
@@ -1290,6 +1291,7 @@ export class Monitor {
             relayer,
             l1Token.address
           );
+        allPendingWithdrawalBalances[l1Token.symbol] = pendingWithdrawalBalances;
         for (const _chainId of Object.keys(pendingWithdrawalBalances)) {
           const chainId = Number(_chainId);
           if (pendingWithdrawalBalances[chainId].eq(bnZero)) {
@@ -1315,6 +1317,11 @@ export class Monitor {
         }
       })
     );
+    this.logger.debug({
+      at: "Monitor#updatePendingL2Withdrawals",
+      message: "Updated pending L2->L1 withdrawals",
+      allPendingWithdrawalBalances,
+    });
   }
 
   async updatePendingRebalances(relayer: Address, relayerBalanceTable: RelayerBalanceTable): Promise<void> {
