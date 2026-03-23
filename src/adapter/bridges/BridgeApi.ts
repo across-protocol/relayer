615edded8c40f4eb770b0cf5b0e5d26e26fe0a24
bennett <bennett@umaproject.org>
2026-03-23 10:31:40 -0500

    refactor: move common bridge api functions to utils file
    Signed-off-by: bennett <bennett@umaproject.org>

--- src/adapter/bridges/BridgeApi.ts
+++ src/adapter/bridges/BridgeApi.ts
@@ -10,59 +10,24 @@ import {
   EvmAddress,
   winston,
   toBN,
+  bnZero,
   getNetworkName,
   CHAIN_IDs,
-  TOKEN_SYMBOLS_MAP,
   getTimestampForBlock,
   groupObjectCountsByProp,
   toAddressType,
   getTokenInfo,
-  delay,
   isDefined,
+  BridgeApiClient,
+  BRIDGE_API_MINIMUMS,
+  BRIDGE_API_DESTINATION_TOKENS,
+  BRIDGE_API_DESTINATION_TOKEN_SYMBOLS,
 } from "../../utils";
 import { TransferTokenParams } from "../utils";
 import ERC20_ABI from "../../common/abi/MinimalERC20.json";
-import axios, { AxiosHeaders } from "axios";
-
-export const BRIDGE_API_MINIMUMS: { [l2ChainId: number]: BigNumber } = {
-  [CHAIN_IDs.MAINNET]: toBN(5_000_000), // 5 USDC
-  [CHAIN_IDs.TEMPO]: toBN(5_000_000), // 5 USDC
-};
-
-export type BridgeResponse = {
-  state: string;
-  created_at: string;
-  updated_at: string;
-  destination: {
-    payment_rail: string;
-    currency: string;
-    to_address: string;
-  };
-  source_deposit_instructions: {
-    payment_rail: string;
-    currency: string;
-    to_address: string;
-    from_address: string;
-  };
-  receipt:
-    | {
-        initial_amount: string;
-        final_amount: string;
-        source_tx_hash: string;
-      }
-    | undefined;
-};
-
-// We need to instruct this bridge what tokens we expect to receive on L2, since the bridge
-// API supports multiple destination tokens for a single L1 token.
-const BRIDGE_API_DESTINATION_TOKENS: { [l2ChainId: number]: string } = {
-  [CHAIN_IDs.TEMPO]: TOKEN_SYMBOLS_MAP.pathUSD.addresses[CHAIN_IDs.TEMPO],
-};
 
 export class BridgeApi extends BaseBridgeAdapter {
-  protected bridgeApiBase: string;
-  protected bridgeApiKey: string;
-  protected customerId: string;
+  protected api: BridgeApiClient;
   protected l1TokenInfo: TokenInfo;
 
   constructor(
@@ -87,9 +52,14 @@ export class BridgeApi extends BaseBridgeAdapter {
     assert(isDefined(BRIDGE_API_KEY), "BRIDGE_API_KEY must be set in the environment");
     assert(isDefined(BRIDGE_CUSTOMER_ID), "BRIDGE_CUSTOMER_ID must be set in the environment");
 
-    this.bridgeApiBase = BRIDGE_API_BASE;
-    this.bridgeApiKey = BRIDGE_API_KEY;
-    this.customerId = BRIDGE_CUSTOMER_ID;
+    this.api = new BridgeApiClient(
+      BRIDGE_API_BASE,
+      BRIDGE_API_KEY,
+      BRIDGE_CUSTOMER_ID,
+      this.hubChainId,
+      this.l2chainId,
+      this.logger
+    );
 
     this.l1TokenInfo = getTokenInfo(l1Token, this.hubChainId);
   }
@@ -107,10 +77,14 @@ export class BridgeApi extends BaseBridgeAdapter {
       `Attempting to bridge unsupported l2 token ${l2Token.toNative()}`
     );
     // If amount is less than the network minimums, then throw.
-    if (amount.lt(BRIDGE_API_MINIMUMS[this.l2chainId])) {
+    if (amount.lt(BRIDGE_API_MINIMUMS[this.hubChainId]?.[this.l2chainId] ?? bnZero)) {
       throw new Error(`Cannot bridge to ${getNetworkName(this.l2chainId)} due to invalid amount ${amount}`);
     }
-    const transferRouteAddress = await this.createTransferRouteEscrowAddress(toAddress);
+    const transferRouteAddress = await this.api.createTransferRouteEscrowAddress(
+      toAddress,
+      this.l1TokenInfo.symbol,
+      BRIDGE_API_DESTINATION_TOKEN_SYMBOLS[this.getL2Bridge().address]
+    );
     return Promise.resolve({
       contract: this.getL1Bridge(),
       method: "transfer",
@@ -125,7 +99,7 @@ export class BridgeApi extends BaseBridgeAdapter {
     eventConfig: EventSearchConfig
   ): Promise<BridgeEvents> {
     const fromTimestamp = await getTimestampForBlock(this.l1Signer.provider, eventConfig.from);
-    const { data: pendingTransfers } = await this._getAllTransfersInRange(toAddress, fromTimestamp * 1000);
+    const { data: pendingTransfers } = await this.api.getAllTransfersInRange(toAddress, fromTimestamp * 1000);
 
     const statusesGrouped = groupObjectCountsByProp(pendingTransfers, (pendingTransfer) => pendingTransfer.state);
     this.logger.debug({
@@ -167,89 +141,5 @@ export class BridgeApi extends BaseBridgeAdapter {
     _eventConfig: EventSearchConfig
   ): Promise<BridgeEvents> {
     return Promise.resolve({});
-  }
-
-  private async _getAllTransfersInRange(toAddress: Address, fromTimestampMs: number) {
-    const headers = this._defaultHeaders();
-    return this._get<BridgeResponse>(`v0/transfers?updated_after_ms=${fromTimestampMs}`, headers);
-  }
-
-  private async createTransferRouteEscrowAddress(toAddress: Address) {
-    const data = {
-      on_behalf_of: `${this.customerId}`,
-      source: {
-        payment_rail: "ethereum",
-        currency: "usdc",
-      },
-      destination: {
-        payment_rail: "tempo",
-        currency: "path_usd",
-        to_address: toAddress.toNative(),
-      },
-      return_instructions: {
-        address: toAddress.toNative(),
-      },
-      features: {
-        allow_any_from_address: true,
-        flexible_amount: true,
-      },
-    };
-    const idempotencyKey = String(Date.now());
-    const headers = {
-      ...this._defaultHeaders(),
-      "Idempotency-Key": idempotencyKey,
-    };
-    const transferRequestData = await this._post<BridgeResponse>(
-      "v0/transfers",
-      data,
-      headers as unknown as AxiosHeaders
-    );
-    return transferRequestData.source_deposit_instructions.to_address;
-  }
-
-  private _defaultHeaders() {
-    return {
-      "Api-Key": `${this.bridgeApiKey}`,
-      "Content-Type": "application/json",
-    } as unknown as AxiosHeaders;
-  }
-
-  private async _get<T>(endpoint: string, headers: AxiosHeaders, nRetries = 2) {
-    try {
-      const response = await axios.get<T>(`${this.bridgeApiBase}/${endpoint}`, { headers });
-      return response.data;
-    } catch (e) {
-      this.logger.debug({
-        at: "BridgeApi#_get",
-        message: "Failed to query bridge API",
-        endpoint,
-        e,
-      });
-      if (nRetries > 0) {
-        await delay(1);
-        return this._get<T>(endpoint, headers, --nRetries);
-      }
-      throw e;
-    }
-  }
-
-  private async _post<T>(endpoint: string, data: Record<string, unknown>, headers: AxiosHeaders, nRetries = 2) {
-    try {
-      const response = await axios.post<T>(`${this.bridgeApiBase}/${endpoint}`, data, { headers });
-      return response.data;
-    } catch (e) {
-      this.logger.debug({
-        at: "BridgeApi#_post",
-        message: "Failed to post to bridge API",
-        endpoint,
-        data,
-        e,
-      });
-      if (nRetries > 0) {
-        await delay(1);
-        return this._post<T>(endpoint, data, headers, --nRetries);
-      }
-      throw e;
-    }
   }
 }
---
+++ src/utils/BridgeUtils.ts
@@ -0,0 +1,159 @@
+import { CHAIN_IDs, Address, delay, TOKEN_SYMBOLS_MAP, toBN, winston, BigNumber } from "./";
+import axios, { AxiosHeaders } from "axios";
+
+// We need to instruct this bridge what tokens we expect to receive on L2, since the bridge
+// API supports multiple destination tokens for a single L1 token.
+export const BRIDGE_API_DESTINATION_TOKENS: { [l2ChainId: number]: string } = {
+  [CHAIN_IDs.TEMPO]: TOKEN_SYMBOLS_MAP.pathUSD.addresses[CHAIN_IDs.TEMPO],
+};
+
+export const BRIDGE_API_DESTINATION_TOKEN_SYMBOLS: { [address: string]: string } = {
+  [TOKEN_SYMBOLS_MAP.pathUSD.addresses[CHAIN_IDs.TEMPO]]: "path_usd",
+};
+
+const NETWORK_NAMES: { [chainId: number]: string } = {
+  [CHAIN_IDs.MAINNET]: "ethereum",
+  [CHAIN_IDs.TEMPO]: "tempo",
+};
+
+export const BRIDGE_API_MINIMUMS: { [sourceChainId: number]: { [dstChainId: number]: BigNumber } } = {
+  [CHAIN_IDs.MAINNET]: {
+    [CHAIN_IDs.TEMPO]: toBN(5_000_000), // 5 USD
+  },
+  [CHAIN_IDs.TEMPO]: {
+    [CHAIN_IDs.MAINNET]: toBN(5_000_000), // 5 USD
+  },
+};
+
+export type BridgeResponse = {
+  state: string;
+  created_at: string;
+  updated_at: string;
+  destination: {
+    payment_rail: string;
+    currency: string;
+    to_address: string;
+  };
+  source_deposit_instructions: {
+    payment_rail: string;
+    currency: string;
+    to_address: string;
+    from_address: string;
+  };
+  receipt:
+    | {
+        initial_amount: string;
+        final_amount: string;
+        source_tx_hash: string;
+      }
+    | undefined;
+};
+
+export class BridgeApiClient {
+  protected srcNetwork: string;
+  protected dstNetwork: string;
+
+  constructor(
+    readonly bridgeApiBase: string,
+    readonly bridgeApiKey: string,
+    readonly customerId: string,
+    srcNetworkId: number,
+    dstNetworkId: number,
+    readonly logger: winston.Logger,
+    readonly nRetries: number = 2
+  ) {
+    this.srcNetwork = NETWORK_NAMES[srcNetworkId];
+    this.dstNetwork = NETWORK_NAMES[dstNetworkId];
+  }
+
+  async getAllTransfersInRange(toAddress: Address, fromTimestampMs: number) {
+    const headers = this.defaultHeaders();
+    return this.getWithRetry<BridgeResponse>(`v0/transfers?updated_after_ms=${fromTimestampMs}`, headers);
+  }
+
+  async createTransferRouteEscrowAddress(toAddress: Address, _l1TokenSymbol: string, _l2TokenSymbol: string) {
+    const l1TokenSymbol = _l1TokenSymbol.toLowerCase();
+    const l2TokenSymbol = _l2TokenSymbol.toLowerCase();
+    const data = {
+      on_behalf_of: `${this.customerId}`,
+      source: {
+        payment_rail: this.srcNetwork,
+        currency: l1TokenSymbol,
+      },
+      destination: {
+        payment_rail: this.dstNetwork,
+        currency: l2TokenSymbol,
+        to_address: toAddress.toNative(),
+      },
+      return_instructions: {
+        address: toAddress.toNative(),
+      },
+      features: {
+        allow_any_from_address: true,
+        flexible_amount: true,
+      },
+    };
+    const idempotencyKey = String(Date.now());
+    const headers = {
+      ...this.defaultHeaders(),
+      "Idempotency-Key": idempotencyKey,
+    };
+    const transferRequestData = await this.postWithRetry<BridgeResponse>(
+      "v0/transfers",
+      data,
+      headers as unknown as AxiosHeaders
+    );
+    return transferRequestData.source_deposit_instructions.to_address;
+  }
+
+  defaultHeaders() {
+    return {
+      "Api-Key": `${this.bridgeApiKey}`,
+      "Content-Type": "application/json",
+    } as unknown as AxiosHeaders;
+  }
+
+  async getWithRetry<T>(endpoint: string, headers: AxiosHeaders, nRetries = this.nRetries) {
+    try {
+      const response = await axios.get<T>(`${this.bridgeApiBase}/${endpoint}`, { headers });
+      return response.data;
+    } catch (e) {
+      this.logger.debug({
+        at: "BridgeApi#_get",
+        message: "Failed to query bridge API",
+        endpoint,
+        e,
+      });
+      if (nRetries > 0) {
+        await delay(1);
+        return this.getWithRetry<T>(endpoint, headers, --nRetries);
+      }
+      throw e;
+    }
+  }
+
+  async postWithRetry<T>(
+    endpoint: string,
+    data: Record<string, unknown>,
+    headers: AxiosHeaders,
+    nRetries = this.nRetries
+  ) {
+    try {
+      const response = await axios.post<T>(`${this.bridgeApiBase}/${endpoint}`, data, { headers });
+      return response.data;
+    } catch (e) {
+      this.logger.debug({
+        at: "BridgeApi#_post",
+        message: "Failed to post to bridge API",
+        endpoint,
+        data,
+        e,
+      });
+      if (nRetries > 0) {
+        await delay(1);
+        return this.postWithRetry<T>(endpoint, data, headers, --nRetries);
+      }
+      throw e;
+    }
+  }
+}
