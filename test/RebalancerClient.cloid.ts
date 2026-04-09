777f3cc13e16d2c1a5426aa84f3e6cd066bc8312
nicholaspai <npai.nyc@gmail.com>
2026-04-09 11:43:34 -0400

    Update RebalancerClient.cloid.ts

--- test/RebalancerClient.cloid.ts
+++ test/RebalancerClient.cloid.ts
@@ -1,30 +1,21 @@
 import { expect } from "chai";
 import { ethers } from "ethers";
-import { getCloidForTimestampAndAccount } from "../src/rebalancer/utils/cloid";
+import { getCloidForAccount } from "../src/rebalancer/utils/utils";
+import { delay } from "../src/utils";
 
 describe("RebalancerClient.cloid", () => {
-  it("derives distinct 16-byte cloids for different relayer accounts at the same timestamp", async () => {
-    const fixedTimestamp = 1_234_567_890;
+  it("derives distinct 16-byte cloids for different relayer accounts at different timestamps", async () => {
     const signerA = ethers.Wallet.createRandom();
     const signerB = ethers.Wallet.createRandom();
     const accountA = await signerA.getAddress();
     const accountB = await signerB.getAddress();
 
-    const cloidA = getCloidForTimestampAndAccount(fixedTimestamp, accountA);
-    const cloidB = getCloidForTimestampAndAccount(fixedTimestamp, accountB);
+    const cloidA = getCloidForAccount(accountA);
+    await delay(1);
+    const cloidB = getCloidForAccount(accountB);
 
     expect(cloidA).to.match(/^0x[0-9a-f]{32}$/);
     expect(cloidB).to.match(/^0x[0-9a-f]{32}$/);
     expect(cloidA).to.not.equal(cloidB);
-  });
-
-  it("is deterministic for the same relayer account and timestamp", async () => {
-    const fixedTimestamp = 1_234_567_890;
-    const signer = ethers.Wallet.createRandom();
-    const account = await signer.getAddress();
-
-    expect(getCloidForTimestampAndAccount(fixedTimestamp, account)).to.equal(
-      getCloidForTimestampAndAccount(fixedTimestamp, account)
-    );
   });
 });
