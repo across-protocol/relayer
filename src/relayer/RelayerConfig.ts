c7908a63470c7977b635f395401460e026e90f0a
Paul <108695806+pxrl@users.noreply.github.com>
2026-05-07 20:32:02 +0200

    improve: strictNullChecks (D/n) (#3359)

--- src/relayer/RelayerConfig.ts
+++ src/relayer/RelayerConfig.ts
@@ -395,7 +395,9 @@ export class RelayerConfig extends CommonConfig {
 
       // Ensure that there is always a deposit confirmation config for the maximum theoretical value of a fill.
       Object.values(this.minDepositConfirmations).forEach((depositConfirmations) => {
-        const { usdThreshold: maxThreshold, minConfirmations: maxConfirmations } = depositConfirmations.at(-1);
+        const last = depositConfirmations.at(-1);
+        assert(isDefined(last), "Empty depositConfirmations array");
+        const { usdThreshold: maxThreshold, minConfirmations: maxConfirmations } = last;
         if (maxThreshold.lt(bnUint256Max)) {
           depositConfirmations.push({
             usdThreshold: bnUint256Max,
